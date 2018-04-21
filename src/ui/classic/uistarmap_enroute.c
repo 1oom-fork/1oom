ea599631ec645e5e422c71f0d4a3def38c23b828
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-04-21 13:12:46 +0000

    Implement smoother starmap scrolling

--- src/ui/classic/uistarmap_enroute.c
+++ src/ui/classic/uistarmap_enroute.c
@@ -147,6 +147,7 @@ void ui_starmap_enroute(struct game_s *g, player_id_t active_player)
 
     d.g = g;
     d.api = active_player;
+    d.anim_delay = 0;
 
     r = &(g->enroute[ui_data.starmap.fleet_selected]);
     d.en.can_move = g->eto[active_player].have_hyperspace_comm ? GOT_HYPERCOMM : NO_MOVE;
