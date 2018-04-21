60055efa42a6184b39116f97502373fb12296324
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-04-21 20:26:37 +0300

    Config file support implemented

--- src/hw/sdl/1/hwsdl1_opt.c
+++ src/hw/sdl/1/hwsdl1_opt.c
@@ -3,8 +3,10 @@
 #include <stdio.h>
 
 #include "hw.h"
+#include "cfg.h"
 #include "hwsdl_opt.h"
 #include "hwsdl_audio.h"
+#include "lib.h"
 #include "options.h"
 #include "types.h"
 
@@ -23,6 +25,14 @@ int hw_opt_gl_filter = 1;
 #endif /* HAVE_SDL1MIXER */
 
 /* -------------------------------------------------------------------------- */
+
+const struct cfg_items_s hw_cfg_items_extra[] = {
+#ifdef HAVE_SDL1GL
+    CFG_ITEM_INT("aspect", &hw_opt_aspect, 0),
+    CFG_ITEM_INT("filter", &hw_opt_gl_filter, 0),
+#endif /* HAVE_SDL1GL */
+    CFG_ITEM_END
+};
 
 #include "hwsdl_opt.c"
 
