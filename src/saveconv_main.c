2a45ffb055c19b07484d3896a7518ed6f1a6e5a5
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-04-21 13:12:46 +0000

    Add PBX support

--- src/saveconv_main.c
+++ src/saveconv_main.c
@@ -247,6 +247,11 @@ const struct cmdline_options_s main_cmdline_options[] = {
     { 0, 0, 0, 0, 0, 0 }
 };
 
+int pbx_add_file(const char *filename)
+{
+    return -1;
+}
+
 /* -------------------------------------------------------------------------- */
 
 static struct game_s game;
