5eccd8653cda24c216ec059a9480e104f2a637fc
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-04-21 20:26:37 +0300

    Config file support implemented

--- src/hw/sdl/1/hwsdl1_opt.c
+++ src/hw/sdl/1/hwsdl1_opt.c
@@ -3,14 +3,16 @@
 #include <stdio.h>
 
 #include "hw.h"
+#include "cfg.h"
 #include "hwsdl_opt.h"
 #include "hwsdl_audio.h"
+#include "lib.h"
 #include "options.h"
 #include "types.h"
 
 /* -------------------------------------------------------------------------- */
 
-#define HW_DEFAULT_FULLSCREEN   0
+#define HW_DEFAULT_FULLSCREEN   false
 
 #ifdef HAVE_SDL1GL
 int hw_opt_gl_filter = 1;
@@ -22,6 +24,13 @@ int hw_opt_gl_filter = 1;
 #endif /* HAVE_SDL1MIXER */
 
 /* -------------------------------------------------------------------------- */
+
+const struct cfg_items_s hw_cfg_items_extra[] = {
+#ifdef HAVE_SDL1GL
+    CFG_ITEM_INT("filter", &hw_opt_gl_filter, 0),
+#endif /* HAVE_SDL1GL */
+    CFG_ITEM_END
+};
 
 #include "hwsdl_opt.c"
 
