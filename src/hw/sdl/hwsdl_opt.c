c62a4f9acf8404f7fd8de719565fbe93ad049df3
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-04-21 20:26:37 +0300

    Config file support implemented
    This commit has been modified to fix bugs:
    aafbbafdb9e854b6d97d9fe01213956bec619c45
    cdd8d0a15580286666c2fc85632207cae36f8cce
    30f47c68070802ed39eb63be1143e0566f69783a
    ba1a7fb4c26b677cc71baef4f8c57bd34386a87b

--- src/hw/sdl/hwsdl_opt.c
+++ src/hw/sdl/hwsdl_opt.c
@@ -12,19 +12,33 @@
 
 /* -------------------------------------------------------------------------- */
 
-int hw_opt_fullscreen = HW_DEFAULT_FULLSCREEN;
+bool hw_opt_fullscreen = HW_DEFAULT_FULLSCREEN;
 int hw_opt_screen_winw = 0;
 int hw_opt_screen_winh = 0;
 int hw_opt_screen_fsw = 0;
 int hw_opt_screen_fsh = 0;
-const char *hw_opt_sdlmixer_sf = NULL;
+char *hw_opt_sdlmixer_sf = NULL;
+
+/* -------------------------------------------------------------------------- */
+
+const struct cfg_items_s hw_cfg_items[] = {
+    CFG_ITEM_BOOL("fs", &hw_opt_fullscreen),
+    CFG_ITEM_INT("winw", &hw_opt_screen_winw, 0),
+    CFG_ITEM_INT("winh", &hw_opt_screen_winh, 0),
+    CFG_ITEM_INT("fsw", &hw_opt_screen_fsw, 0),
+    CFG_ITEM_INT("fsh", &hw_opt_screen_fsh, 0),
+#ifdef HAVE_SDLMIXER
+    CFG_ITEM_STR("sdlmixersf", &hw_opt_sdlmixer_sf, 0),
+#endif
+    CFG_ITEM_END
+};
 
 /* -------------------------------------------------------------------------- */
 
 #ifdef HAVE_SDLMIXER
-int hw_opt_set_sdlmixer_sf(char **argv, void *var)
+static int hw_opt_set_sdlmixer_sf(char **argv, void *var)
 {
-    hw_opt_sdlmixer_sf = argv[1];
+    hw_opt_sdlmixer_sf = lib_stralloc(argv[1]);
     return hw_audio_set_sdlmixer_sf(hw_opt_sdlmixer_sf);
 }
 #endif
@@ -33,10 +47,10 @@ int hw_opt_set_sdlmixer_sf(char **argv, void *var)
 
 const struct cmdline_options_s hw_cmdline_options[] = {
     { "-fs", 0,
-      options_enable_var, (void *)&hw_opt_fullscreen,
+      options_enable_bool_var, (void *)&hw_opt_fullscreen,
       NULL, "Enable fullscreen" },
     { "-window", 0,
-      options_disable_var, (void *)&hw_opt_fullscreen,
+      options_disable_bool_var, (void *)&hw_opt_fullscreen,
       NULL, "Use windowed mode" },
     { "-winw", 1,
       options_set_int_var, (void *)&hw_opt_screen_winw,
