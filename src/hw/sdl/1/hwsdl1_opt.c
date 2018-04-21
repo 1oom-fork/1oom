8943e76d81e58fd8726ec4ebf79bc0a29ec32452
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-04-21 20:26:37 +0300

    Config file support implemented

--- src/hw/sdl/1/hwsdl1_opt.c
+++ src/hw/sdl/1/hwsdl1_opt.c
@@ -3,18 +3,20 @@
 #include <stdio.h>
 
 #include "hw.h"
+#include "cfg.h"
 #include "hwsdl_opt.h"
 #include "hwsdl_audio.h"
+#include "lib.h"
 #include "options.h"
 #include "types.h"
 
 /* -------------------------------------------------------------------------- */
 
-#define HW_DEFAULT_FULLSCREEN   0
+#define HW_DEFAULT_FULLSCREEN   false
 #define HW_DEFAULT_ASPECT   1000000
 
 #ifdef HAVE_OPENGL
-int hw_opt_use_gl = 1;
+bool hw_opt_use_gl = true;
 int hw_opt_aspect = HW_DEFAULT_ASPECT;
 int hw_opt_gl_filter = 1;
 #endif
@@ -25,15 +27,24 @@ int hw_opt_gl_filter = 1;
 
 /* -------------------------------------------------------------------------- */
 
+const struct cfg_items_s hw_cfg_items_extra[] = {
+#ifdef HAVE_OPENGL
+    CFG_ITEM_BOOL("gl", &hw_opt_use_gl),
+    CFG_ITEM_INT("aspect", &hw_opt_aspect, 0),
+    CFG_ITEM_INT("filter", &hw_opt_gl_filter, 0),
+#endif
+    CFG_ITEM_END
+};
+
 #include "hwsdl_opt.c"
 
 const struct cmdline_options_s hw_cmdline_options_extra[] = {
 #ifdef HAVE_OPENGL
     { "-gl", 0,
-      options_enable_var, (void *)&hw_opt_use_gl,
+      options_enable_bool_var, (void *)&hw_opt_use_gl,
       NULL, "Enable OpenGL" },
     { "-nogl", 0,
-      options_disable_var, (void *)&hw_opt_use_gl,
+      options_disable_bool_var, (void *)&hw_opt_use_gl,
       NULL, "Disable OpenGL" },
     { "-aspect", 1,
       options_set_int_var, (void *)&hw_opt_aspect,
