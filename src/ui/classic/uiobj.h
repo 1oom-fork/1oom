5893613140d055edc4433ae89402f62a1888abe7
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-04-23 00:20:50 +0300

    Implement uiobj_table_num_store/uiobj_table_num_restore

--- src/ui/classic/uiobj.h
+++ src/ui/classic/uiobj.h
@@ -19,6 +19,8 @@ extern int uiobj_maxy;
 
 extern void uiobj_table_clear(void);
 extern void uiobj_table_set_last(int16_t oi);
+extern void uiobj_table_num_store(void);
+extern void uiobj_table_num_restore(void);
 extern int16_t uiobj_handle_input_cond(void);
 
 extern void uiobj_finish_frame(void);
