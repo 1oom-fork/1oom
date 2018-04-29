4e4b52d38be6df88762e0ca808dccdd5461bd2f7
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-04-29 09:31:17 +0300

    os: Add -user

--- src/os/win32/os.c
+++ src/os/win32/os.c
@@ -18,6 +18,7 @@ const struct cmdline_options_s os_cmdline_options[] = {
 /* -------------------------------------------------------------------------- */
 
 static char *data_path = NULL;
+static char *user_path = NULL;
 static char *all_data_paths[] = { NULL, NULL, NULL };
 
 /* -------------------------------------------------------------------------- */
@@ -48,6 +49,8 @@ void os_shutdown(void)
 {
     lib_free(data_path);
     data_path = NULL;
+    lib_free(user_path);
+    user_path = NULL;
 }
 
 const char **os_get_paths_data(void)
@@ -73,12 +76,24 @@ void os_set_path_data(const char *path)
 
 const char *os_get_path_user(void)
 {
-    return ".";
+    if (user_path == NULL) {
+        user_path = lib_stralloc(".");
+    }
+    return user_path;
+}
+
+void os_set_path_user(const char *path)
+{
+    if (user_path) {
+        lib_free(user_path);
+        user_path = NULL;
+    }
+    user_path = lib_stralloc(path);
 }
 
 int os_make_path_user(void)
 {
-    return 0;
+    return os_make_path(os_get_path_user());
 }
 
 int os_make_path_for(const char *filename)
