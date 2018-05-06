dff347932639e60282206b5696cd6f1dd556e187
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-06 11:50:20 +0300

    Diplo refactor and hmm naming

--- src/game/game_diplo.h
+++ src/game/game_diplo.h
@@ -21,5 +21,6 @@ extern void game_diplo_stop_war(struct game_s *g, player_id_t pi1, player_id_t p
 extern void game_diplo_limit_mood_treaty(struct game_s *g);
 extern void game_diplo_mood_relax(struct game_s *g);
 extern int16_t game_diplo_get_mood(struct game_s *g, player_id_t p1, player_id_t p2);
+extern uint8_t game_diplo_is_gone(struct game_s *g, player_id_t api, player_id_t pi);
 
 #endif
