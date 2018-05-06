a6d0c101822abccc25375150502529897f3c5c40
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-06 13:21:26 +0300

    Set eco to 0 if pop at max, no eco projects and no waste

--- src/game/game_turn.c
+++ src/game/game_turn.c
@@ -1608,7 +1608,6 @@ static void game_turn_finished_slider(struct game_s *g)
             game_add_planet_to_build_finished(g, pli, pi, FINISHED_ECO2);
         }
         if (1
-          && (p->owner == pi)
           && (p->pop >= p->max_pop3)
           && (p->unrest == PLANET_UNREST_NORMAL)
         ) {
@@ -1652,6 +1651,16 @@ static void game_turn_finished_slider(struct game_s *g)
                         game_add_planet_to_build_finished(g, pli, pi, FINISHED_ECO1);
                     }
                 }
+            } else if (1
+              && (p->slider[PLANET_SLIDER_ECO] > 0)
+              && ((e->race == RACE_SILICOID) || ((e->ind_waste_scale == 0) && (p->waste == 0)))
+            ) {
+                if (p->factories >= (p->pop * e->colonist_oper_factories)) {
+                    p->slider[PLANET_SLIDER_TECH] += p->slider[PLANET_SLIDER_ECO];
+                } else {
+                    p->slider[PLANET_SLIDER_IND] += p->slider[PLANET_SLIDER_ECO];
+                }
+                p->slider[PLANET_SLIDER_ECO] = 0;
             }
         }
         if (BOOLVEC_IS1(p->finished, FINISHED_SHIELD)) {
