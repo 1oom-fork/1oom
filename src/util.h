fa84c87923a14f426ffe1d1692b020c002e73ef7
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-08 11:58:53 +0300

    Fix constness

--- src/util.h
+++ src/util.h
@@ -11,7 +11,7 @@ extern char *util_concat(const char *s, ...);
 extern int util_concat_buf(char *buf, int buflen, ...);
 extern int util_get_line(char *buf, int bufsize, FILE *f);
 extern void util_fname_split(const char *path, char **directory_out, char **name_out);
-extern int util_file_save(const char *name, uint8_t *src, int size);
+extern int util_file_save(const char *name, const uint8_t *src, int size);
 extern void util_trim_whitespace(char *str);
 extern void util_str_tolower(char *str);
 extern bool util_parse_number(const char *str, uint32_t *val_ptr);
