ee4b2080c9ab0051242c150cb30713145a42ba19
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-12 13:30:44 +0300

    hwsdl1::hw_opt_aspect -> hwsdl::hw_opt_aspect

--- src/hw/sdl/hwsdl_opt.c
+++ src/hw/sdl/hwsdl_opt.c
@@ -12,11 +12,16 @@
 
 /* -------------------------------------------------------------------------- */
 
+#define HW_DEFAULT_ASPECT   1000000
+
 int hw_opt_fullscreen = HW_DEFAULT_FULLSCREEN;
 int hw_opt_screen_winw = 0;
 int hw_opt_screen_winh = 0;
 int hw_opt_screen_fsw = 0;
 int hw_opt_screen_fsh = 0;
+#ifdef HAVE_SDLX_ASPECT
+int hw_opt_aspect = HW_DEFAULT_ASPECT;
+#endif
 const char *hw_opt_sdlmixer_sf = NULL;
 
 /* -------------------------------------------------------------------------- */
@@ -54,6 +59,11 @@ const struct cmdline_options_s hw_cmdline_options[] = {
     { "-sdlmixersf", 1,
       hw_opt_set_sdlmixer_sf, NULL,
       "FILE.SF2", "Set SDL_mixer soundfont" },
+#endif
+#ifdef HAVE_SDLX_ASPECT
+    { "-aspect", 1,
+      options_set_int_var, (void *)&hw_opt_aspect,
+      "ASPECT", "Set aspect ratio (*1000000, 0 = off)" },
 #endif
     { NULL, 0, NULL, NULL, NULL, NULL }
 };
