762af46f75a5d154dc2e5aaa89237056a3004241
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-12 13:30:44 +0300

    Add experimental SDL2 support

--- src/hw/sdl/hwsdl_opt.h
+++ src/hw/sdl/hwsdl_opt.h
@@ -7,13 +7,17 @@ extern int hw_opt_screen_winh;
 extern int hw_opt_screen_fsw;
 extern int hw_opt_screen_fsh;
 extern char *hw_opt_sdlmixer_sf;
+extern int hw_opt_aspect;
 
 /* debug */
 extern int hw_opt_overlay_pal;
 
 /* for SDL1 */
 extern bool hw_opt_use_gl;
-extern int hw_opt_aspect;
 extern int hw_opt_gl_filter;
+
+/* for SDL2 */
+extern bool hw_opt_force_sw;
+extern bool hw_opt_int_scaling;
 
 #endif
