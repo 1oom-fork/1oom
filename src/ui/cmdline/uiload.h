308024ac311e0443f48fb874072f0188c0b05b41
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-13 20:18:03 +0300

    Implement a chunk of cmdline UI

--- src/ui/cmdline/uiload.h
+++ src/ui/cmdline/uiload.h
@@ -1,7 +1,10 @@
 #ifndef INC_1OOM_UILOAD_H
 #define INC_1OOM_UILOAD_H
 
-/* returns -1 on cancel or 0..5 on load game */
+struct input_token_s;
+
+/* returns -1 on cancel or 0..7 on load game */
 extern int ui_load_game(void);
+extern int ui_cmd_load(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var);
 
 #endif
