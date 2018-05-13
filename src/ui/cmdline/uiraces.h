7405a3a72cdeff15191e8abe2fe57ce203d1559d
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-13 20:18:03 +0300

    Implement a chunk of cmdline UI

---
+++ src/ui/cmdline/uiraces.h
@@ -0,0 +1,11 @@
+#ifndef INC_1OOM_UIRACES_H
+#define INC_1OOM_UIRACES_H
+
+#include "types.h"
+
+struct game_s;
+struct input_token_s;
+
+extern int ui_cmd_audience(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var);
+
+#endif
