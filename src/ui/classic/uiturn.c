0b681ba665532d88e74036d5afe5c56b09c71db7
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-14 16:13:41 +0300

    Remove unused uiobj helpid

--- src/ui/classic/uiturn.c
+++ src/ui/classic/uiturn.c
@@ -65,7 +65,7 @@ void ui_turn_msg(struct game_s *g, int pi, const char *str)
     d.sm.anim_delay = 0;
     uiobj_set_callback_and_delay(ui_turn_msg_draw_cb, &d, 3);
     uiobj_table_clear();
-    uiobj_add_mousearea(UI_SCREEN_LIMITS, MOO_KEY_UNKNOWN, -1);
+    uiobj_add_mousearea(UI_SCREEN_LIMITS, MOO_KEY_UNKNOWN);
     while (!flag_done) {
         int16_t oi;
         ui_delay_prepare();
