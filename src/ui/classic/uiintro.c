258fb7218a1609eeedf62209895bb2140491c010
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-14 16:13:41 +0300

    Remove unused uiobj helpid

--- src/ui/classic/uiintro.c
+++ src/ui/classic/uiintro.c
@@ -68,7 +68,7 @@ void ui_play_intro(void)
     uiobj_table_clear();
     uiobj_set_skip_delay(true);
 
-    oi_skip = uiobj_add_mousearea(UI_SCREEN_LIMITS, MOO_KEY_UNKNOWN, -1);
+    oi_skip = uiobj_add_mousearea(UI_SCREEN_LIMITS, MOO_KEY_UNKNOWN);
     flag_skip = 0;
     uiobj_set_downcount(2);
 
