fc2a47027ad9d9d5c83e03acd70134241ebd306e
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-17 21:08:39 +0300

    Add MSDOS support using Allegro 4

---
+++ src/hw/alleg/4/hwalleg4_mouse.c
@@ -0,0 +1,1 @@
+#include "hwalleg_mouse.c"
---
+++ src/hw/alleg/hwalleg_audio.h
@@ -0,0 +1,7 @@
+#ifndef INC_1OOM_HWSDL_AUDIO_H
+#define INC_1OOM_HWSDL_AUDIO_H
+
+extern int hw_audio_init(void);
+extern void hw_audio_shutdown(void);
+
+#endif
