395296228322c1175fcd14c032a19a2bab8749ee
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-17 21:08:39 +0300

    Add MSDOS support using Allegro 4

---
+++ src/hw/alleg/hwalleg.c
@@ -0,0 +1,31 @@
+static bool hw_kbd_check_hotkey(int key, int smod)
+{
+    if ((smod & KB_CTRL_FLAG) && (!(smod & KB_ALT_FLAG))) {
+        if ((key >> 8) == KEY_ESC) {
+            log_message("Alleg: got Ctrl-ESC, quitting now\n");
+            exit(EXIT_SUCCESS);
+        } else if ((key >> 8) == KEY_CLOSEBRACE) {
+            if (smod & KB_SHIFT_FLAG) {
+                hw_audio_music_volume(opt_music_volume + 4);
+            } else {
+                hw_audio_sfx_volume(opt_sfx_volume + 4);
+            }
+            return true;
+        } else if ((key >> 8) == KEY_OPENBRACE) {
+            if (smod & KB_SHIFT_FLAG) {
+                hw_audio_music_volume(opt_music_volume - 4);
+            } else {
+                hw_audio_sfx_volume(opt_sfx_volume - 4);
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+/* -------------------------------------------------------------------------- */
+
+int hw_icon_set(const uint8_t *data, const uint8_t *pal, int w, int h)
+{
+    return 0;
+}
