7f60b7bf246135050d43568229c52843c2183511
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-17 15:38:24 +0300

    Write a log file by default

--- src/options.h
+++ src/options.h
@@ -36,6 +36,8 @@ extern int options_enable_bool_var(char **argv, void *var);
 extern int options_disable_bool_var(char **argv, void *var);
 extern int options_set_int_var(char **argv, void *var);
 extern int options_set_str_var(char **argv, void *var);
+extern int options_unset_str_var(char **argv, void *var);
+extern int options_empty_str_var(char **argv, void *var);
 
 extern int options_parse_early(int argc, char **argv);
 extern int options_parse(int argc, char **argv);
