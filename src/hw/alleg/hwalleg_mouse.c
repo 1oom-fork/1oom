d06ad57e1f86227a94ef0eff28a72d3763a63d47
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-17 21:08:39 +0300

    Add MSDOS support using Allegro 4

---
+++ src/hw/alleg/hwalleg_mouse.c
@@ -0,0 +1,32 @@
+#include "config.h"
+
+#include "types.h"
+#include <allegro.h>
+
+#include "hw.h"
+#include "hwalleg_mouse.h"
+#include "mouse.h"
+
+/* -------------------------------------------------------------------------- */
+
+bool hw_mouse_enabled = true;
+
+/* -------------------------------------------------------------------------- */
+
+void hw_mouse_buttons(int state)
+{
+    if (hw_mouse_enabled) {
+        int b = mouse_buttons;
+        if (state & 1) {
+            b |= MOUSE_BUTTON_MASK_LEFT;
+        } else {
+            b &= ~MOUSE_BUTTON_MASK_LEFT;
+        }
+        if (state & 2) {
+            b |= MOUSE_BUTTON_MASK_RIGHT;
+        } else {
+            b &= ~MOUSE_BUTTON_MASK_RIGHT;
+        }
+        mouse_set_buttons_from_hw(b);
+    }
+}
---
+++ src/os/msdos/os.c
@@ -0,0 +1,124 @@
+#include "config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/stat.h>
+
+#include "os.h"
+#include "options.h"
+#include "lib.h"
+#include "util.h"
+
+/* -------------------------------------------------------------------------- */
+
+const struct cmdline_options_s os_cmdline_options[] = {
+    { NULL, 0, NULL, NULL, NULL, NULL }
+};
+
+/* -------------------------------------------------------------------------- */
+
+static char *data_path = NULL;
+static char *user_path = NULL;
+static char *all_data_paths[] = { NULL, NULL, NULL, NULL };
+
+/* -------------------------------------------------------------------------- */
+
+static int os_make_path(const char *path)
+{
+    if ((path == NULL) || ((path[0] == '.') && (path[1] == '\0'))) {
+        return 0;
+    }
+    return mkdir(path, 0700);
+}
+
+/* -------------------------------------------------------------------------- */
+
+const char *idstr_os = "msdos";
+
+int os_early_init(void)
+{
+    return 0;
+}
+
+int os_init(void)
+{
+    return 0;
+}
+
+void os_shutdown(void)
+{
+    lib_free(data_path);
+    data_path = NULL;
+    lib_free(user_path);
+    user_path = NULL;
+}
+
+const char **os_get_paths_data(void)
+{
+    int i = 0;
+    if (data_path) {
+        all_data_paths[i++] = data_path;
+    }
+    all_data_paths[i++] = ".";
+    all_data_paths[i++] = ".\\data";
+    all_data_paths[i] = NULL;
+    return (const char **)all_data_paths;
+}
+
+const char *os_get_path_data(void)
+{
+    return data_path;
+}
+
+void os_set_path_data(const char *path)
+{
+    data_path = lib_stralloc(path);
+}
+
+const char *os_get_path_user(void)
+{
+    if (user_path == NULL) {
+        user_path = lib_stralloc(".");
+    }
+    return user_path;
+}
+
+void os_set_path_user(const char *path)
+{
+    if (user_path) {
+        lib_free(user_path);
+        user_path = NULL;
+    }
+    user_path = lib_stralloc(path);
+}
+
+int os_make_path_user(void)
+{
+    return os_make_path(os_get_path_user());
+}
+
+int os_make_path_for(const char *filename)
+{
+    int res = 0;
+    char *path;
+    util_fname_split(filename, &path, NULL);
+    if (path != NULL) {
+        res = os_make_path(path);
+        lib_free(path);
+    }
+    return res;
+}
+
+const char *os_get_fname_save(char *buf, int savei/*1..9*/)
+{
+    sprintf(buf, "1oomsav%i.bin", savei);
+    return buf;
+}
+
+uint32_t os_get_time_us(void)
+{
+    /* Not implemented */
+    return 0;
+}
