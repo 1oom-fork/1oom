a76d53f1bd04b5c1a2175816ba5473fc0a8c438d
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-17 21:08:39 +0300

    Add MSDOS support using Allegro 4

---
+++ src/hw/alleg/4/hwalleg4_opt.c
@@ -0,0 +1,24 @@
+#include "config.h"
+
+#include <stdio.h>
+
+#include "hw.h"
+#include "cfg.h"
+#include "hwalleg_opt.h"
+#include "lib.h"
+#include "options.h"
+#include "types.h"
+
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+
+const struct cfg_items_s hw_cfg_items_extra[] = {
+    CFG_ITEM_END
+};
+
+#include "hwalleg_opt.c"
+
+const struct cmdline_options_s hw_cmdline_options_extra[] = {
+    { NULL, 0, NULL, NULL, NULL, NULL }
+};
