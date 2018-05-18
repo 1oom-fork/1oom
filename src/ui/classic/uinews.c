71415902d8d55b1905d7c48d1c1d7db05227f358
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-19 01:40:11 +0300

    classic: implement the rest of local player switching

--- src/ui/classic/uinews.c
+++ src/ui/classic/uinews.c
@@ -22,6 +22,7 @@
 #include "uiobj.h"
 #include "uipal.h"
 #include "uisound.h"
+#include "uiswitch.h"
 #include "vgabuf.h"
 
 /* -------------------------------------------------------------------------- */
@@ -305,6 +306,7 @@ void ui_news(struct game_s *g, struct news_s *ns)
 {
     bool flag_skip = false, flag_fade;
     struct news_data_s d;
+    ui_switch_all(g);
     d.ns = ns;
     if (!ui_data.news.flag_also) {
         if (ui_draw_finish_mode == 0) {
