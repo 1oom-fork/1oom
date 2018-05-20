d80a683274582a2645473c9657977ac411e71eb3
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-20 18:59:41 +0300

    cmdline: rewrite intro and remove game name
    Rewrite the cmdline intro sequence to reduce the chance that
    someone will confuse this project to be developed by Simtex or
    presented by MicroProse.

--- src/ui/cmdline/uiintro.c
+++ src/ui/cmdline/uiintro.c
@@ -8,5 +8,12 @@
 
 void ui_play_intro(void)
 {
-    fputs("\n\nMicroProse presents...\n\na Simtex Software production...\n\n*** Master of Orion ***\n\n\n", stdout);
+    fputs("\n\n(imagine a view of a planet from orbit...\n"
+          " names of dead companies...\n"
+          " $GAME_TITLE\n"
+          " ships shooting at missile bases...\n"
+          " missile base shooting back...\n"
+          " *BOOM*\n"
+          " remaining ships warping out...\n"
+          " credits roll by... intro over!)\n", stdout);
 }
