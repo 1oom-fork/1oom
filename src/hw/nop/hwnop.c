c535e75ed7c97a76308dc0a6c385f47fb9794f1b
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-23 18:22:01 +0300

    Clean up unused funcs in hw/nop/

--- src/hw/nop/hwnop.c
+++ src/hw/nop/hwnop.c
@@ -69,21 +69,6 @@ uint32_t hw_get_time_us(void)
     return 0;
 }
 
-int hw_event_handle(void)
-{
-    return 0;
-}
-
-int hw_icon_set(const uint8_t *data, const uint8_t *pal, int w, int h)
-{
-    return 0;
-}
-
-int hw_video_init(int w, int h)
-{
-    return 0;
-}
-
 void hw_video_set_palette(uint8_t *palette, int first, int num)
 {
 }
@@ -91,36 +76,4 @@ void hw_video_set_palette(uint8_t *palette, int first, int num)
 uint8_t *hw_video_get_buf(void)
 {
     return 0;
-}
-
-uint8_t *hw_video_draw_buf(void)
-{
-    return 0;
-}
-
-int hw_audio_music_init(int mus_index, const uint8_t *data, uint32_t len)
-{
-    return 0;
-}
-void hw_audio_music_release(int mus_index)
-{
-}
-void hw_audio_music_play(int mus_index)
-{
-}
-void hw_audio_music_stop(void)
-{
-}
-int hw_audio_sfx_init(int sfx_index, const uint8_t *data, uint32_t len)
-{
-    return 0;
-}
-void hw_audio_sfx_release(int sfx_index)
-{
-}
-void hw_audio_sfx_play(int sfx_index)
-{
-}
-void hw_audio_sfx_stop(void)
-{
 }
