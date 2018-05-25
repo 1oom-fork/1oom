da04d4e4cac40be6806cf69e8221912dcba18676
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-25 10:13:09 +0300

    Fix 1oom typos (cosmetics)

--- src/ui/classic/uispy_sab.c
+++ src/ui/classic/uispy_sab.c
@@ -88,7 +88,7 @@ static void sabotage_draw_cb(void *vptr)
     int pop, bases, fact;
     hw_video_copy_back_from_page2();
     ui_draw_filled_rect(222, 4, 314, 179, 0);
-    lbxgfx_draw_frame(222, 4, d->gfx_saboback, UI_SCREEN_W);
+    lbxgfx_draw_frame(222, 4, d->gfx_sabobac2, UI_SCREEN_W);
     ui_starmap_draw_planetinfo_2(g, d->api, d->target, d->planet);
     /*set_limits(228, 110, 309, 143);*/
     lbxgfx_draw_frame_offs(228, 70, d->gfx_colony, 228, 110, 309, 143, UI_SCREEN_W);
