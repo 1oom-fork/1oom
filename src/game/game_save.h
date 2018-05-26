86f426ce3ddbf2fd6ac53d575bd601de8790dfd6
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-05-26 13:18:00 +0300

    Prepare for next save version

--- src/game/game_save.h
+++ src/game/game_save.h
@@ -13,13 +13,13 @@
 extern bool game_save_tbl_have_save[NUM_ALL_SAVES];
 extern char game_save_tbl_name[NUM_ALL_SAVES][SAVE_NAME_LEN];
 
-extern void *game_save_open_check_header(const char *filename, int i, bool update_table, char *savename);
+extern void *game_save_open_check_header(const char *filename, int i, bool update_table, char *savename, uint32_t *versionptr);
 extern int game_save_get_slot_fname(char *fnamebuf, int buflen, int i);
 extern int game_save_check_saves(char *fnamebuf, int buflen);
 
 struct game_s;
 extern int game_save_do_load_fname(const char *filename, char *savename, struct game_s *g);
-extern int game_save_do_save_fname(const char *filename, const char *savename, const struct game_s *g);
+extern int game_save_do_save_fname(const char *filename, const char *savename, const struct game_s *g, uint32_t version);
 extern int game_save_do_load_i(int savei/*0..NUM_ALL_SAVES-1*/, struct game_s *g);
 extern int game_save_do_save_i(int savei/*0..NUM_ALL_SAVES-1*/, const char *savename, const struct game_s *g);
 
