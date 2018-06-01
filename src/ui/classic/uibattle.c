12828521f6efe06c1c3136441192c2aca11871fd
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-01 12:51:06 +0300

    Implement multiplayer final war, related improvements

--- src/ui/classic/uibattle.c
+++ src/ui/classic/uibattle.c
@@ -940,7 +940,7 @@ void ui_battle_draw_item(const struct battle_s *bt, int itemi, int x, int y)
     b = &(bt->item[itemi]);
     xoff = rnd_0_nm1(5, &g->seed) - 2;
     yoff = rnd_0_nm1(5, &g->seed) - 2;
-    if (b->selected && (!bt->s[b->side].flag_auto)/* && (b->side != SIDE_R)*/) {   /* FIXME check multiplayer */
+    if (b->selected && (!bt->s[b->side].flag_auto)) {
         if (b->selected != 2/*moving*/) {
             uint8_t *gfx;
             if ((b->sx != (BATTLE_AREA_W - 1)) && (!d->flag_scanning)) {
