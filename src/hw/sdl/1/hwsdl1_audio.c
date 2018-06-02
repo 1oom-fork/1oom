9983a86fe79615a209c82dd3bbb204f8f09af095
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-02 12:56:08 +0300

    SDL: init SFX in parallel

--- src/hw/sdl/1/hwsdl1_audio.c
+++ src/hw/sdl/1/hwsdl1_audio.c
@@ -5,6 +5,8 @@
 #include "SDL.h"
 #include "SDL_mixer.h"
 #include "SDL_rwops.h"
+#define USE_SFX_INIT_THREAD
+#define HWSDLX_CreateThread(_func_) SDL_CreateThread(_func_, 0)
 #endif /* HAVE_SDL1MIXER */
 
 #include "hwsdl_audio.c"
--- src/hw/sdl/2/hwsdl2_audio.c
+++ src/hw/sdl/2/hwsdl2_audio.c
@@ -5,6 +5,8 @@
 #include "SDL.h"
 #include "SDL_mixer.h"
 #include "SDL_rwops.h"
+#define USE_SFX_INIT_THREAD
+#define HWSDLX_CreateThread(_func_) SDL_CreateThread(_func_, "SFX init", 0)
 #endif /* HAVE_SDL2MIXER */
 
 #include "hwsdl_audio.c"
