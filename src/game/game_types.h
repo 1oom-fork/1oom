8d29a05b61bcc5a26ce153f50b9daf8184012366
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-03 11:42:52 +0300

    Naming: GAME_EVENT_13 -> GAME_EVENT_ENVIRO

--- src/game/game_types.h
+++ src/game/game_types.h
@@ -119,7 +119,7 @@ typedef enum {
     GAME_EVENT_REBELLION, /*10*/
     GAME_EVENT_CRYSTAL, /*11*/
     GAME_EVENT_AMOEBA, /*12*/
-    GAME_EVENT_13, /*13*/
+    GAME_EVENT_ENVIRO, /*13*/
     GAME_EVENT_14, /*14*/
     GAME_EVENT_15, /*15*/
     GAME_EVENT_16, /*16*/
