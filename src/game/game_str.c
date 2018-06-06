15092d10cdc55ebd092a6da003187b328f3b0700
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-06 17:54:28 +0300

    uiextra: new New Game screen for multiplayer

--- src/game/game_str.c
+++ src/game/game_str.c
@@ -41,6 +41,11 @@ const char *game_str_ng_choose_race = "Choose Race";
 const char *game_str_ng_choose_banner = "Choose Banner";
 const char *game_str_ng_your_name = "Your Name...";
 const char *game_str_ng_home_name = "Home World...";
+const char *game_str_ng_ai = "AI";
+const char *game_str_ng_player = "Player";
+const char *game_str_ng_cancel = "Cancel";
+const char *game_str_ng_ok = "Start Game";
+const char *game_str_ng_allai = "Must have at least one human player!";
 
 const char *game_str_tbl_planet_names[PLANET_NAMES_MAX] = {
     "Paranar", "Denubius", "Antares", "Draconis", "Zoctan", "Rigel", "Talas", "Moro", "Quayal", "Neptunus",
