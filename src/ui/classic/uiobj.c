3f3453101fcfbdd436999243cb186c2d869f04c4
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-06 13:45:54 +0300

    Allow cursor keys to select hotkeyed objects

--- src/ui/classic/uiobj.c
+++ src/ui/classic/uiobj.c
@@ -853,6 +853,11 @@ static int16_t uiobj_kbd_dir_key_dy(int diry)
     return oi;
 }
 
+static inline bool uiobj_kbd_dir_obj_ok(const uiobj_t *p)
+{
+    return ((p->type < 0xb) && (p->x0 != UIOBJ_OFFSCREEN) && ((p->type != 0xa) || p->ta.z12));
+}
+
 static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, int my)
 {
     /* FIXME this behaves like MOO1, meaning it sucks */
@@ -866,7 +871,7 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
         bool flag_found = false;
         for (int i = 1; i < uiobj_table_num; ++i) {
             p = &uiobj_tbl[i];
-            if ((p->type < 0xb) && (p->key == MOO_KEY_UNKNOWN) && ((p->type != 0xa) || p->ta.z12)) {
+            if (uiobj_kbd_dir_obj_ok(p)) {
                 flag_found = true;
                 break;
             }
@@ -885,7 +890,7 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                 }
             }
             p = &uiobj_tbl[i];
-            if ((p->type < 0xb) && (p->key == MOO_KEY_UNKNOWN) && ((p->type != 0xa) || p->ta.z12)) {
+            if (uiobj_kbd_dir_obj_ok(p)) {
                 dy = (diry < 0) ? (my - smidy(p)) : (smidy(p) - my);
                 dx = smidx(p) - mx;
                 if ((p->x0 <= mx) && (p->x1 >= mx) && ((dx < -6) || (dx > 6))) {
@@ -907,7 +912,7 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                     }
                 }
                 p = &uiobj_tbl[i];
-                if ((p->type < 0xb) && (p->key == MOO_KEY_UNKNOWN) && ((p->type != 0xa) || p->ta.z12)) {
+                if (uiobj_kbd_dir_obj_ok(p)) {
                     dy = (diry < 0) ? (my - smidy(p)) : (smidy(p) - my);
                     dx = smidx(p) - mx;
                     if ((p->x0 <= mx) && (p->x1 >= mx) && ((dx < -6) || (dx > 6))) {
@@ -944,7 +949,7 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                 }
             }
             p = &uiobj_tbl[i];
-            if ((p->type < 0xb) && (p->key == MOO_KEY_UNKNOWN) && ((p->type != 0xa) || p->ta.z12)) {
+            if (uiobj_kbd_dir_obj_ok(p)) {
                 dx = (dirx < 0) ? (mx - smidx(p)) : (smidx(p) - mx);
                 dy = smidy(p) - my;
                 if ((p->y0 <= my) && (p->y1 >= my) && ((dy < -6) || (dy > 6))) {
@@ -966,7 +971,7 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                     }
                 }
                 p = &uiobj_tbl[i];
-                if ((p->type < 0xb) && (p->key == MOO_KEY_UNKNOWN) && ((p->type != 0xa) || p->ta.z12)) {
+                if (uiobj_kbd_dir_obj_ok(p)) {
                     dx = (dirx < 0) ? (mx - smidx(p)) : (smidx(p) - mx);
                     dy = smidy(p) - my;
                     if ((p->y0 <= my) && (p->y1 >= my) && ((dy < -6) || (dy > 6))) {
@@ -1003,7 +1008,7 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                 }
             }
             p = &uiobj_tbl[i];
-            if ((p->type < 0xb) && (p->key == MOO_KEY_UNKNOWN) && ((p->type != 0xa) || p->ta.z12)) {
+            if (uiobj_kbd_dir_obj_ok(p)) {
                 dx = (dirx < 0) ? (mx - smidx(p)) : (smidx(p) - mx);
                 dy = (diry < 0) ? (my - smidy(p)) : (smidy(p) - my);
                 if ((dx < 0) || (dy < 0)) {
