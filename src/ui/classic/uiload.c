e2f29af2d22fd45ddae029f833ea752975521c14
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-06 13:36:48 +0300

    uiextra: add continue and undo to load game
    This commit has been modified to fix bugs:
    0d2f425952d56750ba827b1f992e8f45a0b4b195

--- src/ui/classic/uiload.c
+++ src/ui/classic/uiload.c
@@ -25,7 +25,7 @@
 struct load_game_data_s {
     int selected;
     int savenum;
-    int tbl_savei[NUM_SAVES];
+    int tbl_savei[NUM_ALL_SAVES];
     uint8_t *gfx_loadgame;
     uint8_t *gfx_lg_gray;
     uint8_t *gfx_lg_green;
@@ -48,16 +48,15 @@ static void free_lg_data(struct load_game_data_s *d)
 static void load_game_draw_cb(void *vptr)
 {
     struct load_game_data_s *d = vptr;
-    const int xoff = 0x76;
-    const int yoff = 0xa;
     hw_video_copy_back_from_page2();
     lbxgfx_draw_frame(0, 0, d->gfx_loadgame, UI_SCREEN_W);
     for (int i = 0; i < d->savenum; ++i) {
-        int si;
+        int si, y;
         si = d->tbl_savei[i];
-        lbxgfx_draw_frame(0x10 + xoff, 0x17 + yoff + 0x12 * si, (d->selected == i) ? d->gfx_lg_green : d->gfx_lg_gray, UI_SCREEN_W);
-        lbxfont_select(0, 1, 0, 0);
-        lbxfont_print_str_normal(0x1f + xoff, 0x19 + yoff + 0x12 * si, game_save_tbl_name[si], UI_SCREEN_W);
+        y = (si < NUM_SAVES) ? (33 + 18 * si) : (112 + 10 * si);
+        lbxgfx_draw_frame(134, y, (d->selected == i) ? d->gfx_lg_green : d->gfx_lg_gray, UI_SCREEN_W);
+        lbxfont_select(0, (d->selected == i) ? 2 : 1, 0, 0);
+        lbxfont_print_str_normal(149, y + 2, game_save_tbl_name[si], UI_SCREEN_W);
     }
 }
 
@@ -67,12 +66,10 @@ int ui_load_game(void)
 {
     struct load_game_data_s d;
     bool flag_done = false, flag_fadein = false;
-    const int xoff = 0x76;
-    const int yoff = 0xa;
-    int16_t oi_esc, oi_cancel, oi_ok, oi_save[NUM_SAVES];
+    int16_t oi_cancel, oi_ok, oi_save[NUM_ALL_SAVES];
 
     d.savenum = 0;
-    for (int i = 0; i < NUM_SAVES; ++i) {
+    for (int i = 0; i < (ui_extra_enabled ? NUM_ALL_SAVES : NUM_SAVES); ++i) {
         if (game_save_tbl_have_save[i]) {
             d.tbl_savei[d.savenum++] = i;
         }
@@ -88,15 +85,15 @@ int ui_load_game(void)
     lbxpal_select(2, -1, 0);
 
     uiobj_table_clear();
-    oi_esc = uiobj_add_inputkey(MOO_KEY_ESCAPE);
-    oi_cancel = uiobj_add_mousearea(0x14 + xoff, 0x87 + yoff, 0x4a + xoff, 0x98 + yoff, MOO_KEY_LEFT);
-    oi_ok =     uiobj_add_mousearea(0x54 + xoff, 0x87 + yoff, 0x8a + xoff, 0x98 + yoff, MOO_KEY_SPACE);
+    oi_cancel = uiobj_add_mousearea(138, 145, 192, 161, MOO_KEY_ESCAPE);
+    oi_ok = uiobj_add_mousearea(202, 145, 256, 161, MOO_KEY_SPACE);
     uiobj_set_focus(oi_ok);
 
     for (int i = 0; i < d.savenum; ++i) {
-        int y0;
-        y0 = 0x15 + yoff + d.tbl_savei[i] * 0x12;
-        oi_save[i] = uiobj_add_mousearea(0xc + xoff, y0, 0x92 + xoff, y0 + 0xe, MOO_KEY_UNKNOWN);
+        int si, y0;
+        si = d.tbl_savei[i];
+        y0 = (si < NUM_SAVES) ? (31 + 18 * si) : (110 + 10 * si);
+        oi_save[i] = uiobj_add_mousearea(130, y0, 264, y0 + 14, MOO_KEY_1 + si);
     }
 
     d.selected = 0;
@@ -115,8 +112,8 @@ int ui_load_game(void)
                 ui_sound_play_sfx_24();
             }
         }
-        if ((oi == oi_cancel) || (oi == oi_esc) || (oi == UIOBJI_ESC)) {
-            ui_sound_play_sfx(0x06);
+        if ((oi == oi_cancel) || (oi == UIOBJI_ESC)) {
+            ui_sound_play_sfx_06();
             d.selected = -1;
             flag_done = true;
         }
