dc01458954f51852ed243fdb65447512f982bc4d
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-09 17:28:05 +0300

    Support mouse wheel for sliders

--- src/mouse.h
+++ src/mouse.h
@@ -11,6 +11,7 @@ extern int moo_mouse_y;
 extern int moo_mouse_w;
 extern int moo_mouse_h;
 extern int mouse_buttons;
+extern int mouse_scroll;
 extern int mouse_stored_x;
 extern int mouse_stored_y;
 extern int mouse_click_x;
@@ -20,6 +21,7 @@ extern int mouse_click_buttons;
 extern void mouse_set_limits(int w, int h);
 extern void mouse_set_xy_from_hw(int mx, int my);
 extern void mouse_set_buttons_from_hw(int buttons);
+extern void mouse_set_scroll_from_hw(int scroll);
 extern void mouse_set_click_xy(int mx, int my);
 extern void mouse_set_xy(int mx, int my);
 
