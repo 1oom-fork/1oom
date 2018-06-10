c7e762ca5db139176e1725faaf153997d66a0b84
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-10 11:24:37 +0300

    SETRANGE to min if max <= min, fixes uiscale 3 small galaxy

--- src/comp.h
+++ src/comp.h
@@ -14,7 +14,7 @@
 #define SETMAX(a, b) do { if ((b) > (a)) { (a) = (b); }} while (0)
 #endif
 #ifndef SETRANGE
-#define SETRANGE(a, b, c) do { if ((b) > (a)) { (a) = (b); } else if ((c) < (a)) { (a) = (c); } } while (0)
+#define SETRANGE(a, b, c) do { if (((c) <= (b)) || ((b) > (a))) { (a) = (b); } else if ((c) < (a)) { (a) = (c); } } while (0)
 #endif
 #define ADDSATT(_v_, _n_, _top_) do { int _t_; _t_ = (_v_) + (_n_); SETMIN(_t_, (_top_)); (_v_) = _t_; } while (0)
 #define SUBSATT(_v_, _n_, _bot_) do { int _t_; _t_ = (_v_) - (_n_); SETMAX(_t_, (_bot_)); (_v_) = _t_; } while (0)
