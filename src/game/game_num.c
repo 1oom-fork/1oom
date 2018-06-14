fc803b469e6e060d830f8de8564813ed33f497cd
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-14 21:45:00 +0300

    PBX num for disabling Wait special reload

--- src/game/game_num.c
+++ src/game/game_num.c
@@ -10,6 +10,7 @@
 /* -------------------------------------------------------------------------- */
 
 int game_num_bt_turn_max = 50;
+bool game_num_bt_wait_no_reload = false;
 int game_num_stargate_cost = 3000;
 int game_num_weapon_list_max = 30;
 unsigned int game_num_limit_ships = 32000;
