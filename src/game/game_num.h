d55dabf681ccfe3e1133eb883e0b9d1321512ceb
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-14 17:15:50 +0300

    PBX num for Orion conquer news, cleanup

--- src/game/game_num.h
+++ src/game/game_num.h
@@ -26,6 +26,7 @@ extern int game_num_max_ship_maint;
 extern int game_num_max_tribute_bc;
 extern int game_num_event_roll;
 extern int game_num_council_years;
+extern bool game_num_news_orion;
 extern bool game_num_monster_rest_att;
 extern bool game_num_orbital_weap_any;
 extern bool game_num_orbital_weap_4;
