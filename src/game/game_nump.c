2fb437b950d67ac8882a6379068e0625b661b62d
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-14 17:15:50 +0300

    PBX num for Orion conquer news, cleanup

--- src/game/game_nump.c
+++ src/game/game_nump.c
@@ -174,6 +174,7 @@ static const struct numtbl_s {
     DEFNUMITEML(max_ship_maint, NUMTYPE_S, 1, 0x7fffffff),
     DEFNUMITEML(max_tribute_bc, NUMTYPE_S, 1, 0x7fffffff),
     DEFNUMITEML(event_roll, NUMTYPE_S, 0, 0x7fffffff),
+    DEFNUMITEML(news_orion, NUMTYPE_BOOL, 0, 1),
     DEFNUMITEML(monster_rest_att, NUMTYPE_BOOL, 0, 1),
     DEFNUMITEML(orbital_weap_any, NUMTYPE_BOOL, 0, 1),
     DEFNUMITEML(orbital_weap_4, NUMTYPE_BOOL, 0, 1),
