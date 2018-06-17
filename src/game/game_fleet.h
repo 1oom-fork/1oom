d65500812683666aeaeed6495d13a8882bea4933
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-17 22:55:28 +0300

    PBX num for star gate redirection fix

--- src/game/game_fleet.h
+++ src/game/game_fleet.h
@@ -6,6 +6,9 @@
 
 struct game_s;
 struct planet_s;
+struct fleet_enroute_s;
+
+#define FLEET_SPEED_STARGATE    35
 
 extern bool game_send_fleet_from_orbit(struct game_s *g, player_id_t owner, uint8_t from, uint8_t dest, const shipcount_t ships[NUM_SHIPDESIGNS], const uint8_t shiptypes[NUM_SHIPDESIGNS], uint8_t numtypes);
 extern bool game_send_fleet_reloc(struct game_s *g, player_id_t owner, uint8_t from, uint8_t dest, uint8_t si, shipcount_t shipnum);
@@ -14,5 +17,7 @@ extern void game_remove_empty_fleets(struct game_s *g);
 extern void game_remove_player_fleets(struct game_s *g, player_id_t owner);
 extern bool game_fleet_any_dest_player(const struct game_s *g, player_id_t owner, player_id_t target);
 extern void game_fleet_unrefuel(struct game_s *g);
+extern uint8_t game_fleet_get_speed(const struct game_s *g, const struct fleet_enroute_s *r, uint8_t pfrom, uint8_t pto);
+extern void game_fleet_redirect(struct game_s *g, struct fleet_enroute_s *r, uint8_t pfrom, uint8_t pto);
 
 #endif
