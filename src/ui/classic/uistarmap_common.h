788318854865df6c0466080ed1174abcc4dcf38e
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-17 22:55:28 +0300

    PBX num for star gate redirection fix

--- src/ui/classic/uistarmap_common.h
+++ src/ui/classic/uistarmap_common.h
@@ -106,6 +106,7 @@ struct starmap_data_s {
             struct shipnon0_s sn0;
             bool in_frange;
             uint8_t from;
+            uint8_t pon;
             can_move_t can_move;
             int frame_ship;
             int frame_scanner;
