f8ed8dfb639830a4c75a3625afdab45723a2209f
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-19 22:37:11 +0300

    Fix F8/F9

--- src/ui/classic/uistarmap_common.h
+++ src/ui/classic/uistarmap_common.h
@@ -182,6 +182,7 @@ extern void ui_starmap_draw_planetinfo(const struct game_s *g, player_id_t api, 
 extern void ui_starmap_draw_planetinfo_2(const struct game_s *g, int p1, int p2, int planet_i);
 extern int ui_starmap_newship_next(const struct game_s *g, player_id_t pi, int i);
 extern int ui_starmap_newship_prev(const struct game_s *g, player_id_t pi, int i);
+extern int ui_starmap_enemy_incoming(const struct game_s *g, player_id_t pi, int i, bool next);
 extern void ui_starmap_scroll(const struct game_s *g, int scrollx, int scrolly);
 
 #endif
