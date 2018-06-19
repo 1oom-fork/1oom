5ef763a82dffc6826cd3ffb90cefaa420eba1c72
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-19 23:46:45 +0300

    PBX num for transport redirect cheat fix

--- src/game/game_num.c
+++ src/game/game_num.c
@@ -40,6 +40,7 @@ bool game_num_news_orion = false;
 bool game_num_aud_bounty_give = false;
 bool game_num_combat_trans_fix = false;
 bool game_num_stargate_redir_fix = false;
+bool game_num_trans_redir_fix = false;
 
 uint8_t game_num_tbl_hull_w[4] = { 1, 5, 25, 125 };
 
