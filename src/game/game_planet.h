a4299bbccdd52308f75671f36c8a6e019279d60b
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-20 01:05:22 +0300

    <PART> Fix new tech slider autoadjust

--- src/game/game_planet.h
+++ src/game/game_planet.h
@@ -151,7 +151,7 @@ struct game_s;
 
 extern void game_planet_destroy(struct game_s *g, uint8_t planet_i, player_id_t attacker);
 extern uint8_t game_planet_get_random(struct game_s *g, player_id_t owner);
-extern void game_planet_adjust_percent(struct game_s *g, player_id_t owner, int a0, uint8_t percent, int growth);
+extern void game_planet_adjust_percent(struct game_s *g, player_id_t owner, planet_slider_i_t si, uint8_t percent, int growth);
 extern int game_planet_get_w1(const struct game_s *g, uint8_t planet_i);
 extern int game_adjust_prod_by_special(int prod, planet_special_t special);
 extern int game_get_tech_prod(int prod, int slider, race_t race, planet_special_t special);
