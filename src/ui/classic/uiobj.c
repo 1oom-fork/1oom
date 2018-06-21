8aa9a76182f6e1a6a9e1fb7e1bbea08e82fdd4d7
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-21 19:16:58 +0300

    Handle shifted keys better

--- src/ui/classic/uiobj.c
+++ src/ui/classic/uiobj.c
@@ -1086,11 +1086,14 @@ static int16_t uiobj_kbd_dir_key(int dirx, int diry)
 static int16_t uiobj_handle_kbd_find_alt(int16_t oi, uint32_t key)
 {
     const uiobj_t *p = &uiobj_tbl[oi];
+    mookey_t k = KBD_GET_KEY(key);
+    uint32_t kmod = KBD_GET_KEYMOD(key);
+    char c = ((k >= 0x20) && (k < 0x7e)) ? KBD_GET_CHAR(key) : 0;
     while (1
       && (oi != uiobj_table_num)
-      && (!((KBD_GET_KEYMOD(key) == p->key) && (p->type != 8)))
+      && (!((p->type != 8) && ((kmod == p->key) || (c && (c == p->key)))))
     ) {
-        if ((p->type == 8) && KBD_MOD_ONLY_ALT(key) && (KBD_GET_KEY(key) == p->key)) {
+        if ((p->type == 8) && KBD_MOD_ONLY_ALT(key) && (k == p->key)) {
             break;
         }
         ++oi;
@@ -1319,24 +1322,27 @@ static int16_t uiobj_handle_input_sub0(void)
     uiobj_mouseoff = ui_cursor_mouseoff;
     if (kbd_have_keypress()) {
         uint32_t key = uiobj_handle_kbd(&oi);
-        if (KBD_GET_KEY(key) == MOO_KEY_UNKNOWN) {
+        mookey_t k = KBD_GET_KEY(key);
+        uint32_t kmod = KBD_GET_KEYMOD(key);
+        char c = ((k >= 0x20) && (k < 0x7e)) ? KBD_GET_CHAR(key) : 0;
+        if (k == MOO_KEY_UNKNOWN) {
             return 0;
         }
         /* checks for F11 and F12 debug keys omitted */
-        if (KBD_GET_KEY(key) == MOO_KEY_F1) {
+        if (kmod == MOO_KEY_F1) {
             if (uiobj_help_id != -1) {
                 ui_help(uiobj_help_id);
             }
             return 0;
         }
-        if (KBD_GET_KEYMOD(key) == MOO_KEY_ESCAPE) {
+        if (kmod == MOO_KEY_ESCAPE) {
             return -1;
         }
         p = &uiobj_tbl[oi];
         if (p->type == 8) {
             return oi;
         }
-        if (KBD_GET_KEYMOD(key) == p->key) {
+        if ((kmod == p->key) || (c && (c == p->key))) {
             if (p->type == 6) {
                 return 0;
             }
@@ -1365,7 +1371,7 @@ static int16_t uiobj_handle_input_sub0(void)
             uiobj_focus_oi = -1;
             return oi;
         }
-        if (KBD_GET_KEY(key) == MOO_KEY_RETURN) {
+        if (k == MOO_KEY_RETURN) {
             oi = uiobj_find_obj_at_cursor();
             if (oi != 0) {
                 p = &uiobj_tbl[oi];
@@ -1400,12 +1406,12 @@ static int16_t uiobj_handle_input_sub0(void)
                 }
             }
         }
-        if ((KBD_GET_CHAR(key) == '+') || (KBD_GET_CHAR(key) == '-')) {
+        if ((c == '+') || (c == '-')) {
             oi = uiobj_find_obj_at_cursor();
             if (oi != 0) {
                 p = &uiobj_tbl[oi];
                 if (p->type == 6) {
-                    if (KBD_GET_CHAR(key) == '+') {
+                    if (c == '+') {
                         uiobj_slider_plus(p, 5);
                     } else {
                         uiobj_slider_minus(p, 5);
