1c37efc58a12565cb3fe884b3b14441e8faf35f9
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-25 15:19:26 +0300

    cmdline: implement tech, cheats, misc

--- src/ui/cmdline/uiplanet.h
+++ src/ui/cmdline/uiplanet.h
@@ -17,5 +17,6 @@ extern int ui_cmd_planet_slider_lock(struct game_s *g, int api, struct input_tok
 extern int ui_cmd_planet_build(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var);
 extern int ui_cmd_planet_reloc(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var);
 extern int ui_cmd_planet_trans(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var);
+extern int ui_cmd_planet_reserve(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var);
 
 #endif
