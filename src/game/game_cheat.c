77e50d12130c06bc92ca73c5eb25fd72f5971450
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-25 15:19:26 +0300

    cmdline: implement tech, cheats, misc

---
+++ src/game/game_cheat.c
@@ -0,0 +1,37 @@
+#include "config.h"
+
+#include "game_cheat.h"
+#include "boolvec.h"
+#include "game.h"
+#include "game_aux.h"
+#include "game_misc.h"
+#include "game_tech.h"
+
+/* -------------------------------------------------------------------------- */
+/* TODO disable for multiplayer and return false */
+
+bool game_cheat_galaxy(struct game_s *g, player_id_t pi)
+{
+    g->gaux->flag_cheat_galaxy = !g->gaux->flag_cheat_galaxy;
+    game_update_tech_util(g);
+    game_update_within_range(g);
+    game_update_visibility(g);
+    for (int i = 0; i < g->galaxy_stars; ++i) {
+        BOOLVEC_SET1(g->planet[i].explored, pi);
+    }
+    return true;
+}
+
+bool game_cheat_events(struct game_s *g, player_id_t pi)
+{
+    g->gaux->flag_cheat_events = !g->gaux->flag_cheat_events;
+    return true;
+}
+
+bool game_cheat_moola(struct game_s *g, player_id_t pi)
+{
+    empiretechorbit_t *e = &(g->eto[pi]);
+    e->reserve_bc += 100;
+    game_update_production(g);
+    return true;
+}
