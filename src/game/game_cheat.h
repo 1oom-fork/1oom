7962ba5ea8476fb1a4058a1d3974fd50b843ec8d
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-25 15:19:26 +0300

    cmdline: implement tech, cheats, misc

---
+++ src/game/game_cheat.h
@@ -0,0 +1,13 @@
+#ifndef INC_1OOM_GAME_CHEAT_H
+#define INC_1OOM_GAME_CHEAT_H
+
+#include "game_types.h"
+#include "types.h"
+
+struct game_s;
+
+extern bool game_cheat_galaxy(struct game_s *g, player_id_t pi);
+extern bool game_cheat_events(struct game_s *g, player_id_t pi);
+extern bool game_cheat_moola(struct game_s *g, player_id_t pi);
+
+#endif
