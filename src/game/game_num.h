931d89858939d7f38da6026ad2bb4536d5248e23
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-27 14:47:36 +0300

    PBX nums for To Hit pre-cap and accumulate fixes

--- src/game/game_num.h
+++ src/game/game_num.h
@@ -6,6 +6,8 @@
 
 extern int game_num_bt_turn_max;
 extern bool game_num_bt_wait_no_reload;
+extern bool game_num_bt_precap_tohit;
+extern bool game_num_bt_no_tohit_acc;
 extern int game_num_stargate_cost;
 extern int game_num_weapon_list_max;
 extern unsigned int game_num_limit_ships;
