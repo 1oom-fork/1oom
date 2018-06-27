0ff7ecfa3cf7228f59360ebbb60c1bf9c970e71b
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-27 12:42:58 +0300

    Fix 1oom typos

--- src/hw/sdl/1/hwsdl1_video.c
+++ src/hw/sdl/1/hwsdl1_video.c
@@ -227,7 +227,7 @@ int hw_video_resize(int w, int h)
     log_message("SDL_SetVideoMode(%i, %i, %i, 0x%x)\n", actual_w, actual_h, 32, flags);
     video.screen = SDL_SetVideoMode(actual_w, actual_h, 32, flags);
     if (!video.screen) {
-        log_error("SDL_SetVideoMode failed!");
+        log_error("SDL_SetVideoMode failed!\n");
         goto fail;
     }
     set_viewport(video.hwrenderbuf->w, video.hwrenderbuf->h, actual_w, actual_h);
@@ -288,11 +288,11 @@ int hw_video_init(int w, int h)
         log_message("SDL_CreateRGBSurface(...)\n");
         video.hwrenderbuf = SDL_CreateRGBSurface(SDL_SWSURFACE, w, h, 32, rmask, gmask, bmask, amask);
         if (!video.hwrenderbuf) {
-            log_error("SDL_CreateRGBSurface failed!");
+            log_error("SDL_CreateRGBSurface failed!\n");
             return -1;
         }
         if ((video.hwrenderbuf->pitch % sizeof(Uint32)) != 0) {
-            log_warning("SDL renderbuf pitch mod %i == %i", sizeof(Uint32), video.hwrenderbuf->pitch);
+            log_warning("SDL renderbuf pitch mod %i == %i\n", sizeof(Uint32), video.hwrenderbuf->pitch);
         }
         i_hw_video.setmode = hw_video_resize;
         i_hw_video.render = video_render_gl_32bpp;
