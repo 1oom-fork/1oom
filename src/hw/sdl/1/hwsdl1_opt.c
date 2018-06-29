a94436b06d007378a81590d71680949319781b72
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-29 10:36:13 +0300

    SDL1: support 15/16/24bpp

--- src/hw/sdl/1/hwsdl1_opt.c
+++ src/hw/sdl/1/hwsdl1_opt.c
@@ -16,6 +16,7 @@
 
 #ifdef HAVE_SDL1GL
 int hw_opt_gl_filter = 1;
+int hw_opt_bpp = 0;
 #define HAVE_SDLX_ASPECT
 #endif /* HAVE_SDL1GL */
 
@@ -27,6 +28,7 @@ int hw_opt_gl_filter = 1;
 
 const struct cfg_items_s hw_cfg_items_extra[] = {
 #ifdef HAVE_SDL1GL
+    CFG_ITEM_INT("bpp", &hw_opt_bpp, 0),
     CFG_ITEM_INT("filter", &hw_opt_gl_filter, 0),
 #endif /* HAVE_SDL1GL */
     CFG_ITEM_END
@@ -36,6 +38,9 @@ const struct cfg_items_s hw_cfg_items_extra[] = {
 
 const struct cmdline_options_s hw_cmdline_options_extra[] = {
 #ifdef HAVE_SDL1GL
+    { "-bpp", 1,
+      options_set_int_var, (void *)&hw_opt_bpp,
+      "BPP", "Set bits/pixel (0 = autodetect)" },
     { "-filt", 1,
       options_set_int_var, (void *)&hw_opt_gl_filter,
       "FILTER", "Set OpenGL filter (0 = nearest, 1 = linear)" },
--- src/hw/sdl/hwsdl_opt.h
+++ src/hw/sdl/hwsdl_opt.h
@@ -12,6 +12,7 @@ extern int hw_opt_force_sw;
 
 /* for SDL1 */
 extern int hw_opt_gl_filter;
+extern int hw_opt_bpp;
 
 /* for SDL2 */
 extern bool hw_opt_int_scaling;
