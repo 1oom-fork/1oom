0c9f017d56b2a449dfd46df70484df5596bbb547
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-06-30 07:45:54 +0300

    SDL2: add -norelmouse for testing

--- src/hw/sdl/2/hwsdl2_opt.c
+++ src/hw/sdl/2/hwsdl2_opt.c
@@ -17,6 +17,7 @@
 
 bool hw_opt_borderless = false;
 bool hw_opt_int_scaling = false;
+bool hw_opt_relmouse = true;
 bool hw_opt_vsync = true;
 
 #ifdef HAVE_SDL2MIXER
@@ -28,6 +29,7 @@ bool hw_opt_vsync = true;
 const struct cfg_items_s hw_cfg_items_extra[] = {
     CFG_ITEM_BOOL("borderless", &hw_opt_borderless),
     CFG_ITEM_BOOL("int_scaling", &hw_opt_int_scaling),
+    CFG_ITEM_BOOL("relmouse", &hw_opt_relmouse),
     CFG_ITEM_BOOL("vsync", &hw_opt_vsync),
     CFG_ITEM_END
 };
@@ -47,6 +49,12 @@ const struct cmdline_options_s hw_cmdline_options_extra[] = {
     { "-nointscaling", 0,
       options_disable_bool_var, (void *)&hw_opt_int_scaling,
       NULL, "Do not force integer scaling" },
+    { "-relmouse", 0,
+      options_enable_bool_var, (void *)&hw_opt_relmouse,
+      NULL, "Use relative mouse mode (default)" },
+    { "-norelmouse", 0,
+      options_disable_bool_var, (void *)&hw_opt_relmouse,
+      NULL, "Do not use relative mouse mode (for testing)" },
     { "-vsync", 0,
       options_enable_bool_var, &hw_opt_vsync,
       NULL, "Enable V-sync" },
