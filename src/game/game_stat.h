b90a2486040ab1cb0cda3c82dc5e32f31c154683
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-03 15:56:47 +0300

    cmdline: scrap, specs, races, stats, spy

--- src/game/game_stat.h
+++ src/game/game_stat.h
@@ -11,4 +11,12 @@ extern int game_stat_prod(const struct game_s *g, player_id_t pi);
 extern int game_stat_pop(const struct game_s *g, player_id_t pi);
 extern int game_stat_planets(const struct game_s *g, player_id_t pi);
 
+struct game_stats_s {
+    player_id_t p[PLAYER_NUM];
+    uint8_t v[6][PLAYER_NUM];
+    int num;
+};
+
+extern void game_stats_all(struct game_s *g, player_id_t api, struct game_stats_s *d);
+
 #endif
