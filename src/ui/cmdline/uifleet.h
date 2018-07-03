d53e9abe9c4dcaea75fa328939451e9a32ded463
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-03 15:56:47 +0300

    cmdline: scrap, specs, races, stats, spy

--- src/ui/cmdline/uifleet.h
+++ src/ui/cmdline/uifleet.h
@@ -7,5 +7,6 @@ struct game_s;
 struct input_token_s;
 
 extern int ui_cmd_fleet_send(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var);
+extern int ui_cmd_fleet_scrap(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var);
 
 #endif
