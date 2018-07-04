9c95a06f1caf3df1d4daedb02571fb87b32437ed
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-04 12:28:40 +0300

    PBX num for disallowing retreat redirection back to battle

--- src/ui/classic/uistarmap_enroute.c
+++ src/ui/classic/uistarmap_enroute.c
@@ -116,10 +116,13 @@ static void ui_starmap_enroute_draw_cb(void *vptr)
     } else {
         d->en.scanner_delay = 0;
     }
-    if ((r->owner == d->api) && (d->en.can_move != NO_MOVE) && (!d->en.in_frange)) {
+    if (1
+      && (r->owner == d->api)
+      && (d->en.can_move != NO_MOVE)
+      && ((!d->en.in_frange) || (game_num_retreat_redir_fix && (d->en.can_move != GOT_HYPERCOMM) && (pto == d->en.pon)))
+    ) {
         lbxgfx_set_new_frame(ui_data.gfx.starmap.reloc_bu_accept, 1);
         lbxgfx_draw_frame(271, 163, ui_data.gfx.starmap.reloc_bu_accept, UI_SCREEN_W, ui_scale);
-
     }
     d->en.frame_ship = (d->en.frame_ship + 1) % 5;
     ui_draw_set_stars_xoffs(&d->en.ds, false);
@@ -146,7 +149,9 @@ void ui_starmap_enroute(struct game_s *g, player_id_t active_player)
         const planet_t *p;
         p = &g->planet[i];
         if ((p->x == r->x) && (p->y == r->y)) {
-            d.en.can_move = ON_PLANET;
+            if (d.en.can_move == NO_MOVE) {
+                d.en.can_move = ON_PLANET;
+            }
             d.en.pon = i;
             break;
         }
@@ -307,7 +312,7 @@ do_accept:
             ui_starmap_fill_oi_tbl_stars(&d);
             if ((r->owner == active_player) && (d.en.can_move != NO_MOVE)) {
                 oi_cancel = uiobj_add_t0(227, 163, "", ui_data.gfx.starmap.reloc_bu_cancel, MOO_KEY_ESCAPE);
-                if (d.en.in_frange) {
+                if (d.en.in_frange && ((!game_num_retreat_redir_fix) || (d.en.can_move == GOT_HYPERCOMM) || (g->planet_focus_i[active_player] != d.en.pon))) {
                     oi_accept = uiobj_add_t0(271, 163, "", ui_data.gfx.starmap.reloc_bu_accept, MOO_KEY_SPACE);
                 }
             }
