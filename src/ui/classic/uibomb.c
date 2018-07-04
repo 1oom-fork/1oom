d5af5db7ca393be39f38cd0e47a784b415812699
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-04 12:49:03 +0300

    Stop instead of fade music in most cases

--- src/ui/classic/uibomb.c
+++ src/ui/classic/uibomb.c
@@ -215,7 +215,7 @@ bool ui_bomb_ask(struct game_s *g, int pi, uint8_t planet_i, int pop_inbound)
     }
     uiobj_unset_callback();
     uiobj_table_clear();
-    hw_audio_music_fadeout();
+    ui_sound_stop_music();
     bomb_free_data(&d);
     return flag_do_bomb;
 }
@@ -254,6 +254,6 @@ void ui_bomb_show(struct game_s *g, int attacker_i, int owner_i, uint8_t planet_
     }
     uiobj_unset_callback();
     uiobj_table_clear();
-    hw_audio_music_fadeout();
+    ui_sound_stop_music();
     bomb_free_data(&d);
 }
