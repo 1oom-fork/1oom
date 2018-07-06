ec60b62ce03f20cd851e3f65df18bd872ed124d3
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-06 15:07:12 +0300

    Clicking cycles selection of overlapping fleets

--- src/ui/classic/uistarmap_transport.c
+++ src/ui/classic/uistarmap_transport.c
@@ -291,7 +291,7 @@ void ui_starmap_transport(struct game_s *g, player_id_t active_player)
             uiobj_table_clear();
             UIOBJ_CLEAR_LOCAL();
             /* uiobj_set_limits(STARMAP_LIMITS); */
-            ui_starmap_fill_oi_tbls(&d);
+            ui_starmap_fill_oi_tbls(&d, true);
             ui_starmap_fill_oi_tbl_stars(&d);
             if ((r->owner == active_player) && (d.ts.can_move != NO_MOVE)) {
                 oi_cancel = uiobj_add_t0(227, 163, "", ui_data.gfx.starmap.reloc_bu_cancel, MOO_KEY_ESCAPE);
