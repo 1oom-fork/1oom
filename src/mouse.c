71daa98f5ca45806279fef80fa5afca04ef74a6e
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-10 18:54:37 +0300

    Naming: mouse_getclear_hmm -> mouse_getclear_click

--- src/mouse.c
+++ src/mouse.c
@@ -97,14 +97,14 @@ int mouse_get_click_y(void)
     return moo_mouse_click_y;
 }
 
-bool mouse_getclear_hmm4(void)
+bool mouse_getclear_click_hw(void)
 {
     bool r = mouse_hmm4;
     mouse_hmm4 = false;
     return r;
 }
 
-bool mouse_getclear_hmm5(void)
+bool mouse_getclear_click_sw(void)
 {
     bool r = mouse_hmm5;
     mouse_hmm5 = false;
