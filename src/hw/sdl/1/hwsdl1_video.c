cd33ddfd724c35677dd847aeafb5c0b0c83842be
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-13 22:55:47 +0300

    Refactor hw_video_toggle_fullscreen/hw_audio_xx_volume

--- src/hw/sdl/1/hwsdl1_video.c
+++ src/hw/sdl/1/hwsdl1_video.c
@@ -252,7 +252,7 @@ int hw_video_resize(int w, int h)
 #endif
 }
 
-int hw_video_toggle_fullscreen(void)
+bool hw_video_toggle_fullscreen(void)
 {
     int (*func)(int, int) = video_sw_set;
 #ifdef HAVE_SDL1GL
@@ -263,7 +263,7 @@ int hw_video_toggle_fullscreen(void)
     hw_opt_fullscreen = !hw_opt_fullscreen;
     if (func(hw_opt_screen_winw, hw_opt_screen_winh)) {
         hw_opt_fullscreen = !hw_opt_fullscreen; /* restore the setting for the config file */
-        return -1;
+        return false;
     }
 #ifdef HAVE_SDL1GL
     if (hw_opt_force_sw)
@@ -271,7 +271,7 @@ int hw_video_toggle_fullscreen(void)
     {
         hw_video_refresh_palette();
     }
-    return 0;
+    return true;
 }
 
 int hw_video_init(int w, int h)
