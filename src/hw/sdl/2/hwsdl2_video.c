9466a91179bab7d6fc998362cf340fc54c4f8281
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-13 22:55:47 +0300

    Refactor hw_video_toggle_fullscreen/hw_audio_xx_volume

--- src/hw/sdl/2/hwsdl2_video.c
+++ src/hw/sdl/2/hwsdl2_video.c
@@ -465,7 +465,7 @@ int hw_video_resize(int w, int h)
     return 0;
 }
 
-int hw_video_toggle_fullscreen(void)
+bool hw_video_toggle_fullscreen(void)
 {
     unsigned int flags = 0;
 
@@ -483,7 +483,7 @@ int hw_video_toggle_fullscreen(void)
     if (!hw_opt_fullscreen) {
         hw_video_resize(0, 0);
     }
-    return 0;
+    return true;
 }
 
 int hw_video_init(int w, int h)
