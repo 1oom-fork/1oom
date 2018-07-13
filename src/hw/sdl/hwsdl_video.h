b5e9c6b2df249ad440629b63b55319c76e31de12
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-13 22:55:47 +0300

    Refactor hw_video_toggle_fullscreen/hw_audio_xx_volume

--- src/hw/sdl/hwsdl_video.h
+++ src/hw/sdl/hwsdl_video.h
@@ -5,7 +5,7 @@ extern void hw_video_shutdown(void);
 extern void hw_video_update(void);
 extern void hw_video_refresh(int front);
 extern int hw_video_resize(int w, int h);
-extern int hw_video_toggle_fullscreen(void);
+extern bool hw_video_toggle_fullscreen(void);
 extern void hw_video_input_grab(bool grab);
 
 #endif
