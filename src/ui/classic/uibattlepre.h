bd1730d6432256e2a47a8aff613655b8e0564263
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-14 23:26:18 +0300

    Add Retreat option for autoresolve

--- src/ui/classic/uibattlepre.h
+++ src/ui/classic/uibattlepre.h
@@ -1,11 +1,12 @@
 #ifndef INC_1OOM_UIBATTLEPRE_H
 #define INC_1OOM_UIBATTLEPRE_H
 
+#include "ui.h"
 #include "types.h"
 
 struct game_s;
 struct battle_s;
 
-extern bool ui_battle_pre(struct game_s *g, const struct battle_s *bt, bool hide_other, int winner);
+extern ui_battle_autoresolve_t ui_battle_pre(struct game_s *g, const struct battle_s *bt, bool hide_other, int winner);
 
 #endif
