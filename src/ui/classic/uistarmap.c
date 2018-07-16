f7177fdb7abbc625a86e4ad1178036beda4bbfbf
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-16 14:09:30 +0300

    Cycle overlapped transports too

--- src/ui/classic/uistarmap.c
+++ src/ui/classic/uistarmap.c
@@ -631,7 +631,7 @@ void ui_starmap_do(struct game_s *g, player_id_t active_player)
                 oi_shippic = uiobj_add_mousearea(228, 139, 275, 175, MOO_KEY_UNKNOWN);
                 oi_wheelshippic = uiobj_add_mousewheel(228, 139, 275, 175, &scrollmisc);
             }
-            ui_starmap_fill_oi_tbls(&d, true);
+            ui_starmap_fill_oi_tbls(&d);
             {
                 int x0, y0;
                 x0 = (p->x - ui_data.starmap.x) * 2 + 6;
