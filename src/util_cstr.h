665582150ef1232078dff6d1d032ff34d92aeb43
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-22 09:11:00 +0300

    New lbxedit tool

--- src/util_cstr.h
+++ src/util_cstr.h
@@ -2,7 +2,9 @@
 #define INC_1OOM_UTIL_CSTR_H
 
 #include <stdio.h>
+#include "types.h"
 
+extern int util_cstr_parse(const char *str, char *dst, uint32_t *len_out);
 extern int util_cstr_out(FILE *fd, const char *str);
 extern int util_cstr_parse_in_place(char *str);
 
