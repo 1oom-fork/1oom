5c6f8a352e8925245c0a293a63a6bc27c38734be
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-23 15:51:41 +0300

    cmdline: fleet, redir

--- src/ui/cmdline/uifleet.h
+++ src/ui/cmdline/uifleet.h
@@ -6,6 +6,8 @@
 struct game_s;
 struct input_token_s;
 
+extern int ui_cmd_fleet_list(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var);
+extern int ui_cmd_fleet_redir(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var);
 extern int ui_cmd_fleet_send(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var);
 extern int ui_cmd_fleet_scrap(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var);
 
