62f4ef5c5972863a126dc9b00cb54480bd6d015c
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-23 15:51:41 +0300

    cmdline: fleet, redir

--- src/ui/cmdline/uifleet.c
+++ src/ui/cmdline/uifleet.c
@@ -1,5 +1,6 @@
 #include "config.h"
 
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -9,6 +10,7 @@
 #include "game.h"
 #include "game_fleet.h"
 #include "game_misc.h"
+#include "game_num.h"
 #include "game_str.h"
 #include "uidefs.h"
 #include "uiinput.h"
@@ -17,7 +19,204 @@
 
 /* -------------------------------------------------------------------------- */
 
+static uint8_t get_fleet_planet_on(const struct game_s *g, const fleet_enroute_t *r)
+{
+    for (int i = 0; i < g->galaxy_stars; ++i) {
+        const planet_t *p;
+        p = &g->planet[i];
+        if ((p->x == r->x) && (p->y == r->y)) {
+            return i;
+        }
+    }
+    return PLANET_NONE;
+}
+
+static void print_fleet_enroute(const struct game_s *g, int api, const fleet_enroute_t *r, uint8_t pon)
+{
+    char buf[80];
+    char pname[20];
+    const empiretechorbit_t *e = &(g->eto[api]);
+    int eta, sd_num = e->shipdesigns_num;
+    const planet_t *p = &(g->planet[r->dest]);
+    sprintf(buf, "#F%i (%i,%i) %s [%i] %s", (int)(r - g->enroute), r->x, r->y, game_str_fl_moving, r->dest, ui_planet_str(g, api, r->dest, pname));
+    printf("%-40s", buf);
+    for (int k = 0; k < sd_num; ++k) {
+        printf(" %5i", r->ships[k]);
+    }
+    if (pon == PLANET_NONE) {
+        pon = get_fleet_planet_on(g, r);
+    }
+    eta = game_calc_eta(g, game_fleet_get_speed(g, r, pon, r->dest), p->x, p->y, r->x, r->y);
+    printf("  %s %i %s\n", game_str_sm_eta, eta, (eta == 1) ? game_str_sm_turn : game_str_sm_turns);
+}
+
+static void print_transport_enroute(const struct game_s *g, int api, const transport_t *r)
+{
+    char buf[80];
+    char pname[20];
+    const empiretechorbit_t *e = &(g->eto[api]);
+    int eta;
+    const planet_t *p = &(g->planet[r->dest]);
+    sprintf(buf, "#T%i (%i,%i) %s [%i] %s", (int)(r - g->transport), r->x, r->y, game_str_fl_moving, r->dest, ui_planet_str(g, api, r->dest, pname));
+    printf("%-40s", buf);
+    printf(" %5i", r->pop);
+    eta = game_calc_eta(g, e->have_engine, p->x, p->y, r->x, r->y);
+    printf("  %s %i %s\n", game_str_sm_eta, eta, (eta == 1) ? game_str_sm_turn : game_str_sm_turns);
+}
+
 /* -------------------------------------------------------------------------- */
+
+int ui_cmd_fleet_list(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var)
+{
+    char buf[80];
+    const empiretechorbit_t *e = &(g->eto[api]);
+    int sd_num = e->shipdesigns_num;
+    {
+        const shipdesign_t *sd = &(g->srd[api].design[0]);
+        for (int j = 0; j < sd_num; ++j) {
+            printf("%c %s", (j == 0) ? '-' : ',', sd[j].name);
+        }
+        putchar('\n');
+    }
+    for (int i = 0; i < g->galaxy_stars; ++i) {
+        const fleet_orbit_t *r = &(e->orbit[i]);
+        for (int j = 0; j < sd_num; ++j) {
+            if (r->ships[j] != 0) {
+                const planet_t *p;
+                p = &(g->planet[i]);
+                sprintf(buf, "%s [%i] (%i,%i) %s", game_str_fl_inorbit, i, p->x, p->y, p->name);
+                printf("%-40s", buf);
+                for (int k = 0; k < sd_num; ++k) {
+                    printf(" %5i", r->ships[k]);
+                }
+                putchar('\n');
+                break;
+            }
+        }
+    }
+    for (int i = 0; i < g->enroute_num; ++i) {
+        const fleet_enroute_t *r = &(g->enroute[i]);
+        if (r->owner == api) {
+            for (int j = 0; j < sd_num; ++j) {
+                if (r->ships[j] != 0) {
+                    print_fleet_enroute(g, api, r, PLANET_NONE);
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int ui_cmd_fleet_redir(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var)
+{
+    const empiretechorbit_t *e = &(g->eto[api]);
+    int sd_num = e->shipdesigns_num;
+    if (num_param == 0) {
+        for (int i = 0; i < g->enroute_num; ++i) {
+            const fleet_enroute_t *r = &(g->enroute[i]);
+            if (r->owner == api) {
+                for (int j = 0; j < sd_num; ++j) {
+                    if (r->ships[j] != 0) {
+                        uint8_t pon;
+                        pon = get_fleet_planet_on(g, r);
+                        if (e->have_hyperspace_comm || (pon != PLANET_NONE)) {
+                            print_fleet_enroute(g, api, r, pon);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        if (e->have_hyperspace_comm) {
+            for (int i = 0; i < g->transport_num; ++i) {
+                const transport_t *r = &(g->transport[i]);
+                if (r->owner == api) {
+                    print_transport_enroute(g, api, r);
+                }
+            }
+        }
+    } else if (num_param == 1) {
+        return -1;
+    } else {
+        bool is_fleet;
+        int dist, n = -1;
+        uint8_t dest;
+        const planet_t *p;
+        if (param->str[0] != '#') {
+            return -1;
+        }
+        if (tolower(param->str[1]) == 'f') {
+            is_fleet = true;
+        } else if (tolower(param->str[1]) == 't') {
+            is_fleet = false;
+        } else {
+            return -1;
+        }
+        if (0
+          || (!util_parse_signed_number(&param->str[2], &n))
+          || (n < 0)
+          || (is_fleet && (n >= g->enroute_num))
+          || ((!is_fleet) && (n >= g->transport_num))
+        ) {
+            return -1;
+        }
+        dest = ui_planet_from_param(g, api, &param[1]);
+        if (dest == PLANET_NONE) {
+            puts("Unknown destination");
+            return -1;
+        }
+        p = &(g->planet[dest]);
+        dist = game_get_min_dist(g, api, dest);
+        if (is_fleet) {
+            fleet_enroute_t *r = &(g->enroute[n]);
+            uint8_t pon;
+            bool in_range;
+            if (r->owner != api) {
+                return -1;
+            }
+            pon = get_fleet_planet_on(g, r);
+            if ((!e->have_hyperspace_comm) && (pon == PLANET_NONE)) {
+                return -1;
+            }
+            in_range = false;
+            if (p->within_frange[api] == 1) {
+                in_range = true;
+            } else if (p->within_frange[api] == 2) {
+                in_range = true;
+                for (int k = 0; k < e->shipdesigns_num; ++k) {
+                    if (r->ships[k] && (!g->srd[api].have_reserve_fuel)) {
+                        in_range = false;
+                        break;
+                    }
+                }
+            }
+            if (!in_range) {
+                printf("%s %i %s\n", game_str_sm_outsr, dist - e->fuel_range, game_str_sm_parsecs2);
+                return -1;
+            }
+            game_fleet_redirect(g, r, pon, dest);
+        } else {
+            transport_t *r = &(g->transport[n]);
+            if (0
+              || (r->owner != api)
+              || (!e->have_hyperspace_comm)
+            ) {
+                return -1;
+            }
+            if (p->within_frange[api] != 1) {
+                printf("%s %i %s\n", game_str_sm_outsr, dist - e->fuel_range, game_str_sm_parsecs2);
+                return -1;
+            }
+            if (!game_transport_dest_ok(g, p, api)) {
+                puts("Destination not OK");
+                return -1;
+            }
+            r->dest = dest;
+        }
+    }
+    return 0;
+}
 
 int ui_cmd_fleet_send(struct game_s *g, int api, struct input_token_s *param, int num_param, void *var)
 {
