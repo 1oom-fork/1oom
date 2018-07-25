b3089af5b6983bfd9c471502685f2d12899fa52d
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-25 10:08:56 +0300

    Fix hex values (tbl_starship)

--- src/game/game_shipdesign.c
+++ src/game/game_shipdesign.c
@@ -10,50 +10,50 @@ int16_t startship_num = 5;
 
 shipdesign_t tbl_startship[NUM_SHIPDESIGNS] = {
     { /* SCOUT */
-      "", 0xa, 0, SHIP_HULL_SMALL, 0,
+      "", 10, 0, SHIP_HULL_SMALL, 0,
       { 0, 0, 0, 0 },
       { 0, 0, 0, 0 },
-      0, 0xa,
+      0, 10,
       { SHIP_SPECIAL_RESERVE_FUEL_TANKS, 0, 0 },
       0, 0, 0, 0, 0, 3
     },
     { /* FIGHTER */
-      "", 0xf, 0, SHIP_HULL_SMALL, 1,
+      "", 15, 0, SHIP_HULL_SMALL, 1,
       { WEAPON_LASER, 0, 0, 0 },
       { 1, 0, 0, 0 },
-      0, 0x1e,
+      0, 30,
       { 0, 0, 0 },
       0, 0, 0, 0, 0, 3
     },
     { /* DESTROYER */
-      "", 0x42, 0, SHIP_HULL_MEDIUM, 6,
+      "", 66, 0, SHIP_HULL_MEDIUM, 6,
       { WEAPON_NUCLEAR_MISSILE_2, WEAPON_LASER, 0, 0 },
       { 1, 3, 0, 0 },
-      0, 0x73,
+      0, 115,
       { 0, 0, 0 },
       0, 0, 0, 0, 0, 18
     },
     { /* BOMBER */
-      "", 0x56, 0, SHIP_HULL_MEDIUM, 7,
+      "", 86, 0, SHIP_HULL_MEDIUM, 7,
       { WEAPON_NUCLEAR_BOMB, WEAPON_LASER, 0, 0 },
       { 2, 2, 0, 0 },
-      0, 0x5a,
+      0, 90,
       { 0, 0, 0 },
       0, 0, 0, 0, 0, 18
     },
     { /* COLONY SHIP */
-      "", 0x24f, 0, SHIP_HULL_LARGE, 12,
+      "", 591, 0, SHIP_HULL_LARGE, 12,
       { 0, 0, 0, 0 },
       { 0, 0, 0, 0 },
-      0, 0xcd,
+      0, 205,
       { SHIP_SPECIAL_STANDARD_COLONY_BASE, 0, 0 },
       0, 0, 0, 0, 0, 100
     },
     { /* (unused) */
-      "", 0xa, 0, SHIP_HULL_SMALL, 0,
+      "", 10, 0, SHIP_HULL_SMALL, 0,
       { 0, 0, 0, 0 },
       { 0, 0, 0, 0 },
-      0, 0xa,
+      0, 10,
       { 0, 0, 0 },
       0, 0, 0, 0, 0, 3
     }
