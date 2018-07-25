cb547d8bbf591a3ff2ce3aea15acda2e3c9931dd
Michal Sciubidlo <michal.sciubidlo@gmail.com>
2018-07-25 21:48:14 +0100

    Rename game_battle_ai_range_hmm1 to get_possible_distance_increase.

--- src/game/game_ai_classic.c
+++ src/game/game_ai_classic.c
@@ -2579,12 +2579,15 @@ static int game_battle_ai_missile_evade(const struct battle_s *bt)
     return evade;
 }
 
-static void game_battle_ai_range_hmm1(struct battle_s *bt, int target_i)
+/*
+ * Tries to approximate by how much enemy ship can increase distance in 2 turns.
+ */
+static void get_possible_distance_increase(struct battle_s *bt, int target_i)
 {
     struct battle_item_s *b = &(bt->item[bt->cur_item]);
     const struct battle_item_s *bd = &(bt->item[target_i]);
     int si, t;
-    if ((b->sx - bd->sx) < 0) {
+    if (b->sx < bd->sx) {
         si = BATTLE_AREA_W - bd->sx - 1;
     } else {
         si = bd->sx;
@@ -2651,10 +2654,12 @@ static int game_battle_ai_best_range(struct battle_s *bt, int target_i)
                 }
             }
             range = (itemi == 0/*planet*/) ? 12 : 0;
+            /* damagemax != damagemin means beam or bomb weapon */
             if ((w->damagemax != w->damagemin) && (!w->is_bomb)) {
                 range = b->extrarange;
             } else if (!w->is_bomb) {
-                game_battle_ai_range_hmm1(bt, target_i);
+                /* if missle (or no weapon but it doesn't matter) */
+                get_possible_distance_increase(bt, target_i);
                 range = b->maxrange;
             }
             if (((w->range + range) >= i) && (b->wpn[j].numshots != 0) && ((!w->damagefade) || (i == 1))) {
@@ -2975,7 +2980,7 @@ static void game_ai_classic_battle_ai_turn(struct battle_s *bt)
             if (!bt->flag_cur_item_destroyed) {
                 const struct battle_item_s *bd = &(bt->item[target_i]);
                 if (util_math_dist_maxabs(b->sx, b->sy, bd->sx, bd->sy) <= 1) {
-                    game_battle_ai_range_hmm1(bt, target_i);
+                    get_possible_distance_increase(bt, target_i);
                     game_battle_attack(bt, itemi, target_i, false);
                 }
             }
@@ -2994,7 +2999,7 @@ static void game_ai_classic_battle_ai_turn(struct battle_s *bt)
             }
         }
         /*5a72a*/
-        game_battle_ai_range_hmm1(bt, target_i);
+        get_possible_distance_increase(bt, target_i);
         game_battle_attack(bt, itemi, target_i, false);
     }
     /*5a740*/
@@ -3041,7 +3046,7 @@ static void game_ai_classic_battle_ai_turn(struct battle_s *bt)
             }
             /*5a8e1*/
             if (target_i > -1) {
-                game_battle_ai_range_hmm1(bt, target_i);
+                get_possible_distance_increase(bt, target_i);
                 game_battle_attack(bt, itemi, target_i, false);
             }
             if (loops > 10) {  /* MOO1 does not need this but it does have the loop counter */
