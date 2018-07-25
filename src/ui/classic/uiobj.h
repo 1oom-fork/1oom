dc90e5b8db459a24886c613d0942b8bfc24da7b7
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-07-25 10:08:56 +0300

    Cleanup (useless BUG? comment)

--- src/ui/classic/uiobj.h
+++ src/ui/classic/uiobj.h
@@ -20,7 +20,6 @@ extern void uiobj_table_clear(void);
 extern void uiobj_table_set_last(int16_t oi);
 extern void uiobj_table_num_store(void);
 extern void uiobj_table_num_restore(void);
-extern void uiobj_handle_objects(void);
 extern int16_t uiobj_handle_input_cond(void);
 
 extern void uiobj_finish_frame(void);
