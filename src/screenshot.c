e1ead8526d526b1e3eaacab4f67d375c8feea1da
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-08-11 12:28:53 +0300

    Screenshot: error on fail, cleanup

--- src/screenshot.c
+++ src/screenshot.c
@@ -20,15 +20,20 @@ bool screenshot_save(const uint8_t *screen, const uint8_t *pal, int w, int h)
 {
     char *fname = lib_malloc(FSDEV_PATH_MAX);
     struct pic_s pic;
+    bool res;
     os_get_fname_screenshot(fname, "pcx");
     pic.type = PIC_TYPE_PCX;
     pic.w = w;
     pic.h = h;
     pic.pitch = w;
-    pic.pal = pal;
-    pic.pix = screen;
-    if (fmt_pic_save(fname, &pic)) {
+    pic.pal = (uint8_t *)pal;
+    pic.pix = (uint8_t *)screen;
+    res = fmt_pic_save(fname, &pic);
+    if (res) {
         log_message("Screenshot: saved '%s'\n", fname);
+    } else {
+        log_error("Screenshot: failed to save '%s'\n", fname);
     }
     lib_free(fname);
+    return res;
 }
