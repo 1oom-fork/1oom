d09771d88958346df096e00962a8796bc29730fa
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-08-12 19:01:43 +0300

    Save and optionally fix attack bounty, fixes #50

--- src/game/game_num.h
+++ src/game/game_num.h
@@ -32,6 +32,7 @@ extern int game_num_max_tribute_bc;
 extern int game_num_event_roll;
 extern bool game_num_news_orion;
 extern bool game_num_aud_ask_break_nap;
+extern bool game_num_aud_bounty_give;
 extern bool game_num_monster_rest_att;
 extern bool game_num_orbital_weap_any;
 extern bool game_num_orbital_weap_4;
