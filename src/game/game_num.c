69f0d25cf75afd1f18c2ca7afe55da9ce5609c62
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-08-12 19:01:43 +0300

    Save and optionally fix attack bounty, fixes #50

--- src/game/game_num.c
+++ src/game/game_num.c
@@ -35,6 +35,7 @@ int game_num_event_roll = 512;
 int game_num_council_years = 25;
 bool game_num_news_orion = false;
 bool game_num_aud_ask_break_nap = false;
+bool game_num_aud_bounty_give = false;
 bool game_num_monster_rest_att = false;
 bool game_num_orbital_weap_any = false;
 bool game_num_orbital_weap_4 = false;
