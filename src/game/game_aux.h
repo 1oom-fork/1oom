484e1d2e0e818eebeced181bca8a7e3f8fe55d05
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-08-13 16:51:21 +0300

    Classic+: sensible AI-AI stop war check, fix Classic determinism

--- src/game/game_aux.h
+++ src/game/game_aux.h
@@ -61,7 +61,6 @@ struct game_aux_s {
     bool flag_cheat_galaxy;
     bool flag_cheat_events;
     bool initialized;
-    int16_t diplo_d0_rval;
     int savenamebuflen;
     int savebuflen;
     char *savenamebuf;
