dd2c33366521e962baa9b8d148ee4da604ff6f6a
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-09-01 18:02:01 +0300

    Remove opt_xmid_banks

--- src/options.h
+++ src/options.h
@@ -11,7 +11,6 @@ extern int opt_music_volume;
 extern int opt_sfx_volume;
 extern int opt_audiorate;
 extern int opt_audioslice_ms;
-extern int opt_xmid_banks;
 #ifdef HAVE_SAMPLERATE
 extern int opt_use_libsamplerate;
 extern int opt_libsamplerate_scale;
