5b8c9d33328a53a41a887fe6cf2e16c824f22f16
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-09-01 09:15:29 +0300

    Add XMID tempo option, drop tempo event by default

--- src/options.h
+++ src/options.h
@@ -19,6 +19,7 @@ extern int opt_sfx_volume;
 extern int opt_audiorate;
 extern int opt_audioslice_ms;
 extern int opt_xmid_ticksperq;
+extern int opt_xmid_tempo;
 extern bool opt_xmid_banks;
 #ifdef HAVE_SAMPLERATE
 extern bool opt_use_libsamplerate;
