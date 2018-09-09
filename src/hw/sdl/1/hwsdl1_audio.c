bd68ab01aa1baafeeffde545d63fa50eda676b4d
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-09-09 12:17:27 +0300

    SDL: show SDL_Mixer error on music load fail

--- src/hw/sdl/1/hwsdl1_audio.c
+++ src/hw/sdl/1/hwsdl1_audio.c
@@ -211,7 +211,7 @@ int hw_audio_music_init(int mus_index, const uint8_t *data_in, uint32_t len_in)
     m->music = Mix_LoadMUSType_RW(SDL_RWFromMem(data, len), m->sdlmtype, 0);
     lib_free(buf);
     if (!m->music) {
-        log_error("SDLA: Mix_LoadMUSType_RW failed on music %i (type %i)\n", mus_index, m->type);
+        log_error("SDLA: Mix_LoadMUSType_RW failed on music %i (type %i): %s\n", mus_index, m->type, Mix_GetError());
         m->type = MUS_TYPE_UNKNOWN;
         m->sdlmtype = MUS_NONE;
         return -1;
