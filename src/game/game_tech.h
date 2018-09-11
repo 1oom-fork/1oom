624e1a8e46cc0b39449b478ff304594e1aea4761
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-09-11 17:15:51 +0300

    Cleanup (new tech source/stolen from)

--- src/game/game_tech.h
+++ src/game/game_tech.h
@@ -8,6 +8,7 @@
 
 struct game_s;
 struct game_aux_s;
+struct newtech_s;
 
 extern const uint8_t tech_reduce_50percent_per_10pts[51];
 extern const uint8_t tech_reduce_25percent_per_10pts[51];
@@ -24,10 +25,11 @@ extern uint8_t game_get_best_jammer(const struct game_s *g, player_id_t player_i
 extern void game_update_tech_util(struct game_s *g);
 extern const char *game_tech_get_name(const struct game_aux_s *gaux, tech_field_t field, int tech, char *buf);
 extern const char *game_tech_get_descr(const struct game_aux_s *gaux, tech_field_t field, int tech, char *buf);
+extern const char *game_tech_get_newtech_msg(const struct game_s *g, player_id_t pi, struct newtech_s *nt, char *buf);
 extern int game_tech_current_research_percent1(const struct game_s *g, player_id_t player_i, tech_field_t field);
 extern int game_tech_current_research_percent2(const struct game_s *g, player_id_t player_i, tech_field_t field);
 extern bool game_tech_current_research_has_max_bonus(const struct game_s *g, player_id_t player_i, tech_field_t field);
-extern void game_tech_get_new(struct game_s *g, player_id_t player, tech_field_t field, uint8_t tech, uint8_t source, int a8, int ac, bool flag_frame);
+extern void game_tech_get_new(struct game_s *g, player_id_t player, tech_field_t field, uint8_t tech, techsource_t source, int a8, player_id_t stolen_from, bool flag_frame);
 extern bool game_tech_can_choose(const struct game_s *g, player_id_t player, tech_field_t field);
 extern int game_tech_get_next_techs(const struct game_s *g, player_id_t player, tech_field_t field, uint8_t *tbl);
 extern uint32_t game_tech_get_next_rp(const struct game_s *g, player_id_t player, tech_field_t field, uint8_t tech);
