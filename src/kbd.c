be271a8e531d02cac77f082c348a54d49d8f212a
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-09-11 12:59:58 +0300

    The keys UHJK to scroll the map

--- src/kbd.c
+++ src/kbd.c
@@ -1,24 +1,39 @@
 #include "config.h"
 
+#include "boolvec.h"
 #include "kbd.h"
 #include "log.h"
 #include "types.h"
 
 /* -------------------------------------------------------------------------- */
 
-#define KBD_BUFSIZE 8
+#define KBD_BUFSIZE 16
 static struct {
     bool full;
     int head;
     int tail;
+    uint32_t mod;
     uint32_t buf[KBD_BUFSIZE];
+    BOOLVEC_DECLARE(pressed, MOO_KEY_LAST);
 } kbd = { 0 };
 
 /* -------------------------------------------------------------------------- */
 
+void kbd_clear(void)
+{
+    kbd.full = false;
+    kbd.head = 0;
+    kbd.tail = 0;
+    kbd.mod = 0;
+    BOOLVEC_CLEAR(kbd.pressed, MOO_KEY_LAST);
+}
+
 void kbd_add_keypress(mookey_t key, uint32_t mod, char c)
 {
     uint32_t value = ((uint32_t)key) | mod | (((uint32_t)c) << 8);
+    if (key == MOO_KEY_LAST) {
+        return;
+    }
     if (kbd.full) {
         log_warning("kbd: full while inserting 0x%x\n", value);
     } else {
@@ -44,4 +59,17 @@ uint32_t kbd_get_keypress(void)
         kbd.full = false;
     }
     return key;
+}
+
+void kbd_set_pressed(mookey_t key, uint32_t mod, bool pressed)
+{
+    kbd.mod = mod;
+    if ((key != MOO_KEY_UNKNOWN) && (key < MOO_KEY_LAST)) {
+        BOOLVEC_SET(kbd.pressed, key, pressed);
+    }
+}
+
+bool kbd_is_pressed(mookey_t key, uint32_t modon, uint32_t modoff)
+{
+    return (((kbd.mod & modon) == modon) && ((kbd.mod & modoff) == 0) && BOOLVEC_IS1(kbd.pressed, key));
 }
