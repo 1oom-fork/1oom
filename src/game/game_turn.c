13d12bdb2798ff4b4e15a913830476019315b6ec
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-09-13 15:26:01 +0300

    Refactor new tech next/frame for multiplayer

--- src/game/game_turn.c
+++ src/game/game_turn.c
@@ -1584,6 +1584,7 @@ static void game_turn_audiences(struct game_s *g)
                 e->diplo_type[pa] = 0;
             }
         }
+        game_tech_finish_new(g, ph);
         ui_newtech(g, ph);
     }
 }
@@ -1751,6 +1752,7 @@ static void game_turn_update_final_war(struct game_s *g)
     game_tech_final_war_share(g);
     for (player_id_t i = 0; i < g->players; ++i) {
         if (IS_HUMAN(g, i)) {
+            game_tech_finish_new(g, i);
             ui_newtech(g, i);
         }
     }
@@ -1846,6 +1848,7 @@ struct game_end_s game_turn_process(struct game_s *g, bool fix_old_save_rng)
     game_tech_research(g);
     for (int i = 0; i < g->players; ++i) {
         if (IS_HUMAN(g, i)) {
+            game_tech_finish_new(g, i);
             ui_newtech(g, i);
         }
     }
@@ -1882,6 +1885,7 @@ struct game_end_s game_turn_process(struct game_s *g, bool fix_old_save_rng)
     }
     for (int i = 0; i < g->players; ++i) {
         if (IS_HUMAN(g, i)) {
+            game_tech_finish_new(g, i);
             ui_newtech(g, i);
         }
         g->evn.newtech[i].num = 0;
