0965b043e57c18cb4b26c3900e84b365205ceec6
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-09-14 07:56:35 +0300

    Fix AI armor II type bias

--- src/game/game_ai_classic.c
+++ src/game/game_ai_classic.c
@@ -1382,9 +1382,7 @@ static void game_ai_classic_design_ship_base(struct game_s *g, struct ai_turn_p2
         const int tbl_chance[SHIP_HULL_NUM] = { 1, 2, 4, 8 };
         int v;
         v = game_design_build_tbl_fit_armor(g, &ait->gd, tbl_have);
-        if ((rnd_1_n(100, &g->seed) > tbl_chance[hull]) && ((v & 1) == 1)) {
-            /* NOP */
-        } else {
+        if (rnd_1_n(100, &g->seed) > tbl_chance[hull]) {
             v &= ~1;
         }
         sd->armor = v;
