be82a2e4d5559faac5be18586eb44da3497f3375
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-09-19 18:45:34 +0300

    Require explicit disable for SDL(2)_mixer and OpenGL

--- src/hw/sdl/hwsdl_audio.c
+++ src/hw/sdl/hwsdl_audio.c
@@ -610,7 +610,7 @@ bool hw_audio_sfx_volume(int volume)
 int hw_audio_init(void)
 {
     if (opt_audio_enabled) {
-        log_warning("SDLA: no audio due to missing SDL_mixer!\n");
+        log_warning("SDLA: no audio since this build was made with SDL_mixer explicitly disabled!\n");
     }
     return 0;
 }
