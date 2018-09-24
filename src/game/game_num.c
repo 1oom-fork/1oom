2210134476d3d8d2b3f7cd7d742744f3dc769c7a
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-09-24 06:52:34 +0300

    PBX num for first tech 1 RP fix

--- src/game/game_num.c
+++ src/game/game_num.c
@@ -42,6 +42,7 @@ bool game_num_combat_trans_fix = false;
 bool game_num_stargate_redir_fix = false;
 bool game_num_trans_redir_fix = false;
 bool game_num_retreat_redir_fix = false;
+bool game_num_first_tech_rp_fix = false;
 bool game_num_waste_calc_fix = false;
 bool game_num_waste_adjust_fix = false;
 uint8_t game_num_eco_slider_slack = 7;
