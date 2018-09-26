45ad8b3bca8b3c18c5e53d5b3f113e1507957b17
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-09-26 14:50:32 +0300

    Classic+: Mrrshan and Alkari AI-AI battle bonus 15->20 (1.40m)

--- src/game/game_ai_classic.c
+++ src/game/game_ai_classic.c
@@ -2080,7 +2080,7 @@ static void game_ai_classic_battle_ai_ai_get_weights(const struct game_s *g, pla
         tbl[i] += e->tech.percent[TECH_FIELD_CONSTRUCTION] / 2;
         if ((e->race == RACE_MRRSHAN) || (e->race == RACE_ALKARI)) {
             /* FIXME: game_num_race_bonus */
-            tbl[i] += 15;
+            tbl[i] += ((g->ai_id == GAME_AI_CLASSIC) ? 15 : 20);
         }
         tbl[i] *= game_num_tbl_hull_w[sd->hull];
     }
