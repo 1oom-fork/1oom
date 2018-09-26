b92a433efc8a749d2bed3d9b4957127538b62b06
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-09-26 07:39:25 +0300

    Keys: Space as Continue

--- src/ui/classic/uispy_esp.c
+++ src/ui/classic/uispy_esp.c
@@ -220,7 +220,7 @@ void ui_spy_stolen(struct game_s *g, int pi, int spy, int field, uint8_t tech)
     d.gmap = ui_gmap_basic_init(g, true);
     stolen_load_data(&d);
     uiobj_table_clear();
-    uiobj_add_mousearea_all(MOO_KEY_UNKNOWN);
+    uiobj_add_mousearea_all(MOO_KEY_SPACE);
     while (!flag_done) {
         int16_t oi;
         ui_delay_prepare();
