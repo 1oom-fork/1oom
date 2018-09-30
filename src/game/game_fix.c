cc20344d611c65f8218284268c551bfb7e23399c
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-09-30 06:59:45 +0300

    Add fixes for orbital bombardment damage calculations

---
+++ src/game/game_fix.c
@@ -0,0 +1,10 @@
+#include "config.h"
+
+#include "game_fix.h"
+
+/* -------------------------------------------------------------------------- */
+
+bool game_fix_orbital_weap_any = false;
+bool game_fix_orbital_weap_4 = false;
+bool game_fix_orbital_torpedo = false;
+bool game_fix_orbital_comp = false;
