46f7c7eeb57b938a36ede120c2b39884050f5b89
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-09-30 06:59:45 +0300

    Add fixes for orbital bombardment damage calculations

---
+++ src/game/game_fix.c
@@ -0,0 +1,13 @@
+#include "config.h"
+
+#include "game_fix.h"
+#include "game_types.h"
+#include "lib.h"
+#include "log.h"
+
+/* -------------------------------------------------------------------------- */
+
+bool game_fix_orbital_weap_any = false;
+bool game_fix_orbital_weap_4 = false;
+bool game_fix_orbital_torpedo = false;
+bool game_fix_orbital_comp = false;
---
+++ src/game/game_fix.h
@@ -0,0 +1,25 @@
+#ifndef INC_1OOM_GAME_FIX_H
+#define INC_1OOM_GAME_FIX_H
+
+#include "game_types.h"
+#include "types.h"
+
+/* Allow orbital bombardment with any weapon type.
+In MOO1 neither Death Ray, Crystal Ray nor Amoeba Stream can be used for
+orbital bombardment, but can destroy a colony in space combat. */
+extern bool game_fix_orbital_weap_any;
+
+/* Allow orbital bombardment with weapon in slot 4.
+In MOO1 the weapon in slot 4 is not used in orbital bombardment. */
+extern bool game_fix_orbital_weap_4;
+
+/* Fix orbital bombardment missile/torpedo damage halving.
+In MOO1 the missile damage is halved but torpedo damage is untouched. */
+extern bool game_fix_orbital_torpedo;
+
+/* Fix orbital bombardment undeserved battle computer bonus.
+In MOO1 the bonus is given for the best battle computer in any ship design
+regardless of if any such ships are present in the fleet in orbit. */
+extern bool game_fix_orbital_comp;
+
+#endif
