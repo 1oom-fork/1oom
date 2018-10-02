6da5d5a34f0dcdefa9a291c3d325a99daeb39e9b
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-10-02 10:53:48 +0300

    Fix dangling buttons on planet switching

--- src/ui/classic/uistarmap.c
+++ src/ui/classic/uistarmap.c
@@ -477,7 +477,6 @@ void ui_starmap_do(struct game_s *g, player_id_t active_player)
                 if (--i < 0) { i = g->galaxy_stars - 1; }
             } while (g->planet[i].owner != active_player);
             g->planet_focus_i[active_player] = i;
-            p = &(g->planet[i]);
             ui_starmap_set_pos_focus(g, active_player);
             ui_sound_play_sfx_24();
         } else if (oi1 == oi_f3) {
@@ -487,7 +486,6 @@ void ui_starmap_do(struct game_s *g, player_id_t active_player)
                 i = (i + 1) % g->galaxy_stars;
             } while (g->planet[i].owner != active_player);
             g->planet_focus_i[active_player] = i;
-            p = &(g->planet[i]);
             ui_starmap_set_pos_focus(g, active_player);
             ui_sound_play_sfx_24();
         } else if (oi1 == oi_alt_c) {
@@ -511,7 +509,6 @@ void ui_starmap_do(struct game_s *g, player_id_t active_player)
             int i;
             i = ui_starmap_newship_next(g, active_player, g->planet_focus_i[active_player]);
             g->planet_focus_i[active_player] = i;
-            p = &(g->planet[i]);
             ui_starmap_set_pos_focus(g, active_player);
             ui_sound_play_sfx_24();
             if (BOOLVEC_IS1(g->eto[active_player].orbit[i].visible, active_player)) {
@@ -523,7 +520,6 @@ void ui_starmap_do(struct game_s *g, player_id_t active_player)
             int i;
             i = ui_starmap_newship_prev(g, active_player, g->planet_focus_i[active_player]);
             g->planet_focus_i[active_player] = i;
-            p = &(g->planet[i]);
             ui_starmap_set_pos_focus(g, active_player);
             ui_sound_play_sfx_24();
             if (BOOLVEC_IS1(g->eto[active_player].orbit[i].visible, active_player)) {
@@ -538,7 +534,6 @@ void ui_starmap_do(struct game_s *g, player_id_t active_player)
             i = ui_starmap_enemy_incoming(g, active_player, pi, (oi1 == oi_f8));
             if (i != pi) {
                 g->planet_focus_i[active_player] = i;
-                p = &(g->planet[i]);
                 ui_starmap_set_pos_focus(g, active_player);
             }
         } else if (oi1 == oi_f4) {
@@ -557,7 +552,6 @@ void ui_starmap_do(struct game_s *g, player_id_t active_player)
             } while ((!found) && (i != pi));
             if (found) {
                 g->planet_focus_i[active_player] = i;
-                p = &(g->planet[i]);
                 ui_starmap_set_pos_focus(g, active_player);
                 ui_sound_play_sfx_24();
                 ui_data.starmap.orbit_player = active_player;
@@ -580,7 +574,6 @@ void ui_starmap_do(struct game_s *g, player_id_t active_player)
             } while ((!found) && (i != pi));
             if (found) {
                 g->planet_focus_i[active_player] = i;
-                p = &(g->planet[i]);
                 ui_starmap_set_pos_focus(g, active_player);
                 ui_sound_play_sfx_24();
                 ui_data.starmap.orbit_player = active_player;
@@ -611,16 +604,15 @@ void ui_starmap_do(struct game_s *g, player_id_t active_player)
                 i = 0;
             }
             g->planet_focus_i[active_player] = i;
-            p = &(g->planet[i]);
         }
         for (int i = 0; i < g->galaxy_stars; ++i) {
             if ((oi1 == d.oi_tbl_stars[i]) && !g->evn.build_finished_num[active_player]) {
                 g->planet_focus_i[active_player] = i;
-                p = &(g->planet[i]);
                 ui_sound_play_sfx_24();
                 break;
             }
         }
+        p = &(g->planet[g->planet_focus_i[active_player]]);
         d.bottom_highlight = -1;
         if (oi2 == d.oi_gameopts) {
             d.bottom_highlight = 0;
--- src/ui/classic/uistarmap_orbit_own.c
+++ src/ui/classic/uistarmap_orbit_own.c
@@ -332,7 +332,6 @@ void ui_starmap_orbit_own(struct game_s *g, player_id_t active_player)
             int i;
             i = ui_starmap_newship_next(g, active_player, d.oo.from);
             g->planet_focus_i[active_player] = i;
-            p = &(g->planet[i]);
             ui_starmap_set_pos_focus(g, active_player);
             d.oo.from = i;
             ui_sound_play_sfx_24();
@@ -346,7 +345,6 @@ void ui_starmap_orbit_own(struct game_s *g, player_id_t active_player)
             int i;
             i = ui_starmap_newship_prev(g, active_player, d.oo.from);
             g->planet_focus_i[active_player] = i;
-            p = &(g->planet[i]);
             ui_starmap_set_pos_focus(g, active_player);
             d.oo.from = i;
             ui_sound_play_sfx_24();
@@ -420,23 +418,19 @@ do_accept:
                     }
                 }
                 g->planet_focus_i[active_player] = d.oo.from;
-                p = &(g->planet[d.oo.from]);
             }
         } else if (oi1 == oi_scroll) {
             ui_starmap_scroll(g, scrollx, scrolly, scrollz);
         }
         ui_starmap_handle_oi_ctrl(&d, oi1);
-        if (ui_starmap_handle_tag(&d, oi1, false) != PLANET_NONE) {
-            p = &(g->planet[g->planet_focus_i[active_player]]);
-        }
+        ui_starmap_handle_tag(&d, oi1, false);
         for (int i = 0; i < g->galaxy_stars; ++i) {
-            if ((oi1 == d.oi_tbl_stars[i]) && !g->evn.build_finished_num[active_player]) {
+            if (oi1 == d.oi_tbl_stars[i]) {
                 if (ui_extra_enabled && (oi_accept != UIOBJI_INVALID) && (g->planet_focus_i[active_player] == i)) {
                     oi1 = oi_accept;
                     goto do_accept;
                 }
                 g->planet_focus_i[active_player] = i;
-                p = &(g->planet[i]);
                 ui_sound_play_sfx_24();
                 break;
             }
@@ -504,6 +498,7 @@ do_accept:
             }
         }
         if (!flag_done) {
+            p = &g->planet[g->planet_focus_i[active_player]];
             if (1
               && (g->planet_focus_i[active_player] != d.oo.from)
               && ((p->within_frange[active_player] == 1) || ((p->within_frange[active_player] == 2) && d.oo.sn0.have_reserve_fuel))
