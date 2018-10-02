790d1111268e1f158db9e67025bc4181fcbc05bf
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-10-02 11:28:29 +0300

    Fix 1oom typos

--- src/game/game_ai_classic.c
+++ src/game/game_ai_classic.c
@@ -1260,7 +1260,7 @@ static void game_ai_classic_design_ship_base(struct game_s *g, struct ai_turn_p2
         const int tbl_chance[SHIP_HULL_NUM] = { 1, 2, 4, 8 };
         int v;
         v = game_design_build_tbl_fit_armor(g, &ait->gd, tbl_have);
-        if ((rnd_1_n(100, &g->seed) > tbl_chance[hull]) && ((v & 1) == 1)) {
+        if ((rnd_1_n(100, &g->seed) <= tbl_chance[hull]) && ((v & 1) == 1)) {
             /* NOP */
         } else {
             v &= ~1;
@@ -1609,8 +1609,8 @@ static void game_ai_classic_turn_p2_do(struct game_s *g, player_id_t pi)
                 tbl_year[i] = 0;
             }
         }
-        for (int i = 0; i < NUM_SHIPDESIGNS - 1; ++i) {
-            for (int j = 0; j < NUM_SHIPDESIGNS - 1; ++j) {
+        for (int j = 0; j < NUM_SHIPDESIGNS - 1; ++j) {
+            for (int i = 0; i < NUM_SHIPDESIGNS - 1; ++i) {
                 uint16_t y1, y;
                 y1 = tbl_year[i + 1];
                 y = tbl_year[i];
