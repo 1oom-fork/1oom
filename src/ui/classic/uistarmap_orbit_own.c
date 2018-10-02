fdd23a021e52465f98988d4c08e3b3998838059a
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-10-02 11:28:29 +0300

    Fix 1oom typos

--- src/ui/classic/uistarmap_orbit_own.c
+++ src/ui/classic/uistarmap_orbit_own.c
@@ -427,7 +427,7 @@ void ui_starmap_orbit_own(struct game_s *g, player_id_t active_player)
         }
         ui_starmap_handle_oi_ctrl(&d, oi1);
         for (int i = 0; i < g->galaxy_stars; ++i) {
-            if ((oi1 == d.oi_tbl_stars[i]) && !g->evn.build_finished_num[active_player]) {
+            if (oi1 == d.oi_tbl_stars[i]) {
                 g->planet_focus_i[active_player] = i;
                 ui_sound_play_sfx_24();
                 break;
