2dab2cc9d59fe562008e70469f21103e203982ba
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-10-19 06:58:20 +0300

    Quit to main menu as in v1.3

--- src/game/game.c
+++ src/game/game.c
@@ -608,7 +608,7 @@ int main_do(void)
         switch (game_end.type) {
             case GAME_END_QUIT:
                 log_message("Game: quit (ingame)\n");
-                goto done;
+                break;
             case GAME_END_NONE:
             case GAME_END_FINAL_WAR:
                 break;
