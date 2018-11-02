73bc18c140fdf8f675059fa38548a17076e9661d
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-11-02 09:42:33 +0200

    Add missing base(s) text to scrap UI, fixes #65

--- src/game/game_strp.c
+++ src/game/game_strp.c
@@ -277,6 +277,8 @@ static const struct strtbl_s {
     DEFSTRITEM(gm_mapkey),
     DEFSTRITEM(bs_line1),
     DEFSTRITEM(bs_line2),
+    DEFSTRITEM(bs_base),
+    DEFSTRITEM(bs_bases),
     DEFSTRITEMTBL(tbl_te_field),
     DEFSTRITEM(te_adv),
     DEFSTRITEM(te_tech),
