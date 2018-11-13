3d845eb2c1c58a7658b9413cc4b9b931be390444
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-11-13 18:45:30 +0200

    Add UI fixes

---
+++ src/ui/classic/uifix.c
@@ -0,0 +1,12 @@
+#include "config.h"
+
+#include "uifix.h"
+
+/* -------------------------------------------------------------------------- */
+
+bool ui_fix_planet_list_pos = false;
+bool ui_fix_slider_text_ind = false;
+bool ui_fix_spy_cost = false;
+bool ui_fix_starmap_oor_msg = false;
+
+bool ui_qol_cursor_nav_all_obj = false;
