cc4a868c21565ceba04332d37865cf313217633a
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-11-13 18:45:30 +0200

    Cleanup (remove unused in hwnop)

--- src/hw/nop/hwnop.c
+++ src/hw/nop/hwnop.c
@@ -55,16 +55,6 @@ void hw_log_error(const char *msg)
     fputs(msg, stderr);
 }
 
-int hw_event_handle(void)
-{
-    return 0;
-}
-
-int hw_video_init(int w, int h)
-{
-    return 0;
-}
-
 void hw_video_set_palette(uint8_t *palette, int first, int num)
 {
 }
@@ -72,36 +62,4 @@ void hw_video_set_palette(uint8_t *palette, int first, int num)
 uint8_t *hw_video_get_buf(void)
 {
     return 0;
-}
-
-uint8_t *hw_video_draw_buf(void)
-{
-    return 0;
-}
-
-int hw_audio_music_init(int mus_index, const uint8_t *data, uint32_t len)
-{
-    return 0;
-}
-void hw_audio_music_release(int mus_index)
-{
-}
-void hw_audio_music_play(int mus_index)
-{
-}
-void hw_audio_music_stop(void)
-{
-}
-int hw_audio_sfx_init(int sfx_index, const uint8_t *data, uint32_t len)
-{
-    return 0;
-}
-void hw_audio_sfx_release(int sfx_index)
-{
-}
-void hw_audio_sfx_play(int sfx_index)
-{
-}
-void hw_audio_sfx_stop(void)
-{
 }
