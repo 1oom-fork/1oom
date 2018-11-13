c3be2ce56d72b5ee8b0153ef1f65b8083815dea7
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-11-13 18:45:30 +0200

    Add game fixes

---
+++ src/game/game_fix.c
@@ -0,0 +1,12 @@
+#include "config.h"
+
+#include "game_fix.h"
+
+/* -------------------------------------------------------------------------- */
+
+bool game_fix_broken_garbage = false;
+bool game_fix_max_factories = false;
+bool game_fix_orbital_weap_any = false;
+bool game_fix_orbital_torpedo = false;
+bool game_fix_orbital_comp = false;
+bool game_fix_sg_finished = false;
