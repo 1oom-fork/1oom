d1ab3972b88ea5f2193dcce8b75d51c02c8fe47f
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-11-13 18:45:30 +0200

    Add UI fixes

---
+++ src/ui/classic/uifix.c
@@ -0,0 +1,7 @@
+#include "config.h"
+
+#include "uifix.h"
+
+/* -------------------------------------------------------------------------- */
+
+bool ui_qol_cursor_nav_all_obj = false;
---
+++ src/ui/classic/uifix.h
@@ -0,0 +1,9 @@
+#ifndef INC_1OOM_UI_FIX_H
+#define INC_1OOM_UI_FIX_H
+
+#include "types.h"
+
+/* Allow navigation through objects with assigned hotkeys using the cursor keys. */
+extern bool ui_qol_cursor_nav_all_obj;
+
+#endif
