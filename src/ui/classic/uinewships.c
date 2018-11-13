139f3023d1b6e33b4994f4975fcf11fc7587000f
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-11-13 18:45:30 +0200

    Fix typos (v1.0)
    e232e2397fde81402dfeb6903ce1f8bed3183c93
    0fdbf9d770f3f8e786c3934629bd76c1bffb9fdb
    581945d6efda3a9fbbc068df8e9d12e5949cd6f1
    db2538858672753fd381c6000a7271662118d532

--- src/ui/classic/uinewships.c
+++ src/ui/classic/uinewships.c
@@ -31,6 +31,7 @@ struct newships_data_s {
     player_id_t api;
     uint8_t *gfx_newship;
     struct starmap_data_s sm;
+    struct draw_stars_s ds;
 };
 
 static void newships_load_data(struct newships_data_s *d)
@@ -61,7 +62,6 @@ static void newships_draw_cb(void *vptr)
         n = g->evn.new_ships[d->api][i];
         if (n != 0) {
             const shipdesign_t *sd = &(g->srd[d->api].design[i]);
-            struct draw_stars_s ds;
             uint8_t *gfx;
             int x0, y0;
             x0 = x + 8 + (i % 3) * 48;
@@ -71,16 +71,15 @@ static void newships_draw_cb(void *vptr)
             ui_draw_line1(x0, y0 + 30, x0 + 39, y0 + 30, 0x5c);
             lbxfont_select(2, 0, 0, 0);
             lbxfont_print_str_center(x0 + 20, y0 + 33, sd->name, UI_SCREEN_W);
-            ds.xoff1 = 0;
-            ds.xoff2 = 0;
-            ui_draw_stars(x0, y0 + 2, i * 10, 40, &ds);
+            ui_draw_stars(x0, y0 + 2, i * 10, 40, &d->ds);
             gfx = ui_data.gfx.ships[sd->look];
             lbxgfx_set_frame_0(gfx);
             lbxgfx_draw_frame(x0 + 4, y0 + 3, gfx, UI_SCREEN_W);
-            lbxfont_select(0, 0, 0, 0);
+            lbxfont_select(0, 0xd, 0, 0);
             lbxfont_print_num_right(x0 + 36, y0 + 23, n, UI_SCREEN_W);
         }
     }
+    ui_draw_set_stars_xoffs(&d->ds, false);
 }
 
 /* -------------------------------------------------------------------------- */
