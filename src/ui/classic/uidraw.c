4ac9fdc423fbc69cfdd934a9bb2408c09adcaad3
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-11-13 18:45:30 +0200

    Cleanup (hex values)

--- src/ui/classic/uidraw.c
+++ src/ui/classic/uidraw.c
@@ -230,8 +230,8 @@ void ui_draw_hmm3(int x0, int y0, int x1, int y1, uint8_t color0, uint8_t color1
 static void ui_draw_finish_hmm3(int x, int y, int f)
 {
     int vx, vy;
-    vx = x + 0x13;
-    vy = y + 0x13;
+    vx = x + 19;
+    vy = y + 19;
     x += f;
     y += f;
     vx -= f;
@@ -243,9 +243,9 @@ static void ui_draw_finish_hmm3(int x, int y, int f)
 
 static void ui_draw_finish_hmm2(void)
 {
-    for (int f = 0; f < 0xa; ++f) {
-        for (int x = 0; x < VGABUF_W; x += 0x14) {
-            for (int y = 0; y < VGABUF_H; y += 0x14) {
+    for (int f = 0; f < 10; ++f) {
+        for (int x = 0; x < VGABUF_W; x += 20) {
+            for (int y = 0; y < VGABUF_H; y += 20) {
                 ui_draw_finish_hmm3(x, y, f);
             }
         }
