968ff245b152bed8c219915a25d0d7d9a1dfc0f5
Kilgore Trout Mask Replicant <kilgore.replicant@tutanota.com>
2018-11-13 18:45:30 +0200

    Add game fixes

---
+++ src/game/game_fix.h
@@ -0,0 +1,29 @@
+#ifndef INC_1OOM_GAME_FIX_H
+#define INC_1OOM_GAME_FIX_H
+
+#include "types.h"
+
+/* Remove broken redundant code. */
+extern bool game_fix_broken_garbage;
+
+/* The 2500 factories limit is too low for Meklars. */
+extern bool game_fix_max_factories;
+
+/* Allow orbital bombardment with any weapon type.
+In MOO1 neither Death Ray, Crystal Ray nor Amoeba Stream can be used for
+orbital bombardment, but can destroy a colony in space combat. */
+extern bool game_fix_orbital_weap_any;
+
+/* Fix orbital bombardment missile/torpedo damage halving.
+In MOO1 the missile damage is halved but torpedo damage is untouched. */
+extern bool game_fix_orbital_torpedo;
+
+/* Fix orbital bombardment undeserved battle computer bonus.
+In MOO1 the bonus is given for the best battle computer in any ship design
+regardless of if any such ships are present in the fleet in orbit. */
+extern bool game_fix_orbital_comp;
+
+/* Fix sliders not adjusting correctly after building a Stargate. */
+extern bool game_fix_sg_finished;
+
+#endif
