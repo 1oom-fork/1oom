4f43227bd84976a646b3476b592530004b46a452
Tapani <thetap@protonmail.com>
2018-12-02 19:35:05 +0100

    uistarmap: Fix graphics bug with brighter stars drawn further away than dimmer ones

--- src/ui/classic/uifix.c
+++ src/ui/classic/uifix.c
@@ -7,6 +7,7 @@
 bool ui_fix_planet_list_pos = false;
 bool ui_fix_slider_text_ind = false;
 bool ui_fix_spy_cost = false;
+bool ui_fix_starmap_background = false;
 bool ui_fix_starmap_oor_msg = false;
 
 bool ui_qol_cursor_nav_all_obj = false;
