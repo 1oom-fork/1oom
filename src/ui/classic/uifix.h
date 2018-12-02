bdacd450537296a442e3ee5f5258502fb00853e8
Tapani <thetap@protonmail.com>
2018-12-02 19:35:05 +0100

    uistarmap: Fix graphics bug with brighter stars drawn further away than dimmer ones

--- src/ui/classic/uifix.h
+++ src/ui/classic/uifix.h
@@ -3,6 +3,9 @@
 
 #include "types.h"
 
+/* Fix graphics bug with brighter stars drawn further away than dimmer ones. */
+extern bool ui_fix_starmap_background;
+
 /* Allow navigation through objects with assigned hotkeys using the cursor keys. */
 extern bool ui_qol_cursor_nav_all_obj;
 
