64d1c9574be30e66dbc6fd166e6c586dae9d2204
Tapani <thetap@protonmail.com>
2019-01-08 12:58:13 +0100

    hwsdl2: Add support for detecting ctrl keys
    By default a press of ctrl key is reported as a key (left or right ctrl)
    without the ctrl modifier being indicated.
    The result was that holding down ctrl key was not detectable by reading
    the 1oom keyboard modifier mask.
    
    This patch reports a control key as "Ctrl + Control_L" instead of plain
    "Control_L" (or "Ctrl + Control_R" instead of "Control_R")

--- src/hw/sdl/2/hwsdl2.c
+++ src/hw/sdl/2/hwsdl2.c
@@ -267,6 +267,7 @@ int hw_event_handle(void)
 
     while (SDL_PollEvent(&e)) {
         switch (e.type) {
+            uint32_t mod;
             case SDL_KEYDOWN:
                 {
                     SDL_Keycode sym;
@@ -277,7 +278,6 @@ int hw_event_handle(void)
                     c = 0;
                     if (!(hw_kbd_check_hotkey(sym, smod, c))) {
                         mookey_t key;
-                        uint32_t mod;
                         if (sym & SDLK_SCANCODE_MASK) {
                             key = key_xlat_scan[SDLK_TBLI_FROM_SCAN(sym)];
                             c = 0;
@@ -290,6 +290,7 @@ int hw_event_handle(void)
                             }
                         }
                         mod = mod_xlat(smod);
+                        if (sym == SDLK_LCTRL || sym == SDLK_RCTRL) mod |= MOO_MOD_CTRL;
                         if ((key != MOO_KEY_UNKNOWN) && (key < MOO_KEY_LAST)) {
                             kbd_add_keypress(key, mod, c);
                         }
@@ -304,12 +305,14 @@ int hw_event_handle(void)
                     mookey_t key;
                     sym = e.key.keysym.sym;
                     smod = e.key.keysym.mod;
+                    mod = mod_xlat(smod);
+                    if (sym == SDLK_LCTRL || sym == SDLK_RCTRL) mod &= ~MOO_MOD_CTRL;
                     if (sym & SDLK_SCANCODE_MASK) {
                         key = key_xlat_scan[SDLK_TBLI_FROM_SCAN(sym)];
                     } else {
                         key = key_xlat_key[sym];
                     }
-                    kbd_set_pressed(key, mod_xlat(smod), false);
+                    kbd_set_pressed(key, mod, false);
                 }
                 break;
             case SDL_TEXTINPUT:
