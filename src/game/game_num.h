bf456010642ae7c4ffde84ea297c2fdf0fa32a81
Max Fillinger <max@max-fillinger.net>
2019-05-12 22:05:40 +0200

    events: Option to check number of factories for the accident event
    According to the OSG, the industrial accident event only happens on
    colonies with at least 30 factories. This is not true for MOO 1.3, but
    since it makes sense from a flavor perspective, I've added an option to
    check for it.

--- src/game/game_num.h
+++ src/game/game_num.h
@@ -30,6 +30,7 @@ extern int game_num_max_trans_dmg;
 extern int game_num_max_ship_maint;
 extern int game_num_max_tribute_bc;
 extern int game_num_event_roll;
+extern bool game_num_accident_chk_factories;
 extern int game_num_council_years;
 extern int game_num_eco_slider_slack;
 extern int game_num_race_bonus_alkari;
