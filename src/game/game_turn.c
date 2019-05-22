0d56f80e1dad8d48a2ec9ed7e3d56d34c58ff0c9
Tapani <thetap@protonmail.com>
2019-05-22 04:53:51 +0200

    Fix 1oom typos (branching)

--- src/game/game_turn.c
+++ src/game/game_turn.c
@@ -244,7 +244,7 @@ static void game_turn_build_eco(struct game_s *g)
                         if ((p->max_pop1 / 20) % 5) {
                             v += 5;
                         }
-                        SETMIN(v, 5);
+                        SETMAX(v, 5);
                         p->max_pop1 += v;
                         p->max_pop2 += v;
                         p->max_pop3 += v;
@@ -266,7 +266,7 @@ static void game_turn_build_eco(struct game_s *g)
                         if ((p->max_pop1 / 10) % 5) {
                             v += 5;
                         }
-                        SETMIN(v, 5);
+                        SETMAX(v, 5);
                         p->max_pop1 += v;
                         p->max_pop2 += v;
                         p->max_pop3 += v;
