6d352b2fc11dfc881f7840ba6ed43e451ad42d4d
Max Fillinger <max@max-fillinger.net>
2019-06-01 20:31:00 +0200

    Introduce safe string functions

--- src/lib.h
+++ src/lib.h
@@ -7,5 +7,19 @@ extern void *lib_malloc(size_t size);
 extern void *lib_realloc(void *p, size_t size);
 extern void lib_free(void *ptr);
 extern char *lib_stralloc(const char *str);
+extern char *lib_strcpy(char *dst, const char *src, size_t dst_bufsize);
+
+/* strbuild_*: build up strings piece by piece, checking the buffer size. */
+
+struct strbuild_s {
+    char *str_start;
+    char *str_end;     /* Points to the terminating '\0' byte. */
+    size_t remaining;  /* Buffer size after str_end. */
+};
+
+struct strbuild_s strbuild_init(char *buf, size_t bufsize);
+const char *strbuild_finish(struct strbuild_s *str);
+void strbuild_append_char(struct strbuild_s *str, char c);
+void strbuild_catf(struct strbuild_s *str, const char *fmt, ...);
 
 #endif
