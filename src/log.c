bcad88e9a91b49638eb96616f08ebafc29600e3f
Max Fillinger <max@max-fillinger.net>
2019-08-28 01:41:58 +0200

    log: Use snprintf

--- src/log.c
+++ src/log.c
@@ -42,7 +42,7 @@ void log_message(const char* format, ...)
     va_list ap;
 
     va_start(ap, format);
-    vsprintf(msgbuf, format, ap);
+    vsnprintf(msgbuf, MAX_MSG_LEN, format, ap);
     va_end(ap);
 
     log_message_direct(msgbuf);
@@ -53,10 +53,10 @@ void log_warning(const char *format, ...)
     va_list ap;
     int len;
 
-    len = sprintf(msgbuf, "warning: ");
+    len = snprintf(msgbuf, MAX_MSG_LEN, "warning: ");
 
     va_start(ap, format);
-    len = vsprintf(&msgbuf[len], format, ap);
+    vsnprintf(&msgbuf[len], MAX_MSG_LEN - len, format, ap);
     va_end(ap);
 
     log_warning_direct(msgbuf);
@@ -67,10 +67,10 @@ void log_error(const char *format, ...)
     va_list ap;
     int len;
 
-    len = sprintf(msgbuf, "error: ");
+    len = snprintf(msgbuf, MAX_MSG_LEN, "error: ");
 
     va_start(ap, format);
-    len = vsprintf(&msgbuf[len], format, ap);
+    vsnprintf(&msgbuf[len], MAX_MSG_LEN - len, format, ap);
     va_end(ap);
 
     log_error_direct(msgbuf);
@@ -81,10 +81,10 @@ void log_fatal_and_die(const char *format, ...)
     va_list ap;
     int len;
 
-    len = sprintf(msgbuf, "FATAL: ");
+    len = snprintf(msgbuf, MAX_MSG_LEN, "FATAL: ");
 
     va_start(ap, format);
-    len = vsprintf(&msgbuf[len], format, ap);
+    vsnprintf(&msgbuf[len], MAX_MSG_LEN - len, format, ap);
     va_end(ap);
 
     log_error_direct(msgbuf);
