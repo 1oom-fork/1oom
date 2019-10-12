f0c59b05ba945de51a802fab20ed8f30cddeadd3
Zachary KLine <zkline@speedpost.net>
2019-10-12 16:16:25 -0700

    Add output to "b", command to make the command line UI friendlier.

--- src/ui/cmdline/uiplanet.c
+++ src/ui/cmdline/uiplanet.c
@@ -370,6 +370,11 @@ int ui_cmd_planet_build(struct game_s *g, int api, struct input_token_s *param, 
         /* TODO handle param */
         return -1;
     }
+    if (p->buildship == BUILDSHIP_STARGATE) {
+        printf("%s\n", game_str_sm_stargate);
+    } else {
+        printf("Building %s\n", g->srd[api].design[p->buildship].name);
+    }
     return 0;
 }
 
