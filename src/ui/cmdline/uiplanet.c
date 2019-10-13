ca813c2b62cddbcf0978a04486ff1c287aba1a86
Max Fillinger <max@max-fillinger.net>
2019-10-13 02:19:45 +0200

    Use safe string operations in cmdline ui

--- src/ui/cmdline/uiplanet.c
+++ src/ui/cmdline/uiplanet.c
@@ -11,6 +11,7 @@
 #include "game_planet.h"
 #include "game_str.h"
 #include "game_tech.h"
+#include "lib.h"
 #include "uidefs.h"
 #include "util.h"
 
@@ -80,7 +81,7 @@ planet_slider_i_t ui_planet_slider_from_param(struct input_token_s *param)
 
 /* -------------------------------------------------------------------------- */
 
-const char *ui_planet_str(const struct game_s *g, player_id_t api, uint8_t planet_i, char *buf)
+const char *ui_planet_str(const struct game_s *g, player_id_t api, uint8_t planet_i, char *buf, size_t bufsize)
 {
     const planet_t *p = &(g->planet[planet_i]);
     if (BOOLVEC_IS1(p->explored, api)) {
@@ -92,7 +93,7 @@ const char *ui_planet_str(const struct game_s *g, player_id_t api, uint8_t plane
     )) {
         return p->name;
     } else {
-        sprintf(buf, "#%c%i", game_str_tbl_sm_stinfo[p->star_type][0]/*HACK*/, planet_i);
+        lib_sprintf(buf, bufsize, "#%c%i", game_str_tbl_sm_stinfo[p->star_type][0]/*HACK*/, planet_i);
         return buf;
     }
 }
@@ -155,7 +156,7 @@ void ui_planet_look(const struct game_s *g, player_id_t api, uint8_t planet_i, b
     int dist = game_get_min_dist(g, api, planet_i);
     {
         char buf[16];
-        printf("[%i] (%i,%i) %s: ", planet_i, p->x, p->y, ui_planet_str(g, api, planet_i, buf));
+        printf("[%i] (%i,%i) %s: ", planet_i, p->x, p->y, ui_planet_str(g, api, planet_i, buf, sizeof(buf)));
         if (game_xy_is_in_nebula(g, p->x, p->y)) {
             printf("(In nebula)\n");
         }
@@ -442,8 +443,8 @@ int ui_cmd_planet_trans(struct game_s *g, player_id_t api, struct input_token_s 
             return -1;
         } else if (pt->type < e->have_colony_for) {
             int pos;
-            pos = sprintf(buf, "%s ", game_str_sm_trcontr1);
-            sprintf(&buf[pos], "%s ", game_str_tbl_sm_pltype[pt->type]);
+            pos = lib_sprintf(buf, sizeof(buf), "%s ", game_str_sm_trcontr1);
+            lib_sprintf(&buf[pos], sizeof(buf) - pos, "%s ", game_str_tbl_sm_pltype[pt->type]);
             util_str_tolower(&buf[pos], sizeof(buf));
             printf("%s %s\n", buf, game_str_sm_trcontr2);
             return -1;
@@ -452,11 +453,11 @@ int ui_cmd_planet_trans(struct game_s *g, player_id_t api, struct input_token_s 
             return -1;
         } else {
             if (pf->have_stargate && pt->have_stargate && (pf->owner == pt->owner)) {
-                strcpy(buf, game_str_sm_stargate);
+                lib_strcpy(buf, game_str_sm_stargate, sizeof(buf));
             } else {
                 int eta, engine = e->have_engine;
                 eta = game_calc_eta(g, engine, pf->x, pf->y, pt->x, pt->y);
-                sprintf(buf, "%s %i %s", game_str_sm_eta, eta, (eta == 1) ? game_str_sm_turn : game_str_sm_turns);
+                lib_sprintf(buf, sizeof(buf), "%s %i %s", game_str_sm_eta, eta, (eta == 1) ? game_str_sm_turn : game_str_sm_turns);
             }
             puts(buf);
         }
