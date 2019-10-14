0159ce15bbc62f6d7108a2f6b99804a2265daa3a
Ignatius <ignat@gmx.net>
2019-10-14 19:02:12 +0200

    Add implementation of soil enrichment rounding fix

--- src/game/game_nump.c
+++ src/game/game_nump.c
@@ -199,6 +199,7 @@ static const struct numtbl_s {
     DEFNUMITEML(doom_stack_fix, NUMTYPE_BOOL, 0, 1),
     DEFNUMITEML(eco_slider_slack, NUMTYPE_U8, 0, 0xff),
     DEFNUMITEML(reset_tform_to_max, NUMTYPE_BOOL, 0, 1),
+    DEFNUMITEML(soil_rounding_fix, NUMTYPE_BOOL, 0, 1),
     DEFNUMITEMLTBLL(tbl_hull_w, NUMTYPE_U8, 0, 0xff),
     DEFNUMITEMLTBLL(tech_costmuld, NUMTYPE_U8, 0, 0xff),
     DEFNUMITEMLTBLL(tech_costmula, NUMTYPE_U8, 0, 0xff),
