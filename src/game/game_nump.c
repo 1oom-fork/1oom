4ac7919d5166c36293613a9d660486214fa22fba
Ignatius <ignat@gmx.net>
2019-11-03 23:20:29 +0100

    game_misc.c: make game_update_production() account for transports
    Population scheduled for transport no longer contributes to production.

--- src/game/game_nump.c
+++ src/game/game_nump.c
@@ -196,6 +196,7 @@ static const struct numtbl_s {
     DEFNUMITEML(eco_slider_slack, NUMTYPE_U8, 0, 0xff),
     DEFNUMITEML(reset_tform_to_max, NUMTYPE_BOOL, 0, 1),
     DEFNUMITEML(soil_rounding_fix, NUMTYPE_BOOL, 0, 1),
+    DEFNUMITEML(leaving_trans_fix, NUMTYPE_BOOL, 0, 1),
     DEFNUMITEMLTBLL(tbl_hull_w, NUMTYPE_U8, 0, 0xff),
     DEFNUMITEMLTBLL(tech_costmuld, NUMTYPE_U8, 0, 0xff),
     DEFNUMITEMLTBLL(tech_costmula, NUMTYPE_U8, 0, 0xff),
