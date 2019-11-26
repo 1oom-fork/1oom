db6b9113b9ce389d960eb9b85924708cadb83e11
Ignatius <ignat@gmx.net>
2019-11-26 09:20:58 +0100

    game_cheat: disable cheat codes when options are locked

--- src/game/game_cheat.c
+++ src/game/game_cheat.c
@@ -13,6 +13,7 @@
 
 bool game_cheat_galaxy(struct game_s *g, player_id_t pi)
 {
+    if (g->opt.lock ) return false;
     g->gaux->flag_cheat_galaxy = !g->gaux->flag_cheat_galaxy;
     game_update_tech_util(g);
     game_update_within_range(g);
@@ -25,6 +26,7 @@ bool game_cheat_galaxy(struct game_s *g, player_id_t pi)
 
 bool game_cheat_events(struct game_s *g, player_id_t pi)
 {
+    if (g->opt.lock ) return false;
     g->gaux->flag_cheat_events = !g->gaux->flag_cheat_events;
     return true;
 }
@@ -32,6 +34,7 @@ bool game_cheat_events(struct game_s *g, player_id_t pi)
 bool game_cheat_moola(struct game_s *g, player_id_t pi)
 {
     empiretechorbit_t *e = &(g->eto[pi]);
+    if (g->opt.lock ) return false;
     e->reserve_bc += 100;
     game_update_production(g);
     return true;
@@ -39,6 +42,7 @@ bool game_cheat_moola(struct game_s *g, player_id_t pi)
 
 bool game_cheat_traits(struct game_s *g, player_id_t pi)
 {
+    if (g->opt.lock ) return false;
     for (player_id_t i = PLAYER_0; i < g->players; ++i) {
         g->eto[i].trait2 = rnd_0_nm1(TRAIT2_NUM, &g->seed);
         g->eto[i].trait1 = rnd_0_nm1(TRAIT1_NUM, &g->seed);
