7ef258a348407a85d3707a89983317c6c1580103
Arho Mahlamäki <arho@amahl.fi>
2021-03-25 14:04:27 +0200

    gfxscale: add gfxscale_draw_pixel_fmt1 routine (LBX format 1)

--- src/gfxscale.h
+++ src/gfxscale.h
@@ -14,4 +14,16 @@ static inline uint8_t *gfxscale_draw_pixel(uint8_t *q, uint8_t b, uint16_t pitch
     return q;
 }
 
+/* used by lbxgfx.c for rle format 1 */
+static inline uint8_t *gfxscale_draw_pixel_fmt1(uint8_t *q, const uint8_t tbl[], uint16_t pitch, int scale)
+{
+    for (int sy = 0; sy < scale; ++sy) {
+        for (int sx = 0; sx < scale; ++sx) {
+            q[sx] = tbl[q[sx]];
+        }
+        q += pitch;
+    }
+    return q;
+}
+
 #endif
