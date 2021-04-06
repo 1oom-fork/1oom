a9b521609bc6f80a0030d1c8f4d645fe04e6dc32
Arho Mahlamäki <arho@amahl.fi>
2021-04-06 13:41:52 +0300

    hwsdl1: merge #includer stubs into hwsdl1_common.c

--- src/hw/sdl/1/hwsdl1_common.c
+++ src/hw/sdl/1/hwsdl1_common.c
@@ -1,12 +1,15 @@
 #include "config.h"
+#include "SDL.h"
 
 #ifdef HAVE_SDL1MIXER
 #define HAVE_SDLMIXER
-#include "SDL.h"
 #include "SDL_mixer.h"
 #include "SDL_rwops.h"
 #define USE_SFX_INIT_THREAD
 #define HWSDLX_CreateThread(_func_) SDL_CreateThread(_func_, 0)
 #endif /* HAVE_SDL1MIXER */
-
 #include "hwsdl_audio.c"
+
+#include "hwsdl_mouse.c"
+#include "hwsdl_aspect.c"
+
