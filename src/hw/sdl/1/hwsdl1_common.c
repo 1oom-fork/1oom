0587353fe2614139f77e5e2ed96046b0ac8a1fc0
Arho Mahlamäki <arho@amahl.fi>
2021-05-11 12:22:59 +0300

    hwsdl1: discard extra mouse event after warping

--- src/hw/sdl/1/hwsdl1_common.c
+++ src/hw/sdl/1/hwsdl1_common.c
@@ -14,6 +14,11 @@
 #include "hwsdl_aspect.c"
 
 void hw_mouse_warp(int x, int y) {
+    SDL_Event junk[100];
     SDL_WarpMouse(x, y);
+    SDL_PumpEvents();
+    while (SDL_PeepEvents(junk, 100, SDL_GETEVENT, SDL_MOUSEMOTIONMASK)==100) {
+        /* eat generated mouse events. only randomly generated on some OS + SDL1 library combo */
+    }
 }
 
