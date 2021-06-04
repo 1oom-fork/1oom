1oom-fork/1oom

commit 912dce137d246c4d788227c9fe06ed14cfeee805

Fix moving the cursor with arrow keys
uiclassic moves the cursor but doesn't notify the hardware backend.
This causes the cursor to jump around when using both
keyboard and the mouse to move it. This commit makes uiclassic notify
the (SDL2) hardware backend when suddenly repositioning the cursor.

hw.h: add prototype hw_mouse_warp(x,y)
mouse.c: call hw_mouse_warp() from mouse_set_xy()
hwsdl: implement hw_mouse_warp()
hwalleg, hwnop, hwx11: add no-op stub hw_mouse_warp()

src/hw/sdl/2/hwsdl2_window.c
@@ -38,6 +38,24 @@ static SDL_Renderer *the_renderer = NULL;
 
 /* -------------------------------------------------------------------------- */
 
+void hw_mouse_warp(int x, int y)
+{
+	if (the_window) {
+		SDL_Rect win = {0};
+		int wx=0, wy=0;
+
+		SDL_GetWindowSize(the_window, &win.w, &win.h);
+		moo_to_window(x, y, &win, &wx, &wy);
+
+		/* this function uses window coordinates. not logical units */
+		SDL_WarpMouseInWindow(the_window, wx, wy);
+
+		/* eat the generated mouse event */
+		SDL_PumpEvents();
+		SDL_FlushEvent(SDL_MOUSEMOTION);
+	}
+}
+
 static void print_format(const char *title, uint32_t p)
 {
     uint32_t r, g, b, a;

src/hw.h
@@ -34,6 +34,9 @@ extern void hw_textinput_stop(void);
 
 extern int hw_icon_set(const uint8_t *data, const uint8_t *pal, int w, int h);
 
+/* Warp mouse (or do nothing if input device is a touch screen) */
+extern void hw_mouse_warp(int x, int y);
+
 extern int hw_video_init(int w, int h);
 extern void hw_video_set_palette(const uint8_t *palette, int first, int num);
 extern uint8_t hw_video_get_palette_byte(int i);

src/hw/alleg/hwalleg_mouse.c
@@ -82,3 +82,9 @@ void hw_mouse_scroll(int scroll)
 {
     mouse_set_scroll_from_hw(scroll);
 }
+
+void hw_mouse_warp(int x, int y)
+{
+	(void) (x + y);
+}
+

src/hw/nop/hwnop.c
@@ -98,3 +98,9 @@ bool hw_audio_music_volume(int volume)
 {
     return true;
 }
+
+void hw_mouse_warp(int x, int y)
+{
+	(void) (x + y);
+}
+

src/hw/sdl/1/hwsdl1_common.c
@@ -13,3 +13,7 @@
 #include "hwsdl_mouse.c"
 #include "hwsdl_aspect.c"
 
+void hw_mouse_warp(int x, int y) {
+    SDL_WarpMouse(x, y);
+}
+

src/hw/x11/x11main.c
@@ -496,6 +496,11 @@ int hw_init(void) {
     return 0;
 }
 
+void hw_mouse_warp(int x, int y)
+{
+	(void) (x + y);
+}
+
 int main(int argc, char **argv) {
     return main_1oom(argc, argv);
 }

src/mouse.c
@@ -1,6 +1,7 @@
 #include "config.h"
 
 #include "mouse.h"
+#include "hw.h"
 
 /* ------------------------------------------------------------------------- */
 
@@ -49,6 +50,7 @@ void mouse_set_xy(int mx, int my)
     if (!mouse_disable_set_xy) {
         moouse_x = mx;
         moouse_y = my;
+        hw_mouse_warp(mx, my);
     }
 }
 

commit cc2b61c04c9bd11e17b06dc7b8d7c84760159842

hwsdl2: fix mouse warping again
Fix mouse warp bug introduced in
912dce137d246c4d788227c9fe06ed14cfeee805

hw_mouse_warp() now takes black padding bars into account

src/hw/sdl/2/hwsdl2_video_texture.c
@@ -99,7 +99,8 @@ void hwsdl_texture_output(SDL_Renderer *rdr)
     }
 
     /* what the user clicks has to match what was drawn on screen.
-     * thus the best place to set mouse coordinate range is right here */
+     * thus the best place to set mouse coordinate range is right here
+     * NOTE box.x and box.y are 0 because SDL deals with repositioning of the picture */
     hw_mouse_set_win_range(box.x, box.y, box.w, box.h);
 
     SDL_SetRenderTarget(rdr, NULL);

src/hw/sdl/2/hwsdl2_window.c
@@ -42,10 +42,21 @@ void hw_mouse_warp(int x, int y)
 {
     if (the_window) {
         SDL_Rect win = {0};
+        SDL_Rect vp = {0}; /* viewport in window units */
         int wx=0, wy=0;
 
         SDL_GetWindowSize(the_window, &win.w, &win.h);
-        moo_to_window(x, y, &win, &wx, &wy);
+
+        vp = win;
+        if (hw_opt_aspect != 0) {
+            /* SDL has no function to query where the picture is drawn?
+            Gotta figure it out ourselves and risk being wrong */
+            shrink_to_aspect_ratio(&vp.w, &vp.h, 1000000, hw_opt_aspect);
+            vp.x = (win.w - vp.w) / 2;
+            vp.y = (win.h - vp.h) / 2;
+        }
+
+        moo_to_window(x, y, &vp, &wx, &wy);
 
         /* this function uses window coordinates. not logical units */
         SDL_WarpMouseInWindow(the_window, wx, wy);