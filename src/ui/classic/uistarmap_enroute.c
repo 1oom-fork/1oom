1672c4cba00a4cf7047b5781510f3003f5afd2d2
MyName <fork1oom@yandex.com>
2022-03-12 10:53:55 +0300

    Fix bug: unable to change fleet selection in some rare cases

--- src/ui/classic/uistarmap_enroute.c
+++ src/ui/classic/uistarmap_enroute.c
@@ -268,27 +268,25 @@ do_accept:
                 break;
             }
         }
-        if ((r->owner != active_player) || (d.en.can_move == NO_MOVE)) {
-            for (int i = 0; i < g->transport_num; ++i) {
-                if (oi1 == d.oi_tbl_transport[i]) {
-                    ui_data.starmap.fleet_selected = i;
-                    ui_data.ui_main_loop_action = UI_MAIN_LOOP_TRANSPORT_SEL;
+        for (int i = 0; i < g->transport_num; ++i) {
+            if (oi1 == d.oi_tbl_transport[i]) {
+                ui_data.starmap.fleet_selected = i;
+                ui_data.ui_main_loop_action = UI_MAIN_LOOP_TRANSPORT_SEL;
+                ui_sound_play_sfx_24();
+                flag_done = true;
+                break;
+            }
+        }
+        for (int i = 0; i < g->galaxy_stars; ++i) {
+            for (player_id_t j = PLAYER_0; j < g->players; ++j) {
+                if (oi1 == d.oi_tbl_pl_stars[j][i]) {
+                    g->planet_focus_i[active_player] = i;
+                    d.en.from = i;
+                    ui_data.starmap.orbit_player = j;
+                    ui_data.ui_main_loop_action = (j == active_player) ? UI_MAIN_LOOP_ORBIT_OWN_SEL : UI_MAIN_LOOP_ORBIT_EN_SEL;
                     ui_sound_play_sfx_24();
                     flag_done = true;
-                    break;
-                }
-            }
-            for (int i = 0; i < g->galaxy_stars; ++i) {
-                for (player_id_t j = PLAYER_0; j < g->players; ++j) {
-                    if (oi1 == d.oi_tbl_pl_stars[j][i]) {
-                        g->planet_focus_i[active_player] = i;
-                        d.en.from = i;
-                        ui_data.starmap.orbit_player = j;
-                        ui_data.ui_main_loop_action = (j == active_player) ? UI_MAIN_LOOP_ORBIT_OWN_SEL : UI_MAIN_LOOP_ORBIT_EN_SEL;
-                        ui_sound_play_sfx_24();
-                        flag_done = true;
-                        j = g->players; i = g->galaxy_stars;
-                    }
+                    j = g->players; i = g->galaxy_stars;
                 }
             }
         }
