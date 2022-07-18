2c742c864ca5b99af1c7dba6e312c4ec29b11d45
MyName <fork1oom@yandex.com>
2022-07-18 22:16:14 +0300

    uigame: allow uistarmap_bases/ships to handle build_finished

--- src/ui/classic/uigame.c
+++ src/ui/classic/uigame.c
@@ -82,7 +82,11 @@ ui_turn_action_t ui_game_turn(struct game_s *g, int *load_game_i_ptr, int pi)
                     g->planet_focus_i[pi] = ui_data.start_planet_focus_i;
                 }
             }
-            ui_data.ui_main_loop_action = UI_MAIN_LOOP_STARMAP;
+            if (ui_data.ui_main_loop_action != UI_MAIN_LOOP_SCRAP_BASES
+              &&ui_data.ui_main_loop_action != UI_MAIN_LOOP_PLANET_SHIPS)
+            {
+                ui_data.ui_main_loop_action = UI_MAIN_LOOP_STARMAP;
+            }
             ui_starmap_set_pos_focus(g, pi);
         }
 
