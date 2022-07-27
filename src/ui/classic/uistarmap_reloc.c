d190fb2d30d6ece1f9259006a2794fcf1a66a012
MyName <fork1oom@yandex.com>
2022-07-27 10:27:36 +0300

    uistarmap_reloc.c: Fix garbage variable and related bugs

--- src/ui/classic/uistarmap_reloc.c
+++ src/ui/classic/uistarmap_reloc.c
@@ -34,7 +34,7 @@ static void ui_starmap_reloc_draw_cb(void *vptr)
     ui_starmap_draw_basic(d);
     x0 = (pf->x - ui_data.starmap.x) * 2 + 8;
     y0 = (pf->y - ui_data.starmap.y) * 2 + 8;
-    if (pf->reloc != d->rl.from) {
+    if (g->planet_focus_i[d->api] != d->rl.from) {
         int x1, y1;
         x1 = (pt->x - ui_data.starmap.x) * 2 + 14;
         y1 = (pt->y - ui_data.starmap.y) * 2 + 14;
@@ -69,11 +69,11 @@ void ui_starmap_reloc(struct game_s *g, player_id_t active_player)
     int16_t oi_scroll, oi_cancel, oi_accept, oi_search;
     uint16_t scrollx = 0, scrolly = 0;
     struct starmap_data_s d;
-    uint8_t oldreloc;
     d.g = g;
     d.api = active_player;
     d.anim_delay = 0;
     {
+        uint8_t oldreloc;
         uint8_t pi = g->planet_focus_i[active_player];
         d.rl.from = pi;
         oldreloc = g->planet[pi].reloc;
@@ -102,7 +102,6 @@ void ui_starmap_reloc(struct game_s *g, player_id_t active_player)
         oi1 = uiobj_handle_input_cond();
         oi2 = uiobj_at_cursor();
         ui_delay_prepare();
-        g->planet[d.rl.from].reloc = g->planet_focus_i[active_player];
         ui_starmap_handle_scrollkeys(&d, oi1);
         if (oi1 == d.oi_gameopts) {
             ui_data.ui_main_loop_action = UI_MAIN_LOOP_GAMEOPTS;
@@ -147,7 +146,6 @@ void ui_starmap_reloc(struct game_s *g, player_id_t active_player)
                 } else {
                     ui_sound_play_sfx_06();
                     flag_done = true;
-                    g->planet[d.rl.from].reloc = oldreloc;
                     ui_data.ui_main_loop_action = UI_MAIN_LOOP_STARMAP;
                 }
             }
@@ -155,7 +153,6 @@ void ui_starmap_reloc(struct game_s *g, player_id_t active_player)
         if ((oi1 == oi_cancel) || (oi1 == UIOBJI_ESC)) {
             ui_sound_play_sfx_06();
             flag_done = true;
-            g->planet[d.rl.from].reloc = oldreloc;
             ui_data.ui_main_loop_action = UI_MAIN_LOOP_STARMAP;
         } else if (oi1 == oi_accept) {
 do_accept:
