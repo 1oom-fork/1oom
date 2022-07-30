02cbd2adacd64dd920ac77ed03a06d21537ccf2b
MyName <fork1oom@yandex.com>
2022-07-30 22:37:59 +0300

    uiextra: uistarmap.c: Move 'Build finished' text

--- src/ui/classic/uistarmap_common.c
+++ src/ui/classic/uistarmap_common.c
@@ -209,7 +209,8 @@ static void ui_starmap_draw_textbox_finished(const struct game_s *g, player_id_t
         }
     }
     game_planet_get_finished_text(g, p, i, buf, UI_STRBUF_SIZE);
-    ui_draw_textbox_2str("", buf, 54);
+    int y = ui_extra_enabled ? 45 : 54;
+    ui_draw_textbox_2str("", buf, y);
     ui_draw_textbox_2str("", game_str_sm_planratio, 110);
 }
 
