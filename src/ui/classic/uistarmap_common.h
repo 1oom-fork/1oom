a08d1670c10030ea59a38c3b111bc57a38a7e803
MyName <fork1oom@yandex.com>
2022-07-30 20:27:56 +0300

    uistarmap: uiextra: Alt+O shows useful info for explored stars

--- src/ui/classic/uistarmap_common.h
+++ src/ui/classic/uistarmap_common.h
@@ -49,6 +49,7 @@ struct starmap_data_s {
     int16_t oi_alt_m;
     int16_t oi_alt_r;
     int16_t oi_ctrl_r;
+    int16_t oi_alt_o;
     int16_t oi_tbl_stars[PLANETS_MAX];
     int16_t oi_scroll;
     int16_t oi_ctrl_left;
@@ -130,6 +131,7 @@ struct starmap_data_s {
         d.oi_alt_c = UIOBJI_INVALID; \
         d.oi_alt_f = UIOBJI_INVALID; \
         d.oi_alt_m = UIOBJI_INVALID; \
+        d.oi_alt_o = UIOBJI_INVALID; \
         d.oi_alt_r = UIOBJI_INVALID; \
         d.oi_ctrl_r = UIOBJI_INVALID; \
         for (int i = 0; i < g->galaxy_stars; ++i) { \
