f33ceeee9486ed192534846ac2e778f1c998dccf
MyName <fork1oom@yandex.com>
2022-08-04 13:52:23 +0300

    Main menu: Add Options menu

--- src/hw/sdl/hwsdl_video.h
+++ src/hw/sdl/hwsdl_video.h
@@ -7,6 +7,7 @@ extern void hw_video_refresh(int front);
 extern int hw_video_resize(int w, int h);
 extern void hw_video_shrink(void);
 extern void hw_video_enlarge(void);
+extern bool hw_video_toggle_borderless(void);
 extern bool hw_video_toggle_fullscreen(void);
 extern bool hw_video_update_aspect(void);
 extern void hw_video_input_grab(bool grab);
