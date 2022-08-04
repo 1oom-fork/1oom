32825752cfda23239d6d580e706c05a495d1ed78
MyName <fork1oom@yandex.com>
2022-08-04 13:52:23 +0300

    Main menu: Add Options menu

--- src/hw/sdl/2/hwsdl2_video.h
+++ src/hw/sdl/2/hwsdl2_video.h
@@ -5,4 +5,8 @@ extern int hw_video_get_window_id(void);
 extern void hw_video_set_visible(bool visible);
 extern void hw_video_mouse_warp(int mx, int my);
 
+extern bool hw_video_toggle_vsync(void);
+extern bool hw_video_toggle_int_scaling(void);
+extern bool hw_video_filter_next(void);
+
 #endif
