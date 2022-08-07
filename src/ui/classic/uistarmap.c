d5b7a157a835e7e30388cc0042cb4b943feb3c12
MyName <fork1oom@yandex.com>
2022-08-08 00:30:59 +0300

    Merge: uistarmap: Fix #,= behavior

--- src/ui/classic/uistarmap.c
+++ src/ui/classic/uistarmap.c
@@ -573,12 +573,10 @@ void ui_starmap_do(struct game_s *g, player_id_t active_player)
             ui_starmap_set_pos_focus(g, active_player);
             ui_sound_play_sfx_24();
         } else if (oi1 == oi_equals || oi1 == oi_hash) {
-            if (ui_extra_enabled && p->reserve > 0) {
+            if (p->reserve > 0) {
                 g->eto[active_player].reserve_bc += p->reserve;
                 p->reserve = 0;
                 ui_sound_play_sfx_24();
-            } else if (2 * p->reserve >= p->prod_after_maint) {
-                ui_sound_play_sfx_06();
             } else {
                 int v = p->prod_after_maint - 2 * p->reserve;
                 SETMIN(v, g->eto[active_player].reserve_bc);
