b1d56c00414dcfbaa9617808e0b397f29da77c75
MyName <fork1oom@yandex.com>
2022-08-08 14:57:35 +0300

    uistarmap_common.c: Fix duplicate code by introducing ui_starmap_common_init

--- src/ui/classic/uistarmap_reloc.c
+++ src/ui/classic/uistarmap_reloc.c
@@ -69,9 +69,7 @@ void ui_starmap_reloc(struct game_s *g, player_id_t active_player)
     int16_t oi_scroll, oi_cancel, oi_accept, oi_search;
     int16_t scrollx = 0, scrolly = 0;
     struct starmap_data_s d;
-    d.g = g;
-    d.api = active_player;
-    d.anim_delay = 0;
+    ui_starmap_common_init(g, &d, active_player);
     {
         uint8_t oldreloc;
         uint8_t pi = g->planet_focus_i[active_player];
