99f343cd32ced9ebdfc4d5846d90534f78387579
MyName <fork1oom@yandex.com>
2022-08-08 15:26:34 +0300

    uistarmap_common.c: Fix duplicate code by introducing controllable flag
    Note: This change takes advantage of the fact that r->owner, d.en.pon and
    g->eto[active_player].have_hyperspace_comm are constants in this scope

--- src/ui/classic/uistarmap_transport.c
+++ src/ui/classic/uistarmap_transport.c
@@ -45,7 +45,7 @@ static void ui_starmap_transport_draw_cb(void *vptr)
     }
     ui_draw_filled_rect(225, 8, 314, 180, 7);
     lbxgfx_draw_frame(224, 4, ui_data.gfx.starmap.tranbord, UI_SCREEN_W);
-    if ((r->owner == d->api) && (d->ts.can_move != NO_MOVE)) {
+    if (d->controllable) {
         lbxgfx_draw_frame(224, 159, ui_data.gfx.starmap.tranxtra, UI_SCREEN_W);
     }
     ui_draw_filled_rect(227, 8, 310, 39, 0);
@@ -66,7 +66,7 @@ static void ui_starmap_transport_draw_cb(void *vptr)
         lbxgfx_draw_frame_offs(x1, y1, ui_data.gfx.starmap.planbord, STARMAP_LIMITS, UI_SCREEN_W);
         x0 = (r->x - ui_data.starmap.x) * 2 + 8;
         y0 = (r->y - ui_data.starmap.y) * 2 + 8;
-        if ((r->owner == d->api) && (d->ts.can_move != NO_MOVE)) {
+        if (d->controllable) {
             dest_ok = game_transport_dest_ok(g, pt, d->api);
         }
         {
@@ -82,7 +82,7 @@ static void ui_starmap_transport_draw_cb(void *vptr)
         }
         lbxgfx_draw_frame_offs(x0, y0, gfx, STARMAP_LIMITS, UI_SCREEN_W);
         dist = game_get_min_dist(g, r->owner, g->planet_focus_i[d->api]);
-        if ((r->owner == d->api) && (d->ts.can_move != NO_MOVE) && (pt->within_frange[d->api] == 0)) {
+        if (d->controllable && (pt->within_frange[d->api] == 0)) {
             /* FIXME use proper positioning for varying str length */
             d->ts.in_frange = false;
             lib_sprintf(buf, sizeof(buf), "  %s   %i %s.", game_str_sm_outsr, dist - e->fuel_range, game_str_sm_parsecs2);
@@ -129,7 +129,7 @@ static void ui_starmap_transport_draw_cb(void *vptr)
         d->ts.scanner_delay = 0;
     }
     d->ts.frame_ship = (d->ts.frame_ship + 1) % 5;
-    if ((r->owner == d->api) && (d->ts.can_move != NO_MOVE) && (!d->ts.in_frange)) {
+    if (d->controllable && (!d->ts.in_frange)) {
         lbxgfx_set_new_frame(ui_data.gfx.starmap.reloc_bu_accept, 1);
         lbxgfx_draw_frame(271, 163, ui_data.gfx.starmap.reloc_bu_accept, UI_SCREEN_W);
     }
@@ -153,7 +153,7 @@ void ui_starmap_transport(struct game_s *g, player_id_t active_player)
     d.ts.scanner_delay = 0;
     d.ts.from = g->planet_focus_i[active_player];
     g->planet_focus_i[active_player] = r->dest;
-    d.ts.can_move = g->eto[active_player].have_hyperspace_comm ? GOT_HYPERCOMM : NO_MOVE;
+    d.controllable = g->eto[active_player].have_hyperspace_comm && (r->owner == active_player);
 
     uiobj_table_clear();
 
@@ -210,7 +210,7 @@ void ui_starmap_transport(struct game_s *g, player_id_t active_player)
         } else if (oi1 == oi_search) {
             ui_sound_play_sfx_24();
             if (ui_search_set_pos(g, active_player)) {
-                if ((r->owner != active_player) || (d.ts.can_move == NO_MOVE)) {
+                if (!d.controllable) {
                     d.ts.from = g->planet_focus_i[active_player];
                     flag_done = true;
                     ui_data.ui_main_loop_action = UI_MAIN_LOOP_STARMAP;
@@ -229,7 +229,7 @@ do_accept:
             }
             ui_data.ui_main_loop_action = UI_MAIN_LOOP_STARMAP;
         }
-        if ((r->owner != active_player) || (d.ts.can_move == NO_MOVE)) {
+        if (!d.controllable) {
             for (int i = 0; i < g->enroute_num; ++i) {
                 if (oi1 == d.oi_tbl_enroute[i]) {
                     ui_data.starmap.fleet_selected = i;
@@ -273,7 +273,7 @@ do_accept:
                     goto do_accept;
                 }
                 g->planet_focus_i[active_player] = i;
-                if ((r->owner != active_player) || (d.ts.can_move == NO_MOVE)) {
+                if (!d.controllable) {
                     d.ts.from = i;
                     flag_done = true;
                     ui_data.ui_main_loop_action = UI_MAIN_LOOP_STARMAP;
@@ -305,7 +305,7 @@ do_accept:
             UIOBJ_CLEAR_LOCAL();
             ui_starmap_fill_oi_tbls(&d);
             ui_starmap_fill_oi_tbl_stars(&d);
-            if ((r->owner == active_player) && (d.ts.can_move != NO_MOVE)) {
+            if (d.controllable) {
                 oi_cancel = uiobj_add_t0(227, 163, "", ui_data.gfx.starmap.reloc_bu_cancel, MOO_KEY_ESCAPE);
                 if (game_transport_dest_ok(g, &(g->planet[g->planet_focus_i[active_player]]), active_player)) {
                     oi_accept = uiobj_add_t0(271, 163, "", ui_data.gfx.starmap.reloc_bu_accept, MOO_KEY_SPACE);
