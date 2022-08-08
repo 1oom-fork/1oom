a76a623504db17e261549e77df2f3098174fdc41
MyName <fork1oom@yandex.com>
2022-08-08 15:26:34 +0300

    uistarmap_common.c: Fix duplicate code by introducing controllable flag
    Note: This change takes advantage of the fact that r->owner, d.en.pon and
    g->eto[active_player].have_hyperspace_comm are constants in this scope

--- src/ui/classic/uistarmap_common.h
+++ src/ui/classic/uistarmap_common.h
@@ -29,11 +29,10 @@ struct shipnon0_s {
     bool have_reserve_fuel;
 };
 
-typedef enum { NO_MOVE, GOT_HYPERCOMM, ON_PLANET } can_move_t;
-
 struct starmap_data_s {
     struct game_s *g; /* FIXME non-const only for ui_starmap_draw_cb1 */
     player_id_t api;
+    bool controllable;
     int bottom_highlight;
     int anim_delay;
     int16_t oi_gameopts;
@@ -84,7 +83,6 @@ struct starmap_data_s {
         struct {
             bool in_frange;
             uint8_t from;
-            can_move_t can_move;
             struct draw_stars_s ds;
             int frame_ship;
             int frame_scanner;
@@ -111,7 +109,6 @@ struct starmap_data_s {
             bool in_frange;
             uint8_t from;
             uint8_t pon;
-            can_move_t can_move;
             struct draw_stars_s ds;
             int frame_ship;
             int frame_scanner;
