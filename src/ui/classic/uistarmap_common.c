cb17c15bf1c495c253ced0f931b1f39418b1c072
MyName <fork1oom@yandex.com>
2022-08-09 13:13:47 +0300

    uistarmap_common.c: Fix duplicate code by introducing show_planet_focus flag
    Note: This change takes advantage of the fact that r->owner and
    g->eto[d.api].have_ia_scanner are constants in this scope

--- src/ui/classic/uistarmap_common.c
+++ src/ui/classic/uistarmap_common.c
@@ -958,5 +958,6 @@ void ui_starmap_common_init(struct game_s *g, struct starmap_data_s *d, player_i
     d->g = g;
     d->api = active_player;
     d->controllable = false;
+    d->show_planet_focus = true;
     d->anim_delay = 0;
 }
