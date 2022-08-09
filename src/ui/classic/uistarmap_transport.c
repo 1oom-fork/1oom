c77fa419725bb334b6e30140302ec0ffc1d41031
MyName <fork1oom@yandex.com>
2022-08-09 13:13:47 +0300

    uistarmap_common.c: Fix duplicate code by introducing show_planet_focus flag
    Note: This change takes advantage of the fact that r->owner and
    g->eto[d.api].have_ia_scanner are constants in this scope

--- src/ui/classic/uistarmap_transport.c
+++ src/ui/classic/uistarmap_transport.c
@@ -58,7 +58,7 @@ static void ui_starmap_transport_draw_cb(void *vptr)
     lib_sprintf(buf, sizeof(buf), "%s %s", game_str_tbl_race[e->race], game_str_sm_fleet);
     lbxfont_select_set_12_4(5, tbl_banner_fontparam[e->banner], 0, 0);
     lbxfont_print_str_center(267, 10, buf, UI_SCREEN_W, ui_scale);
-    if ((r->owner == d->api) || (g->eto[d->api].have_ia_scanner)) {
+    if (d->show_planet_focus) {
         const planet_t *pd = &(g->planet[r->dest]);
         uint8_t *gfx;
         int x0, y0, x1, y1, dist;
@@ -118,7 +118,7 @@ static void ui_starmap_transport_draw_cb(void *vptr)
     lib_sprintf(buf, sizeof(buf), "%i %s", r->pop, game_str_sm_colony);
     lbxfont_print_str_center(267, 48, buf, UI_SCREEN_W, ui_scale);
     lbxfont_print_str_center(267, 57, (r->pop == 1) ? game_str_sm_trans1 : game_str_sm_transs, UI_SCREEN_W, ui_scale);
-    if ((r->owner == d->api) || (g->eto[d->api].have_ia_scanner)) {
+    if (d->show_planet_focus) {
         const planet_t *pd = &(g->planet[r->dest]);
         lbxfont_print_str_center(267, 110, game_str_sm_tdest, UI_SCREEN_W, ui_scale);
         lbxfont_print_str_center(267, 120, pd->name, UI_SCREEN_W, ui_scale);
@@ -149,6 +149,7 @@ void ui_starmap_transport(struct game_s *g, player_id_t active_player)
     transport_t *r = &(g->transport[ui_data.starmap.fleet_selected]);
 
     ui_starmap_common_init(g, &d, active_player);
+    d.show_planet_focus = ((r->owner == d.api) || (g->eto[d.api].have_ia_scanner));
     d.ts.in_frange = false;
     d.ts.frame_ship = 0;
     d.ts.frame_scanner = 0;
