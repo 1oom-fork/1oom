b84ca695548f9f4e58a936c71355f170a059c6ae
MyName <fork1oom@yandex.com>
2022-09-23 15:36:58 +0300

    uistarmap.c: Fix undefined behavior related to search UI

--- src/ui/classic/uistarmap.c
+++ src/ui/classic/uistarmap.c
@@ -382,6 +382,7 @@ void ui_starmap_do(struct game_s *g, player_id_t active_player)
         } else if (oi1 == oi_search) {
             ui_sound_play_sfx_24();
             ui_search_set_pos(g, active_player);
+            flag_done = true;
         }
         for (int i = 0; i < g->enroute_num; ++i) {
             if (oi1 == d.oi_tbl_enroute[i]) {
