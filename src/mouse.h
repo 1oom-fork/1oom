774ff20b84fc40aef8a1f2a2b66ddb5183970e5b
MyName <fork1oom@yandex.com>
2022-09-26 22:40:07 +0300

    uiscale: Add cursor scaling

--- src/mouse.h
+++ src/mouse.h
@@ -16,6 +16,7 @@ extern int mouse_stored_y;
 extern int mouse_click_x;
 extern int mouse_click_y;
 extern int mouse_click_buttons;
+extern int mouse_cursor_scale;
 
 extern void mouse_set_xy_from_hw(int mx, int my);
 extern void mouse_set_buttons_from_hw(int buttons);
