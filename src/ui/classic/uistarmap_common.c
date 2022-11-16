db8539e53265a8c377d0fcc994e578fdc473ddc6
MyName <fork1oom@yandex.com>
2022-11-16 14:24:08 +0300

    uistarmap_common.c: Fix duplicate code by introducing ui_starmap_clamp_xy

--- src/ui/classic/uistarmap_common.c
+++ src/ui/classic/uistarmap_common.c
@@ -592,6 +592,12 @@ void ui_starmap_draw_button_text(struct starmap_data_s *d, bool highlight)
     lbxfont_print_str_normal(263, 184, game_str_sm_next_turn, UI_SCREEN_W);
 }
 
+static void ui_starmap_clamp_xy(const struct game_s *g, int *x, int *y)
+{
+    SETRANGE(*x, 0, g->galaxy_maxx - 108);
+    SETRANGE(*y, 0, g->galaxy_maxy - 86);
+}
+
 void ui_starmap_set_pos_focus(const struct game_s *g, player_id_t active_player)
 {
     const planet_t *p = &g->planet[g->planet_focus_i[active_player]];
@@ -602,8 +608,7 @@ void ui_starmap_set_pos(const struct game_s *g, int x, int y)
 {
     x -= 54;
     y -= 43;
-    SETRANGE(x, 0, g->galaxy_maxx - 108);
-    SETRANGE(y, 0, g->galaxy_maxy - 86);
+    ui_starmap_clamp_xy(g, &x, &y);
     ui_data.starmap.x = x;
     ui_data.starmap.x2 = x;
     ui_data.starmap.y = y;
@@ -652,8 +657,7 @@ void ui_starmap_handle_oi_ctrl(struct starmap_data_s *d, int16_t oi)
         changed = true;
     }
     if (changed) {
-        SETRANGE(x, 0, g->galaxy_maxx - 0x6c);
-        SETRANGE(y, 0, g->galaxy_maxy - 0x56);
+        ui_starmap_clamp_xy(g, &x, &y);
         ui_data.starmap.x2 = x;
         ui_data.starmap.y2 = y;
     }
@@ -718,8 +722,7 @@ void ui_starmap_handle_scrollkeys(struct starmap_data_s *d, int16_t oi)
         x += ui_starmap_scrollkey_accel(xh);
     }
     if (xh || yh) {
-        SETRANGE(x, 0, g->galaxy_maxx - 0x6c);
-        SETRANGE(y, 0, g->galaxy_maxy - 0x56);
+        ui_starmap_clamp_xy(g, &x, &y);
         ui_data.starmap.x2 = x;
         ui_data.starmap.y2 = y;
         ui_data.starmap.x = x;
@@ -957,8 +960,7 @@ void ui_starmap_scroll(const struct game_s *g, int scrollx, int scrolly)
     int x, y;
     x = ui_data.starmap.x + scrollx - 54;
     y = ui_data.starmap.y + scrolly - 43;
-    SETRANGE(x, 0, g->galaxy_maxx - 108);
-    SETRANGE(y, 0, g->galaxy_maxy - 86);
+    ui_starmap_clamp_xy(g, &x, &y);
     ui_data.starmap.x2 = x;
     ui_data.starmap.y2 = y;
 }
