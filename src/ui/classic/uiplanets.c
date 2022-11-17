a698a7515de59a9ed97bd647096fa82a03b9c836
MyName <fork1oom@yandex.com>
2022-11-17 10:21:07 +0300

    Merge: uiplanets: Build/Replace everywhere

--- src/ui/classic/uiplanets.c
+++ src/ui/classic/uiplanets.c
@@ -731,10 +731,10 @@ again:
                     ui_cursor_setup_area(1, &ui_cursor_area_tbl[0]);
                 }
                 goto again;
-            } else if( ui_extra_enabled && oi == oi_tbl_dock[i] && kbd_is_modifier(MOO_MOD_CTRL) && !flag_trans) {
+            } else if( oi == oi_tbl_dock[i] && kbd_is_modifier(MOO_MOD_CTRL) && !flag_trans) {
                 struct planet_s *p = &g->planet[tbl_onscreen_planets[i]];
                 game_planet_ship_build_everywhere(g, p->owner, p->buildship);
-            } else if( ui_extra_enabled && oi == oi_tbl_dock[i] && kbd_is_modifier(MOO_MOD_ALT) && !flag_trans) {
+            } else if( oi == oi_tbl_dock[i] && kbd_is_modifier(MOO_MOD_ALT) && !flag_trans) {
                 struct planet_s *p = &g->planet[d.focus_i];
                 struct planet_s *pr = &g->planet[tbl_onscreen_planets[i]];
                 game_planet_ship_replace_everywhere(g, p->owner, pr->buildship, p->buildship);
