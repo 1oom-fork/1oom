a656feda7a2a94bd79ed3fdf879312f034ac4d38
MyName <fork1oom@yandex.com>
2022-11-22 07:08:47 +0300

    uistarmap: planet: Pressing Alt + right arrow sets slider to max

--- src/ui/classic/uistarmap.c
+++ src/ui/classic/uistarmap.c
@@ -393,10 +393,11 @@ void ui_starmap_do(struct game_s *g, player_id_t active_player)
                 ui_sound_play_sfx_24();
             } else if (!p->slider_lock[i]) {
                 bool do_adj = false;
+                bool upd_eco = false;
                 int v;
                 if (oi1 == d.sm.oi_tbl_slider_minus[i]) {
                     if (kbd_is_modifier(MOO_MOD_ALT) && i == 3) {
-                        game_planet_update_eco_on_waste(g, p, d.api, true);
+                        upd_eco = true;
                     } else if (kbd_is_modifier(MOO_MOD_ALT)) {
                         p->slider[i] = 0;
                         do_adj = true;
@@ -408,14 +409,23 @@ void ui_starmap_do(struct game_s *g, player_id_t active_player)
                         do_adj = true;
                     }
                 } else if (oi1 == d.sm.oi_tbl_slider_plus[i]) {
-                    if (kbd_is_modifier(MOO_MOD_CTRL)) v = p->slider[i] + 1;
-                    else v = p->slider[i] + 4;
-                    SETMIN(v, 100);
-                    p->slider[i] = v;
-                    do_adj = true;
+                    if (kbd_is_modifier(MOO_MOD_ALT)) {
+                        p->slider[i] = 100;
+                        do_adj = true;
+                        if (i != 3) upd_eco = true;
+                    } else {
+                        if (kbd_is_modifier(MOO_MOD_CTRL)) v = p->slider[i] + 1;
+                        else v = p->slider[i] + 4;
+                        SETMIN(v, 100);
+                        p->slider[i] = v;
+                        do_adj = true;
+                    }
                 }
                 if (do_adj) {
                     game_adjust_slider_group(p->slider, i, p->slider[i], PLANET_SLIDER_NUM, p->slider_lock);
+                }
+                if (upd_eco) {
+                    game_planet_update_eco_on_waste(g, p, d.api, true);
                 }
             }
         }
