d6bb65e167ea4954c958e12aaf33b8013c6317d3
MyName <fork1oom@yandex.com>
2022-12-24 03:11:43 +0300

    Fix version string

--- src/version.sh
+++ src/version.sh
@@ -5,12 +5,12 @@ OUTFILE=$2
 GIT=$3
 VERSION1OOM=$4
 
-if test x$GIT != "xno" && test -d $TOPSRCDIR/.git
-    then
-    VERSIONSTR=`$GIT -C $TOPSRCDIR describe --tags`
-else
+#if test x$GIT != "xno" && test -d $TOPSRCDIR/.git
+#    then
+#    VERSIONSTR=`$GIT -C $TOPSRCDIR describe --tags`
+#else
     VERSIONSTR=$VERSION1OOM
-fi
+#fi
 
 OUTSTR="#define VERSION_STR \"$VERSIONSTR\""
 
