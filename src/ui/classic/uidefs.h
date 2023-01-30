f264b800fb0f8de6d4420e7ba8701e06a8db02ed
MyName <fork1oom@yandex.com>
2023-01-30 18:29:07 +0300

    uiextra: uiplanets.c: Keep sort order

--- src/ui/classic/uidefs.h
+++ src/ui/classic/uidefs.h
@@ -251,6 +251,7 @@ struct ui_data_s {
         uint16_t index[FLEET_ENROUTE_MAX + PLANETS_MAX];
         uint32_t value[FLEET_ENROUTE_MAX + PLANETS_MAX];
         int fleet_order[PLAYER_NUM];
+        int planets_order[PLAYER_NUM];
     } sorted;   /* global for qsort */
     uint8_t star_frame[PLANETS_MAX];
     ui_main_loop_action_t ui_main_loop_action;
