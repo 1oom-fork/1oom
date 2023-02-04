c92261b93b8e218dd9ee71b79de07407c21e6773
MyName <fork1oom@yandex.com>
2023-02-04 19:46:51 +0300

    uiextra: uiobj.c: Fix cursor keys behavior

--- src/ui/classic/uiobj.c
+++ src/ui/classic/uiobj.c
@@ -863,6 +863,9 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
     }
     if ((diry != 0) && (dirx == 0)) {
         for (int i = 1; i < uiobj_table_num; ++i) {
+            if (ui_extra_enabled) {
+                break;
+            }
             if (i == oi2) {
                 if (i != (uiobj_table_num - 1)) {
                     ++i;
@@ -883,7 +886,7 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                 }
             }
         }
-        if (oi == oi2) {
+        if (oi == oi2 || ui_extra_enabled) {
             for (int i = 1; i < uiobj_table_num; ++i) {
                 if (i == oi2) {
                     if (i != (uiobj_table_num - 1)) {
@@ -896,7 +899,7 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                 if (uiobj_kbd_dir_obj_ok(p)) {
                     dy = (diry < 0) ? (my - scmidy(p)) : (scmidy(p) - my);
                     dx = scmidx(p) - mx;
-                    if ((p->x0 * p->scale <= mx) && (p->x1 * p->scale >= mx) && ((dx < -6 * p->scale) || (dx > 6 * p->scale))) {
+                    if (!ui_extra_enabled && (p->x0 * p->scale <= mx) && (p->x1 * p->scale >= mx) && ((dx < -6 * p->scale) || (dx > 6 * p->scale))) {
                         dx = 6 * p->scale;
                     }
                     if (dx < 0) {
@@ -906,7 +909,11 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                         dx = UIOBJ_OFFSCREEN;
                     }
                     if (dy == 0) {
-                        dy = 1;
+                        if (ui_extra_enabled) {
+                            continue;
+                        } else {
+                            dy = 1;
+                        }
                     }
                     slope = (dx * 100) / dy;
                     if ((slope >= 0) && (slope < 0x2c)) {
@@ -922,6 +929,9 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
     }
     if ((dirx != 0) && (diry == 0)) {
         for (int i = 1; i < uiobj_table_num; ++i) {
+            if (ui_extra_enabled) {
+                break;
+            }
             if (i == oi2) {
                 if (i != (uiobj_table_num - 1)) {
                     ++i;
@@ -942,7 +952,7 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                 }
             }
         }
-        if (oi == oi2) {
+        if (oi == oi2 || ui_extra_enabled) {
             for (int i = 1; i < uiobj_table_num; ++i) {
                 if (i == oi2) {
                     if (i != (uiobj_table_num - 1)) {
@@ -955,7 +965,7 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                 if (uiobj_kbd_dir_obj_ok(p)) {
                     dx = (dirx < 0) ? (mx - scmidx(p)) : (scmidx(p) - mx);
                     dy = scmidy(p) - my;
-                    if ((p->y0 * p->scale <= my) && (p->y1 * p->scale >= my) && ((dy < -6 * p->scale) || (dy > 6 * p->scale))) {
+                    if (!ui_extra_enabled && (p->y0 * p->scale <= my) && (p->y1 * p->scale >= my) && ((dy < -6 * p->scale) || (dy > 6 * p->scale))) {
                         dy = 6 * p->scale;
                     }
                     if (dy < 0) {
@@ -965,7 +975,11 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                         dy = UIOBJ_OFFSCREEN;
                     }
                     if (dx == 0) {
-                        dx = 1;
+                        if (ui_extra_enabled) {
+                            continue;
+                        } else {
+                            dx = 1;
+                        }
                     }
                     slope = (dy * 100) / dx;
                     if ((slope >= 0) && (slope < 0x2c)) {
