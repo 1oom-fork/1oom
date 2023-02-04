3a38a656f18c550e94f2b2f1f815a3e0d3d723db
MyName <fork1oom@yandex.com>
2023-02-04 19:13:33 +0300

    Change numpad behavior for 2/4/6/8 buttons to better reflect expectations

--- src/ui/classic/uiobj.c
+++ src/ui/classic/uiobj.c
@@ -864,28 +864,7 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
         }
     }
     if ((diry != 0) && (dirx == 0)) {
-        for (int i = 1; i < uiobj_table_num; ++i) {
-            if (i == oi2) {
-                if (i != (uiobj_table_num - 1)) {
-                    ++i;
-                } else {
-                    break;
-                }
-            }
-            p = &uiobj_tbl[i];
-            if (uiobj_kbd_dir_obj_ok(p)) {
-                dy = (diry < 0) ? (my - scmidy(p)) : (scmidy(p) - my);
-                dx = scmidx(p) - mx;
-                if ((p->x0 * p->scale <= mx) && (p->x1 * p->scale >= mx) && ((dx < -6 * p->scale) || (dx > 6 * p->scale))) {
-                    dx = 6 * p->scale;
-                }
-                if ((dx > -6 * p->scale) && (dx < 6 * p->scale) && (dy > 0) && (dy < mind)) {
-                    mind = dy;
-                    oi = i;
-                }
-            }
-        }
-        if (oi == oi2) {
+        {
             for (int i = 1; i < uiobj_table_num; ++i) {
                 if (i == oi2) {
                     if (i != (uiobj_table_num - 1)) {
@@ -898,9 +877,6 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                 if (uiobj_kbd_dir_obj_ok(p)) {
                     dy = (diry < 0) ? (my - scmidy(p)) : (scmidy(p) - my);
                     dx = scmidx(p) - mx;
-                    if ((p->x0 * p->scale <= mx) && (p->x1 * p->scale >= mx) && ((dx < -6 * p->scale) || (dx > 6 * p->scale))) {
-                        dx = 6 * p->scale;
-                    }
                     if (dx < 0) {
                         dx = -dx;
                     }
@@ -908,11 +884,11 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                         dx = UIOBJ_OFFSCREEN;
                     }
                     if (dy == 0) {
-                        dy = 1;
+                        continue;
                     }
                     slope = (dx * 100) / dy;
                     if ((slope >= 0) && (slope < 0x2c)) {
-                        dist = (dx * dx) + (dy * dy);
+                        dist = (dx * dx * 2) + (dy * dy);
                         if (dist < mind) {
                             mind = dist;
                             oi = i;
@@ -923,28 +899,7 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
         }
     }
     if ((dirx != 0) && (diry == 0)) {
-        for (int i = 1; i < uiobj_table_num; ++i) {
-            if (i == oi2) {
-                if (i != (uiobj_table_num - 1)) {
-                    ++i;
-                } else {
-                    break;
-                }
-            }
-            p = &uiobj_tbl[i];
-            if (uiobj_kbd_dir_obj_ok(p)) {
-                dx = (dirx < 0) ? (mx - scmidx(p)) : (scmidx(p) - mx);
-                dy = scmidy(p) - my;
-                if ((p->y0 * p->scale <= my) && (p->y1 * p->scale >= my) && ((dy * p->scale < -6) || (dy * p->scale > 6))) {
-                    dy = 6 * p->scale;
-                }
-                if ((dy > -6 * p->scale) && (dy < 6 * p->scale) && (dx > 0) && (dx < mind)) {
-                    mind = dx;
-                    oi = i;
-                }
-            }
-        }
-        if (oi == oi2) {
+        {
             for (int i = 1; i < uiobj_table_num; ++i) {
                 if (i == oi2) {
                     if (i != (uiobj_table_num - 1)) {
@@ -957,9 +912,6 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                 if (uiobj_kbd_dir_obj_ok(p)) {
                     dx = (dirx < 0) ? (mx - scmidx(p)) : (scmidx(p) - mx);
                     dy = scmidy(p) - my;
-                    if ((p->y0 * p->scale <= my) && (p->y1 * p->scale >= my) && ((dy < -6 * p->scale) || (dy > 6 * p->scale))) {
-                        dy = 6 * p->scale;
-                    }
                     if (dy < 0) {
                         dy = -dy;
                     }
@@ -967,11 +919,11 @@ static int16_t uiobj_kbd_dir_key_dxdy(int dirx, int diry, int16_t oi2, int mx, i
                         dy = UIOBJ_OFFSCREEN;
                     }
                     if (dx == 0) {
-                        dx = 1;
+                        continue;
                     }
                     slope = (dy * 100) / dx;
                     if ((slope >= 0) && (slope < 0x2c)) {
-                        dist = (dx * dx) + (dy * dy);
+                        dist = (dx * dx) + (dy * dy * 2);
                         if (dist < mind) {
                             mind = dist;
                             oi = i;
