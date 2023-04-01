473fdc1ddc1aaa68193eccaf226b8ce1fce5d8c9
MyName <fork1oom@yandex.com>
2023-04-01 15:58:15 +0300

    uistarmap: Restore supposedly inline update_mouse_hover()

--- src/ui/classic/uistarmap_orbit_own.c
+++ src/ui/classic/uistarmap_orbit_own.c
@@ -470,24 +470,7 @@ void ui_starmap_orbit_own(struct game_s *g, player_id_t active_player)
             }
         }
         if (!flag_done) {
-            d.bottom_highlight = -1;
-            if (oi2 == d.oi_gameopts) {
-                d.bottom_highlight = 0;
-            } else if (oi2 == d.oi_design) {
-                d.bottom_highlight = 1;
-            } else if (oi2 == d.oi_fleet) {
-                d.bottom_highlight = 2;
-            } else if (oi2 == d.oi_map) {
-                d.bottom_highlight = 3;
-            } else if (oi2 == d.oi_races) {
-                d.bottom_highlight = 4;
-            } else if (oi2 == d.oi_planets) {
-                d.bottom_highlight = 5;
-            } else if (oi2 == d.oi_tech) {
-                d.bottom_highlight = 6;
-            } else if (oi2 == d.oi_next_turn) {
-                d.bottom_highlight = 7;
-            }
+            ui_starmap_update_mouse_hover(&d, oi2);
             ui_starmap_orbit_own_draw_cb(&d);
             uiobj_table_clear();
             UIOBJ_CLEAR_LOCAL();
