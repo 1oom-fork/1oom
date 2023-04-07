bdad8f3f2cd58a177b066a3e7740564b6568718a
MyName <fork1oom@yandex.com>
2023-04-08 00:39:18 +0300

    game.c: Make game_opt_skip_intro_always global

--- src/game/game.h
+++ src/game/game.h
@@ -337,4 +337,6 @@ static inline bool IS_HUMAN(const struct game_s *g, player_id_t i)
 #define IS_AI(_g_, _i_) BOOLVEC_IS1((_g_)->is_ai, (_i_))
 #define IS_ALIVE(_g_, _i_) ((_g_)->evn.home[(_i_)] != PLANET_NONE)
 
+extern bool game_opt_skip_intro_always;
+
 #endif
