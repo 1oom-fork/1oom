9187f4d82b52d84bfe1afb4c5667e40bf0ef8cc3
MyName <fork1oom@yandex.com>
2023-04-13 03:49:17 +0300

    uistarmap_common.c: Make Alt+r common except when planet_focus is hidden

--- src/ui/classic/uistarmap_common.h
+++ src/ui/classic/uistarmap_common.h
@@ -46,6 +46,7 @@ struct starmap_data_s {
     int16_t oi_next_turn;
     int16_t oi_alt_c;
     int16_t oi_alt_m;
+    int16_t oi_alt_r;
     int16_t oi_tbl_stars[PLANETS_MAX];
     int16_t oi_scroll;
     int16_t oi_ctrl_left;
@@ -126,6 +127,7 @@ struct starmap_data_s {
         d.oi_next_turn = UIOBJI_INVALID; \
         d.oi_alt_c = UIOBJI_INVALID; \
         d.oi_alt_m = UIOBJI_INVALID; \
+        d.oi_alt_r = UIOBJI_INVALID; \
         for (int i = 0; i < g->galaxy_stars; ++i) { \
             d.oi_tbl_stars[i] = UIOBJI_INVALID; \
         } \
