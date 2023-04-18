386fd7f058afa6a1d76426741939c90db2092e07
MyName <fork1oom@yandex.com>
2023-04-18 10:31:36 +0300

    uistarmap_reloc.c: Fix ruler color

--- src/ui/classic/uistarmap_reloc.c
+++ src/ui/classic/uistarmap_reloc.c
@@ -35,9 +35,15 @@ static void ui_starmap_reloc_draw_cb(void *vptr)
     y0 = (pf->y - ui_data.starmap.y) * 2 + 8;
     if (g->planet_focus_i[d->api] != d->rl.from) {
         int x1, y1;
+        const uint8_t *ctbl;
         x1 = (pt->x - ui_data.starmap.x) * 2 + 14;
         y1 = (pt->y - ui_data.starmap.y) * 2 + 14;
-        ui_draw_line_limit_ctbl(x0 + 6, y0 + 6, x1, y1, colortbl_line_green, 5, ui_data.starmap.line_anim_phase);
+        if (game_reloc_dest_ok(g, g->planet_focus_i[d->api], d->api)) {
+            ctbl = colortbl_line_green;
+        } else {
+            ctbl = colortbl_line_red;
+        }
+        ui_draw_line_limit_ctbl(x0 + 6, y0 + 6, x1, y1, ctbl, 5, ui_data.starmap.line_anim_phase);
     }
     lbxgfx_draw_frame_offs_delay(x0, y0, !d->anim_delay, ui_data.gfx.starmap.planbord, 6, 6, 221, 177, UI_SCREEN_W);
     lbxgfx_draw_frame(222, 80, ui_data.gfx.starmap.relocate, UI_SCREEN_W);
