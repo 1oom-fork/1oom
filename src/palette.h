1c92eb270e08dae4b957d443a4e06746d77ead83
MyName <fork1oom@yandex.com>
2023-05-18 12:15:39 +0300

    hw_video: Move 6bit palette to palette.c/h

---
+++ src/palette.h
@@ -0,0 +1,25 @@
+#ifndef INC_1OOM_PALETTE_H
+#define INC_1OOM_PALETTE_H
+
+#include <string.h>
+
+#include "types.h"
+
+extern uint8_t ui_palette[256*3]; /* 6bit palette from the game */
+
+static inline void ui_palette_clear(void)
+{
+    memset(ui_palette, 0, sizeof(ui_palette));
+}
+
+static inline void ui_palette_set_byte(int i, uint8_t b)
+{
+    ui_palette[i] = b & 0x3f;
+}
+
+static inline void ui_palette_set(const uint8_t *pal, int first, int num)
+{
+    memcpy(&ui_palette[first * 3], pal, num * 3);
+}
+
+#endif
