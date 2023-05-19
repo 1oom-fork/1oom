7651b5afaeb9ff1b50cbcd8eeee2c49f1a7e83aa
MyName <fork1oom@yandex.com>
2023-05-19 13:29:33 +0300

    Allegro4: Add option to run in windowed mode

--- src/hw/alleg/hwalleg_opt.c
+++ src/hw/alleg/hwalleg_opt.c
@@ -12,8 +12,20 @@
 
 /* -------------------------------------------------------------------------- */
 
+#ifndef IS_MSDOS
+bool hw_opt_fullscreen = false;
+#endif
+
 /* -------------------------------------------------------------------------- */
 
 const struct cmdline_options_s hw_cmdline_options[] = {
+#ifndef IS_MSDOS
+    { "-fs", 0,
+      options_enable_bool_var, (void *)&hw_opt_fullscreen,
+      NULL, "Enable fullscreen" },
+    { "-window", 0,
+      options_disable_bool_var, (void *)&hw_opt_fullscreen,
+      NULL, "Use windowed mode" },
+#endif
     { NULL, 0, NULL, NULL, NULL, NULL }
 };
