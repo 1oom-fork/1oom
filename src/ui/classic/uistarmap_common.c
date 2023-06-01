770b442f0f1c95f91300193b69695f0b7038ba6e
MyName <fork1oom@yandex.com>
2023-06-01 10:59:10 +0300

    game_planet_get_slider_text: Fix context distortion

--- src/ui/classic/uistarmap_common.c
+++ src/ui/classic/uistarmap_common.c
@@ -173,7 +173,7 @@ static void ui_starmap_draw_sliders_and_prod(struct starmap_data_s *d)
     lbxfont_select(2, 6, 0, 0);
     {
         int v;
-        v = game_planet_get_slider_text(g, p, d->api, PLANET_SLIDER_SHIP, buf, sizeof(buf));
+        v = game_planet_get_slider_text(g, p, PLANET_SLIDER_SHIP, buf, sizeof(buf));
         lbxfont_print_str_right(x, 83, buf, UI_SCREEN_W, ui_scale);
         lbxfont_select(0, 0xd, 0, 0);
         if (v >= 0) {
@@ -181,13 +181,13 @@ static void ui_starmap_draw_sliders_and_prod(struct starmap_data_s *d)
         }
     }
     lbxfont_select(2, 6, 0, 0);
-    game_planet_get_slider_text(g, p, d->api, PLANET_SLIDER_DEF, buf, sizeof(buf));
+    game_planet_get_slider_text(g, p, PLANET_SLIDER_DEF, buf, sizeof(buf));
     lbxfont_print_str_right(x, 94, buf, UI_SCREEN_W, ui_scale);
-    game_planet_get_slider_text(g, p, d->api, PLANET_SLIDER_IND, buf, sizeof(buf));
+    game_planet_get_slider_text(g, p, PLANET_SLIDER_IND, buf, sizeof(buf));
     lbxfont_print_str_right(x, 105, buf, UI_SCREEN_W, ui_scale);
     {
         int v;
-        v = game_planet_get_slider_text_eco(g, p, d->api, ui_extra_enabled, buf, sizeof(buf));
+        v = game_planet_get_slider_text_eco(g, p, ui_extra_enabled, buf, sizeof(buf));
         lbxfont_print_str_right(x, 116, buf, UI_SCREEN_W, ui_scale);
         if (v >= 0) {
             if (ui_extra_enabled) {
@@ -204,7 +204,7 @@ static void ui_starmap_draw_sliders_and_prod(struct starmap_data_s *d)
     }
     {
         int v;
-        v = game_planet_get_slider_text(g, p, d->api, PLANET_SLIDER_TECH, buf, sizeof(buf));
+        v = game_planet_get_slider_text(g, p, PLANET_SLIDER_TECH, buf, sizeof(buf));
         if (v > 9999) {
             ui_draw_filled_rect(288, 127, 312, 132, 7, ui_scale);
         } else {
