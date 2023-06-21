84cc5251e8c68616195225a08b5cbb544529b718
MyName <fork1oom@yandex.com>
2023-06-21 07:58:06 +0300

    SDL: Change scope of hwsdl_video.c

--- src/hw/sdl/2/hwsdl2_video.c
+++ src/hw/sdl/2/hwsdl2_video.c
@@ -80,6 +80,10 @@ static struct sdl_video_s {
 
 /* -------------------------------------------------------------------------- */
 
+#include "hwsdl_video.c"
+
+/* -------------------------------------------------------------------------- */
+
 static void video_create_upscaled_texture(bool force)
 {
     if (!hw_opt_allow_upscaling) {
@@ -655,5 +659,3 @@ int hw_icon_set(const uint8_t *data, const uint8_t *pal, int w, int h)
     video.icon = icon;
     return 0;
 }
-
-#include "hwsdl_video.c"
