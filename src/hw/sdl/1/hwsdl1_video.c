93aac705865fbabeb7d0d773040c3d5568800c94
MyName <fork1oom@yandex.com>
2023-06-21 07:58:06 +0300

    SDL: Change scope of hwsdl_video.c

--- src/hw/sdl/1/hwsdl1_video.c
+++ src/hw/sdl/1/hwsdl1_video.c
@@ -54,6 +54,10 @@ static struct sdl_video_s {
 
 /* -------------------------------------------------------------------------- */
 
+#include "hwsdl_video.c"
+
+/* -------------------------------------------------------------------------- */
+
 static void video_render_8bpp(int bufi)
 {
     int pitch = video.screen->pitch;
@@ -538,5 +542,3 @@ int hw_icon_set(const uint8_t *data, const uint8_t *pal, int w, int h)
     mask = NULL;
     return 0;
 }
-
-#include "hwsdl_video.c"
