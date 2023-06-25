317b4c4a9119451a8506fe3c806ca04b2e2ed73b
MyName <fork1oom@yandex.com>
2023-06-25 11:30:05 +0300

    SDL: Add borderless option

--- src/hw/sdl/1/hwsdl1_video.c
+++ src/hw/sdl/1/hwsdl1_video.c
@@ -231,6 +231,9 @@ static int video_sw_set(int w, int h)
 {
     int flags;
     flags = SDL_SWSURFACE | SDL_DOUBLEBUF;
+    if (hw_opt_borderless) {
+        flags |= SDL_NOFRAME;
+    }
     if (hw_opt_fullscreen) {
         flags |= SDL_FULLSCREEN;
     }
@@ -264,6 +267,10 @@ int hw_video_resize(int w, int h)
     }
 
     flags = SDL_OPENGL | SDL_RESIZABLE;
+
+    if (hw_opt_borderless) {
+        flags |= SDL_NOFRAME;
+    }
 
     if (hw_opt_fullscreen) {
         flags |= SDL_FULLSCREEN;
