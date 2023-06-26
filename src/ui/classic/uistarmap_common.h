810fa471ffd9720dd71b491ae74fd37a021c89dc
MyName <fork1oom@yandex.com>
2023-06-27 01:18:48 +0300

    uistarmap_enroute.c: Replace en.in_frange with ui_starmap_enroute_in_frange
    Note: this restores the original behavior

--- src/ui/classic/uistarmap_common.h
+++ src/ui/classic/uistarmap_common.h
@@ -112,7 +112,6 @@ struct starmap_data_s {
         } oe;   /* orbit_en */
         struct {
             struct shipnon0_s sn0;
-            bool in_frange;
             uint8_t from;
             uint8_t pon;
             int frame_ship;
