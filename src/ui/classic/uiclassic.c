05d4cece083a60be8b388b84d03a3314462ff425
MyName <fork1oom@yandex.com>
2023-09-13 21:14:52 +0300

    Split ui_extra: Starmap expanded scrolling

--- src/ui/classic/uiclassic.c
+++ src/ui/classic/uiclassic.c
@@ -37,6 +37,7 @@ void ui_extra_toggle_preset(bool enabled)
     ui_fixbugs_enabled = enabled;
     ui_load_opts_extra = enabled;
     ui_space_combat_autoresolve = enabled;
+    ui_sm_expanded_scroll = enabled;
 }
 
 static bool check_ui_icon(void *var)
@@ -66,6 +67,7 @@ const struct cfg_items_s ui_cfg_items[] = {
     CFG_ITEM_BOOL("uifixbugs", &ui_fixbugs_enabled),
     CFG_ITEM_BOOL("load_opts_extra", &ui_load_opts_extra),
     CFG_ITEM_BOOL("space_combat_autoresolve", &ui_space_combat_autoresolve),
+    CFG_ITEM_BOOL("sm_expanded_scroll", &ui_sm_expanded_scroll),
     CFG_ITEM_COMMENT("0..146"),
     CFG_ITEM_INT("uiicon", &ui_icon, check_ui_icon),
     CFG_ITEM_COMMENT("Invert mouse wheel for sliders"),
@@ -129,6 +131,7 @@ bool ui_extra_enabled = false;
 bool ui_fixbugs_enabled = false;
 bool ui_load_opts_extra = false;
 bool ui_space_combat_autoresolve = false;
+bool ui_sm_expanded_scroll = false;
 bool ui_mwi_slider = false;
 bool ui_mwi_counter = false;
 int ui_sm_scroll_speed = 3;
