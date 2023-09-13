d8942342bb3370654c5bb875c571c4aad7dac261
MyName <fork1oom@yandex.com>
2023-09-13 21:14:52 +0300

    Split ui_extra: Starmap expanded scrolling

--- src/ui/classic/uidefs.h
+++ src/ui/classic/uidefs.h
@@ -278,6 +278,7 @@ extern int ui_screen_h;
 extern bool ui_extra_enabled;
 extern bool ui_load_opts_extra;
 extern bool ui_space_combat_autoresolve;
+extern bool ui_sm_expanded_scroll;
 extern bool ui_mwi_slider;
 extern bool ui_mwi_counter;
 extern int ui_sm_scroll_speed;
