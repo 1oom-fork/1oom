98a02507186e174b126c851ef765e55a1d71162f
MyName <fork1oom@yandex.com>
2023-09-13 11:20:20 +0300

    Split ui_extra: Starmap distance tooltip

--- src/ui/classic/uiclassic.c
+++ src/ui/classic/uiclassic.c
@@ -35,6 +35,7 @@ static int ui_icon = 146/*guardian*/;
 void ui_extra_toggle_preset(bool enabled)
 {
     ui_extra_enabled = enabled;
+    ui_sm_distance_tooltip = enabled;
 }
 
 static bool check_ui_scale(void *var)
@@ -73,6 +74,7 @@ static bool check_ui_sm_scroll_speed(void *var)
 const struct cfg_items_s ui_cfg_items[] = {
     CFG_ITEM_INT("uiscale", &ui_scale, check_ui_scale),
     CFG_ITEM_BOOL("uiextra", &ui_extra_enabled),
+    CFG_ITEM_BOOL("sm_distance_tooltip", &ui_sm_distance_tooltip),
     CFG_ITEM_COMMENT("0..146"),
     CFG_ITEM_INT("uiicon", &ui_icon, check_ui_icon),
     CFG_ITEM_COMMENT("Invert mouse wheel for sliders"),
@@ -144,6 +146,7 @@ int ui_screen_h = 0;
 int ui_scale = 0;
 int starmap_scale = 0;
 bool ui_extra_enabled = false;
+bool ui_sm_distance_tooltip = false;
 bool ui_mwi_slider = false;
 bool ui_mwi_counter = false;
 int ui_sm_scroll_speed = 3;
