b18244c9e37f07702f6b418085bb3d9da7624133
MyName <fork1oom@yandex.com>
2023-09-20 20:46:07 +0300

    Split ui_extra: Governor

--- src/ui/classic/uidefs.h
+++ src/ui/classic/uidefs.h
@@ -277,6 +277,7 @@ extern int starmap_scale;
 extern int ui_screen_w;
 extern int ui_screen_h;
 extern bool ui_extra_enabled;
+extern bool ui_governor_enabled;
 extern bool ui_load_opts_extra;
 extern bool ui_space_combat_autoresolve;
 extern bool ui_sm_expanded_scroll;
