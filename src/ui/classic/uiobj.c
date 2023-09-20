0015d1043b8d44ebe324d73c03981b55c3a7fb65
MyName <fork1oom@yandex.com>
2023-09-20 19:02:36 +0300

    Split ui_extra: Mouse warp disabled

--- src/ui/classic/uiobj.c
+++ src/ui/classic/uiobj.c
@@ -1234,7 +1234,7 @@ static void uiobj_click_obj(int16_t oi, int mx, int my)
                 mx = moouse_x;
                 my = moouse_y;
             }
-            if (!ui_extra_enabled) {
+            if (!ui_mouse_warp_disabled) {
                 mouse_set_xy(mx, my);
             }
         }
@@ -1813,7 +1813,7 @@ void uiobj_set_focus_forced(int16_t uiobji)
 
 void uiobj_set_focus(int16_t uiobji)
 {
-    if (!ui_extra_enabled) {
+    if (!ui_mouse_warp_disabled) {
         uiobj_set_focus_forced(uiobji);
     }
 }
