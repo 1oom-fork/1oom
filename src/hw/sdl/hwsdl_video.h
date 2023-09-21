5492859254397a2abcabae0171b18c5c3d9fb1e7
MyName <fork1oom@yandex.com>
2023-09-21 05:09:18 +0300

    SDL2: Precise window resizing

--- src/hw/sdl/hwsdl_video.h
+++ src/hw/sdl/hwsdl_video.h
@@ -2,6 +2,8 @@
 #define INC_1OOM_HWSDL_VIDEO_H
 
 extern int hw_video_resize(int w, int h);
+extern void hw_video_shrink(void);
+extern void hw_video_enlarge(void);
 extern bool hw_video_toggle_fullscreen(void);
 extern void hw_video_input_grab(bool grab);
 
