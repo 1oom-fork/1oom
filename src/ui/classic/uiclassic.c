f1039bce5ece4db4576dd6a17b912fcd44283fb2
MyName <fork1oom@yandex.com>
2023-10-05 14:19:21 +0300

    uimainmenu.c: Toggle game_opt_skip_intro_always on Alt+x

--- src/ui/classic/uiclassic.c
+++ src/ui/classic/uiclassic.c
@@ -39,6 +39,7 @@ void ui_extra_toggle_preset(bool enabled)
     ui_game_opts_extra = enabled;
     ui_load_opts_extra = enabled;
     ui_new_game_opts_extra = enabled;
+    game_opt_skip_intro_always = enabled;
     game_planet_leaving_trans_fix = enabled;
     ui_governor_enabled = enabled;
     ui_space_combat_autoresolve = enabled;
