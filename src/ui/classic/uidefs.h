75b5fa30119229a6b185019189dd09b1d2692ebd
MyName <fork1oom@yandex.com>
2023-10-11 16:26:13 +0300

    Split ui_extra: Starmap explicit cursor context

--- src/ui/classic/uidefs.h
+++ src/ui/classic/uidefs.h
@@ -280,6 +280,7 @@ extern bool ui_space_combat_autoresolve;
 extern bool ui_sm_ships_enabled;
 extern bool ui_sm_expanded_scroll;
 extern bool ui_sm_no_question_mark_cursor;
+extern bool ui_sm_explicit_cursor_context;
 extern bool ui_mouse_lmb_fix;
 extern bool ui_mouse_warp_disabled;
 extern bool ui_sm_mouseover_focus;
