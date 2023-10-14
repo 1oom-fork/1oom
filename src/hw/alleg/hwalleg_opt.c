6d980a6ccce3b759c6ccef1f626241d005a86a9f
MyName <fork1oom@yandex.com>
2023-10-14 20:38:01 +0300

    Allegro4: Fix mouse speed

--- src/hw/alleg/hwalleg_opt.c
+++ src/hw/alleg/hwalleg_opt.c
@@ -13,11 +13,15 @@
 /* -------------------------------------------------------------------------- */
 
 bool hw_opt_fullscreen = true;
+int hw_opt_mouse_slowdown_x = 1;
+int hw_opt_mouse_slowdown_y = 1;
 
 /* -------------------------------------------------------------------------- */
 
 const struct cfg_items_s hw_cfg_items[] = {
     CFG_ITEM_BOOL("fs", &hw_opt_fullscreen),
+    CFG_ITEM_INT("mouse_slowdown_x", &hw_opt_mouse_slowdown_x, NULL),
+    CFG_ITEM_INT("mouse_slowdown_y", &hw_opt_mouse_slowdown_y, NULL),
     CFG_ITEM_END
 };
 
--- src/hw/alleg/hwalleg_opt.h
+++ src/hw/alleg/hwalleg_opt.h
@@ -2,5 +2,7 @@
 #define INC_1OOM_HWSDL_OPT_H
 
 extern bool hw_opt_fullscreen;
+extern int hw_opt_mouse_slowdown_x;
+extern int hw_opt_mouse_slowdown_y;
 
 #endif
