9c2027cba48a85376b71775d43792e30f9293f24
MyName <fork1oom@yandex.com>
2023-10-14 23:51:35 +0300

    Allegro4: Add absolute mouse
    Note: For older Android devices, you need to disable mouse warp
    in the game or abandon the absolute mouse

--- src/hw/alleg/hwalleg_opt.c
+++ src/hw/alleg/hwalleg_opt.c
@@ -13,6 +13,7 @@
 /* -------------------------------------------------------------------------- */
 
 bool hw_opt_fullscreen = true;
+bool hw_opt_relmouse = true;
 int hw_opt_mouse_slowdown_x = 1;
 int hw_opt_mouse_slowdown_y = 1;
 
@@ -20,6 +21,7 @@ int hw_opt_mouse_slowdown_y = 1;
 
 const struct cfg_items_s hw_cfg_items[] = {
     CFG_ITEM_BOOL("fs", &hw_opt_fullscreen),
+    CFG_ITEM_BOOL("relmouse", &hw_opt_relmouse),
     CFG_ITEM_INT("mouse_slowdown_x", &hw_opt_mouse_slowdown_x, NULL),
     CFG_ITEM_INT("mouse_slowdown_y", &hw_opt_mouse_slowdown_y, NULL),
     CFG_ITEM_END
