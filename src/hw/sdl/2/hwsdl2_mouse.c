df475a1a38fcd1ef7293762ad774c410e3f4fa3d
MyName <fork1oom@yandex.com>
2023-10-15 23:26:00 +0300

    SDL2: Add absolute mouse
    Note: This feature requires SDL2 2.0.18+
    Based on Ignatius implementation and Arho Mahlamäki
    attempts to implement mouse warp

--- src/hw/sdl/2/hwsdl2_mouse.c
+++ src/hw/sdl/2/hwsdl2_mouse.c
@@ -1,10 +1,17 @@
 #include "config.h"
+#include "types.h"
 
 #include "SDL.h"
+
+#include "hwsdl2_video.h"
 
 #include "hwsdl_mouse.c"
 
 void hw_mouse_set_xy(int mx, int my)
 {
-    mouse_set_xy_from_hw(mx, my);
+    if (hw_opt_relmouse) {
+        mouse_set_xy_from_hw(mx, my);
+    } else {
+        hw_video_mouse_warp(mx, my);
+    }
 }
