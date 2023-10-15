2923af9959fe04c8000f0d16d89dace28842b82a
MyName <fork1oom@yandex.com>
2023-10-15 23:26:00 +0300

    SDL2: Add absolute mouse
    Note: This feature requires SDL2 2.0.18+
    Based on Ignatius implementation and Arho Mahlamäki
    attempts to implement mouse warp

--- src/hw/sdl/2/hwsdl2_opt.c
+++ src/hw/sdl/2/hwsdl2_opt.c
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 
+#include <SDL.h>
+
 #include "hw.h"
 #include "cfg.h"
 #include "hwsdl_opt.h"
@@ -18,7 +20,11 @@
 
 bool hw_opt_force_sw = false;
 bool hw_opt_int_scaling = false;
-bool hw_opt_relmouse = true;
+#if SDL_VERSION_ATLEAST(2, 0, 18)
+    bool hw_opt_relmouse = false;
+#else
+    bool hw_opt_relmouse = true;
+#endif
 bool hw_opt_autotrim = true;
 bool hw_opt_vsync = true;
 bool hw_opt_allow_upscaling = true;
@@ -61,7 +67,7 @@ const struct cmdline_options_s hw_cmdline_options_extra[] = {
       NULL, "Use relative mouse mode (default)" },
     { "-norelmouse", 0,
       options_disable_bool_var, (void *)&hw_opt_relmouse,
-      NULL, "Do not use relative mouse mode (for testing)" },
+      NULL, "Do not use relative mouse mode" },
     { "-autotrim", 0,
       options_enable_bool_var, &hw_opt_autotrim,
       NULL, "Enable automatic resize" },
