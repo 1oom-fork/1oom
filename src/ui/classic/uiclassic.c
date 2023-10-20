414531d3adac188e5c3e4d00033de6f4575137bc
MyName <fork1oom@yandex.com>
2023-10-20 03:40:45 +0300

    Split ui_extra: Starmap ships UI

--- src/ui/classic/uiclassic.c
+++ src/ui/classic/uiclassic.c
@@ -38,6 +38,7 @@ void ui_extra_toggle_preset(bool enabled)
     ui_load_opts_extra = enabled;
     game_opt_skip_intro_always = enabled;
     ui_space_combat_autoresolve = enabled;
+    ui_sm_ships_enabled = enabled;
     ui_sm_distance_tooltip = enabled;
     ui_sm_expanded_scroll = enabled;
     ui_sm_no_question_mark_cursor = enabled;
@@ -85,6 +86,7 @@ const struct cfg_items_s ui_cfg_items[] = {
     CFG_ITEM_BOOL("illogical_hotkey_fix", &ui_illogical_hotkey_fix),
     CFG_ITEM_BOOL("load_opts_extra", &ui_load_opts_extra),
     CFG_ITEM_BOOL("space_combat_autoresolve", &ui_space_combat_autoresolve),
+    CFG_ITEM_BOOL("ui_sm_ships_enabled", &ui_sm_ships_enabled),
     CFG_ITEM_BOOL("sm_expanded_scroll", &ui_sm_expanded_scroll),
     CFG_ITEM_BOOL("sm_distance_tooltip", &ui_sm_distance_tooltip),
     CFG_ITEM_BOOL("sm_no_question_mark_cursor", &ui_sm_no_question_mark_cursor),
@@ -166,6 +168,7 @@ bool ui_extra_enabled = false;
 bool ui_illogical_hotkey_fix = false;
 bool ui_load_opts_extra = false;
 bool ui_space_combat_autoresolve = false;
+bool ui_sm_ships_enabled = false;
 bool ui_sm_expanded_scroll = false;
 bool ui_sm_distance_tooltip = false;
 bool ui_sm_no_question_mark_cursor = false;
