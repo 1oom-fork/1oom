5e19f7950cc250756b57e4eaef27225023952543
MyName <fork1oom@yandex.com>
2023-12-14 08:27:48 +0300

    Add game_cheat_elections

--- src/game/game_aux.c
+++ src/game/game_aux.c
@@ -94,6 +94,7 @@ int game_aux_init(struct game_aux_s *gaux, struct game_s *g)
 
     gaux->move_temp = 0;
     gaux->flag_cheat_galaxy = false;
+    gaux->flag_cheat_elections = false;
     gaux->flag_cheat_events = false;
     gaux->flag_cheat_stars = false;
     gaux->initialized = true;
