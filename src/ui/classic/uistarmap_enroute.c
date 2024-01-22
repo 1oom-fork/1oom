35ded22a74b7765195aef2c81addc3e3209ba734
MyName <fork1oom@yandex.com>
2024-01-23 01:29:28 +0300

    uistarmap: Revert common drawing code to match the original

--- src/ui/classic/uistarmap_enroute.c
+++ src/ui/classic/uistarmap_enroute.c
@@ -51,8 +51,7 @@ static void ui_starmap_enroute_draw_cb(void *vptr)
     const planet_t *pt = &g->planet[pto];
     char buf[0x80];
 
-    ui_starmap_draw_starmap(d);
-    ui_starmap_draw_button_text(d, true);
+    ui_starmap_draw_basic(d);
     {
         int x, y;
         x = (r->x - ui_data.starmap.x) * 2 + 5;
