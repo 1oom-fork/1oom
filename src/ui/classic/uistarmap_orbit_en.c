4deb5a392d51c4c406115e7b5229859202f9d0cb
MyName <fork1oom@yandex.com>
2024-01-23 01:29:28 +0300

    uistarmap: Revert common drawing code to match the original

--- src/ui/classic/uistarmap_orbit_en.c
+++ src/ui/classic/uistarmap_orbit_en.c
@@ -36,8 +36,7 @@ static void ui_starmap_orbit_en_draw_cb(void *vptr)
     char buf[0x80];
     STARMAP_LIM_INIT();
 
-    ui_starmap_draw_starmap(d);
-    ui_starmap_draw_button_text(d, true);
+    ui_starmap_draw_basic(d);
     {
         int x, y;
         x = (p->x - ui_data.starmap.x) * 2 + 23;
