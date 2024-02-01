ec8d09a2caff9d9212d998d0cb885586e793830c
MyName <fork1oom@yandex.com>
2024-02-01 08:39:18 +0300

    uicaught: Fix extra semicolon

--- src/ui/classic/uicaught.c
+++ src/ui/classic/uicaught.c
@@ -57,7 +57,7 @@ static void caught_draw_cb(void *vptr)
     for (int i = 0; i < g->players; ++i) {
         if ((i != d->api) && BOOLVEC_IS1(e->contact, i)) {
             int v;
-            lib_sprintf(buf, sizeof(buf), "%s:", game_str_tbl_race[g->eto[i].race]);;
+            lib_sprintf(buf, sizeof(buf), "%s:", game_str_tbl_race[g->eto[i].race]);
             lbxfont_print_str_normal(x + 11, y, buf, UI_SCREEN_W, ui_scale);
             v = g->evn.spies_caught[i][d->api];
             lbxfont_print_num_normal(x + 68, y, v, UI_SCREEN_W, ui_scale);
