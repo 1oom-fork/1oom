2da012ab65e939242bb876883baa909fbd9476be
MyName <fork1oom@yandex.com>
2024-02-19 07:36:16 +0300

    Implement copy protection dialog

--- src/game/game_turn.c
+++ src/game/game_turn.c
@@ -37,6 +37,8 @@
 
 /* -------------------------------------------------------------------------- */
 
+int copyprot_status = 0;
+
 static void game_turn_limit_ships(struct game_s *g)
 {
     for (int ei = 0; ei < g->enroute_num; ++ei) {
@@ -1739,11 +1741,9 @@ struct game_end_s game_turn_process(struct game_s *g)
         BOOLVEC_TBL_COPY1(old_contact, g->eto[i].within_frange, i, PLAYER_NUM);
         old_focus[i] = g->planet_focus_i[i];
     }
-#if 0
     if ((g->year > 40) && (!rnd_0_nm1(30, &g->seed)) && (copyprot_status == 0)) {
         copyprot_status = 1;
     }
-#endif
     game_turn_countdown_ceasefire(g);
     /* temp_turn_hmm3 = 0; */
     game_turn_update_mood_blunder(g);
@@ -1857,11 +1857,9 @@ struct game_end_s game_turn_process(struct game_s *g)
     }
     game_fleet_unrefuel(g);
     game_update_production(g);
-#if 0
     if (copyprot_status == 1) {
-        copyprotection_check();
+        ui_copyprotection_check(g);
     }
-#endif
     for (player_id_t i = PLAYER_0; i < g->players; ++i) {
         game_update_eco_on_waste(g, i, false);
     }
@@ -1871,12 +1869,10 @@ struct game_end_s game_turn_process(struct game_s *g)
         g->planet_focus_i[i] = old_focus[i];
     }
     ++g->year;
-#if 0
     if (copyprot_status == 1) {
-        copyprotection_lose(&game_end);
+        ui_copyprotection_lose(g, &game_end);
         return game_end;
     }
-#endif
     game_turn_show_newships(g);
     /* TODO autosave */
     game_update_tech_util(g);
