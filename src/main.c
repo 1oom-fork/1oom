8376a73cac4719a3eebecee9254cec00160229ae
MyName <fork1oom@yandex.com>
2024-03-05 16:15:20 +0300

    Implement thanks_for_playing message

--- src/main.c
+++ src/main.c
@@ -44,6 +44,7 @@ static void main_shutdown(void)
     ui_shutdown();
     hw_shutdown();
     os_shutdown();
+    log_message("Thanks for playing Master of Orion.\n");
 }
 
 /* -------------------------------------------------------------------------- */
