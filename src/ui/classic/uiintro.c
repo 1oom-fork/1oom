68db78815057730aba1643233eff08084719c361
MyName <fork1oom@yandex.com>
2024-04-06 19:53:37 +0300

    Fix 1oom typos (branching)

--- src/ui/classic/uiintro.c
+++ src/ui/classic/uiintro.c
@@ -30,7 +30,7 @@ void ui_play_intro(void)
 {
     int16_t uiobji_now, uiobji_ma;
     uint16_t frame;
-    bool flag_skip, flag_fadein;
+    bool flag_skip, flag_fadein = false;
     uint8_t *intro_sfx1;
     uint8_t *intro_sfx3;
     uint8_t *intro_sfx5;
@@ -114,6 +114,7 @@ void ui_play_intro(void)
         old_gfx = intro_gfx;
         ui_palette_fadeout_14_14_2();
         frame = 0;
+        flag_fadein = false;
     }
 
     while ((frame < 0x1e) && (!flag_skip)) {
@@ -161,6 +162,7 @@ void ui_play_intro(void)
         old_gfx = intro_gfx;
         ui_palette_fadeout_14_14_2();
         frame = 0;
+        flag_fadein = false;
     }
 
     while ((frame < 0x46) && (!flag_skip)) {
@@ -211,6 +213,7 @@ void ui_play_intro(void)
         old_gfx = intro_gfx;
         ui_palette_fadeout_14_14_2();
         frame = 0;
+        flag_fadein = false;
     }
 
     while ((frame < 0x45) && (!flag_skip)) {
@@ -258,6 +261,7 @@ void ui_play_intro(void)
         old_gfx = intro_gfx;
         ui_palette_fadeout_14_14_2();
         frame = 0;
+        flag_fadein = false;
     }
 
     while ((frame < 0x28) && (!flag_skip)) {
@@ -301,6 +305,7 @@ void ui_play_intro(void)
         old_gfx = intro_gfx;
         ui_palette_fadeout_14_14_2();
         frame = 0;
+        flag_fadein = false;
     }
 
     while ((frame < 0x1e) && (!flag_skip)) {
@@ -339,6 +344,7 @@ void ui_play_intro(void)
         old_gfx = intro_gfx;
         ui_palette_fadeout_14_14_2();
         frame = 0;
+        flag_fadein = false;
     }
 
     while ((frame < 0xb4) && (!flag_skip)) {
