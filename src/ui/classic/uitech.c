17eb1160a2255ab24a758759ff71ef30a6cfd302
MyName <fork1oom@yandex.com>
2024-04-17 05:29:24 +0300

    Fix 1oom typos (cosmetics)

--- src/ui/classic/uitech.c
+++ src/ui/classic/uitech.c
@@ -226,7 +226,9 @@ static void tech_draw_cb(void *vptr)
                 y0 = y + (8 - (complpercent * 4) / 50);
                 y1 = y + 7;
                 vgabuf_limits_set(0, y0, VGABUF_W - 1, y1);
-                lbxgfx_draw_frame_offs(287, y, ui_data.gfx.screens.litebulb_on);
+                if (((complpercent * 4) / 50) > 0) {
+                    lbxgfx_draw_frame_offs(287, y, ui_data.gfx.screens.litebulb_on);
+                }
                 vgabuf_limits_set_all();
             }
         } else {
