dcce7cc5b20be0239deaa84f24b6266b0995708c
MyName <fork1oom@yandex.com>
2024-04-20 16:50:01 +0300

    Add a bug with fleet construction as a separate rule

--- src/game/game_num.h
+++ src/game/game_num.h
@@ -60,6 +60,7 @@ extern bool game_num_colonized_factories_fix;
 extern bool game_num_cond_switch_to_ind_fix;
 extern bool game_num_slider_eco_done_fix;
 extern bool game_num_ship_scanner_fix;
+extern bool game_num_ai_fleet_cheating_fix;
 extern uint8_t game_num_tbl_hull_w[4];
 extern uint8_t game_num_tech_costmuld[DIFFICULTY_NUM];
 extern uint8_t game_num_tech_costmula[DIFFICULTY_NUM];
