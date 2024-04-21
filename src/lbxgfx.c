e5dd011333b42473fa410bbc692d395f8d8f0f6b
MyName <fork1oom@yandex.com>
2024-04-21 14:50:05 +0300

    uistarmap: Fix animation delay for planbord/shipbord gfx

--- src/lbxgfx.c
+++ src/lbxgfx.c
@@ -356,7 +356,7 @@ void lbxgfx_draw_frame_pal(int x, int y, uint8_t *data, uint16_t pitch)
     lbxgfx_draw_frame_do(p, data, pitch);
 }
 
-void lbxgfx_draw_frame_offs(int x, int y, uint8_t *data, uint16_t pitch)
+void lbxgfx_draw_frame_offs_delay(int x, int y, bool next, uint8_t *data, uint16_t pitch)
 {
     int xskip, yskip, x0, y0, x1, y1, w, h;
     int lx0 = gfxlim_minx, ly0 = gfxlim_miny, lx1 = gfxlim_maxx, ly1 = gfxlim_maxy;
@@ -403,11 +403,18 @@ void lbxgfx_draw_frame_offs(int x, int y, uint8_t *data, uint16_t pitch)
         lbxgfx_draw_pixels_offs_fmt1(x0, y0, w, h, xskip, yskip, lbxgfx_get_frameptr(data, frame) + 1, pitch);
     }
 
-    ++frame;
-    if (frame >= lbxgfx_get_frames(data)) {
-        frame = lbxgfx_get_frames2(data);
+    if (next) {
+        ++frame;
+        if (frame >= lbxgfx_get_frames(data)) {
+            frame = lbxgfx_get_frames2(data);
+        }
+        lbxgfx_set_frame(data, frame);
     }
-    lbxgfx_set_frame(data, frame);
+}
+
+void lbxgfx_draw_frame_offs(int x, int y, uint8_t *data, uint16_t pitch)
+{
+    lbxgfx_draw_frame_offs_delay(x, y, 1, data, pitch);
 }
 
 void lbxgfx_set_new_frame(uint8_t *data, uint16_t newframe)
