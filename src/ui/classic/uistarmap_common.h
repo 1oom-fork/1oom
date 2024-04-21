8cfdba1b376cd41065778bc8a0e67baf7f13ac8c
MyName <fork1oom@yandex.com>
2024-04-21 13:29:41 +0300

    Restore global scope for stars_xoffs (part3: cleanup, fixes)

--- src/ui/classic/uistarmap_common.h
+++ src/ui/classic/uistarmap_common.h
@@ -80,7 +80,6 @@ struct starmap_data_s {
             bool in_frange;
             uint8_t from;
             can_move_t can_move;
-            struct draw_stars_s ds;
             int frame_ship;
             int frame_scanner;
             int scanner_delay;
@@ -104,7 +103,6 @@ struct starmap_data_s {
             struct shipnon0_s sn0;
             uint8_t from;
             can_move_t can_move;
-            struct draw_stars_s ds;
             int frame_ship;
             int frame_scanner;
             int scanner_delay;
