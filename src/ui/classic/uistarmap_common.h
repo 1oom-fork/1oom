92a0db857ddfa0984ffc835bbf00d991723fa839
MyName <fork1oom@yandex.com>
2024-04-21 12:45:31 +0300

    Restore global scope for frame_ship

--- src/ui/classic/uistarmap_common.h
+++ src/ui/classic/uistarmap_common.h
@@ -80,7 +80,6 @@ struct starmap_data_s {
             bool in_frange;
             uint8_t from;
             can_move_t can_move;
-            int frame_ship;
         } ts;   /* transport */
         struct {
             shipcount_t ships[NUM_SHIPDESIGNS];
@@ -101,7 +100,6 @@ struct starmap_data_s {
             bool in_frange;
             uint8_t from;
             can_move_t can_move;
-            int frame_ship;
         } en;   /* enroute */
     };
 };
