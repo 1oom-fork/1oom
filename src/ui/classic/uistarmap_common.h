ee1f8689d937be768d79b45f2f351bcec1bbec00
MyName <fork1oom@yandex.com>
2024-04-21 12:17:42 +0300

    uistarmap: Restore global scope for frame_scanner/scanner_delay

--- src/ui/classic/uistarmap_common.h
+++ src/ui/classic/uistarmap_common.h
@@ -96,8 +96,6 @@ struct starmap_data_s {
             uint8_t from;
             struct draw_stars_s ds;
             int frame_ship;
-            int frame_scanner;
-            int scanner_delay;
         } ts;   /* transport */
         struct {
             shipcount_t ships[NUM_SHIPDESIGNS];
@@ -110,8 +108,6 @@ struct starmap_data_s {
             struct shipnon0_s sn0;
             uint8_t from;
             player_id_t player;
-            int frame_scanner;
-            int scanner_delay;
             int yoff;
         } oe;   /* orbit_en */
         struct {
@@ -120,8 +116,6 @@ struct starmap_data_s {
             uint8_t pon;
             struct draw_stars_s ds;
             int frame_ship;
-            int frame_scanner;
-            int scanner_delay;
         } en;   /* enroute */
     };
 };
