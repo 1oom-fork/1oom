015e9efb0611cd0488cd25266d3e9964a6686c45
MyName <fork1oom@yandex.com>
2024-06-07 12:44:49 +0300

    Main menu: Add rules

--- src/game/game_nump.h
+++ src/game/game_nump.h
@@ -1,6 +1,15 @@
 #ifndef INC_1OOM_GAME_NUMP_H
 #define INC_1OOM_GAME_NUMP_H
 
+#include "types.h"
+
+extern bool game_opt_fix_bugs;
+extern bool game_opt_fix_guardian_repair;
+extern bool game_opt_fix_starting_ships;
+
+extern void game_num_fix_bugs(void);
+extern void game_num_fix_guardian_repair(void);
+extern void game_num_fix_starting_ships(void);
 extern void game_num_dump(void);
 
 #endif
