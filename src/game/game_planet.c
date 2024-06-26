9d4107f97aef6c1a6b70b04fa00f8c6054a770f1
MyName <fork1oom@yandex.com>
2024-06-26 11:56:08 +0300

    New inline: game_planet_get_fact_adj_cost

--- src/game/game_planet.c
+++ src/game/game_planet.c
@@ -320,7 +320,7 @@ int game_planet_get_slider_text(const struct game_s *g, uint8_t planet_i, planet
             {
                 const char *str = NULL;
                 int vthis, cost;
-                cost = e->factory_adj_cost;
+                cost = game_planet_get_fact_adj_cost(g, p);
                 vthis = game_adjust_prod_by_special((p->prod_after_maint * p->slider[PLANET_SLIDER_IND]) / 100, p->special);
                 if (vthis != 0) {
                     int v20;
