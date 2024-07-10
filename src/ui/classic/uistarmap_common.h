ed176ac1b8cd54bcd9a7da89b1cf80bd1c0d8f8b
MyName <fork1oom@yandex.com>
2024-07-10 22:01:14 +0300

    Restore original scope (starmap_data_s::from)

--- src/ui/classic/uistarmap_common.h
+++ src/ui/classic/uistarmap_common.h
@@ -57,6 +57,7 @@ struct starmap_data_s {
     int16_t oi_tbl_enroute[FLEET_ENROUTE_MAX];
     int16_t oi_tbl_transport[TRANSPORT_MAX];
     int16_t oi_tbl_pl_stars[PLAYER_NUM][PLANETS_MAX];
+    uint8_t from;
     union {
         struct {
             int16_t oi_ship;
@@ -68,17 +69,12 @@ struct starmap_data_s {
             int16_t oi_tbl_slider_plus[PLANET_SLIDER_NUM];
         } sm;   /* starmap_do */
         struct {
-            uint8_t from;
-        } rl;   /* reloc */
-        struct {
-            uint8_t from;
             int16_t num;
             bool other;
             bool blink;
         } tr;   /* trans */
         struct {
             bool in_frange;
-            uint8_t from;
             can_move_t can_move;
             int frame_ship;
             int frame_scanner;
@@ -88,12 +84,10 @@ struct starmap_data_s {
             shipcount_t ships[NUM_SHIPDESIGNS];
             uint8_t shiptypenon0numsel; /* number of ship types selected with nonzero amount */
             struct shipnon0_s sn0;
-            uint8_t from;
         } oo;   /* orbit_own */
         struct {
             shipcount_t ships[NUM_SHIPDESIGNS];
             struct shipnon0_s sn0;
-            uint8_t from;
             player_id_t player;
             int frame_scanner;
             int scanner_delay;
@@ -101,7 +95,6 @@ struct starmap_data_s {
         } oe;   /* orbit_en */
         struct {
             struct shipnon0_s sn0;
-            uint8_t from;
             can_move_t can_move;
             int frame_ship;
             int frame_scanner;
