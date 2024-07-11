3bbff6fbd2ae5c268f88ff397e61f14b1c0a0f78
MyName <fork1oom@yandex.com>
2024-07-11 19:28:49 +0300

    Ruleset: update_on_load

--- src/game/game_num.h
+++ src/game/game_num.h
@@ -39,6 +39,7 @@ extern bool game_num_news_orion;
 extern bool game_num_aud_ask_break_nap;
 extern bool game_num_aud_bounty_give;
 extern bool game_num_aud_update_tech;
+extern bool game_num_update_on_load;
 extern bool game_num_monster_rest_att;
 extern bool game_num_combat_trans_fix;
 extern bool game_num_stargate_redir_fix;
