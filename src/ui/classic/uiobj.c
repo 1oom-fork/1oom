fcbb7955755316a202d9cb32bf3e797486ae5903
MyName <fork1oom@yandex.com>
2024-07-26 05:28:22 +0300

    uiobj: Restore uiobj_limits global scope

--- src/ui/classic/uiobj.c
+++ src/ui/classic/uiobj.c
@@ -143,13 +143,6 @@ static uiobj_t uiobj_tbl[UIOBJ_MAX];
 
 /* -------------------------------------------------------------------------- */
 
-int uiobj_minx = 0;
-int uiobj_miny = 0;
-int uiobj_maxx = UI_SCREEN_W - 1;
-int uiobj_maxy = UI_SCREEN_H - 1;
-
-/* -------------------------------------------------------------------------- */
-
 #ifdef FEATURE_MODEBUG
 #define DEBUGLEVEL_UIOBJ    1
 static void dump_uiobj_p(const uiobj_t *p)
@@ -1686,28 +1679,6 @@ void uiobj_set_xyoff(int xoff, int yoff)
 {
     uiobj_xoff = xoff;
     uiobj_yoff = yoff;
-}
-
-void uiobj_set_limits(int minx, int miny, int maxx, int maxy)
-{
-    SETMAX(minx, 0);
-    SETMAX(miny, 0);
-    SETMIN(maxx, UI_SCREEN_W - 1);
-    SETMIN(maxy, UI_SCREEN_H - 1);
-    if (minx > maxx) { int t = minx; minx = maxx; maxx = t; }
-    if (miny > maxy) { int t = miny; miny = maxy; maxy = t; }
-    uiobj_minx = minx;
-    uiobj_miny = miny;
-    uiobj_maxx = maxx;
-    uiobj_maxy = maxy;
-}
-
-void uiobj_set_limits_all(void)
-{
-    uiobj_minx = 0;
-    uiobj_miny = 0;
-    uiobj_maxx = UI_SCREEN_W - 1;
-    uiobj_maxy = UI_SCREEN_H - 1;
 }
 
 void uiobj_set_help_id(int16_t v)
