bb20ff69962e507461e8b5c3741647f916d5a190
MyName <fork1oom@yandex.com>
2024-08-09 06:14:00 +0300

    uistarmap: Add a slider lock interface for the entire empire

--- src/game/game_str.c
+++ src/game/game_str.c
@@ -490,6 +490,7 @@ const char *game_str_adj_max = " ratios of all of your colonies to maximum?";
 const char *game_str_adj_slider[PLANET_SLIDER_NUM] = {
     "ship", "defense", "industry", "ecology", "technology"
 };
+const char *game_str_adj_lock = " slider lock for all of your colonies?";
 
 const char *game_str_ra_nocont = "No Contact";
 const char *game_str_ra_notpres = "Not Present";
