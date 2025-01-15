183e4c6ac2729c5f3378c574998f654f6fa0ea94
MyName <fork1oom@yandex.com>
2025-01-15 14:37:19 +0300

    Cleanup (IS_PLAYER)

--- src/ui/classic/uigame.c
+++ src/ui/classic/uigame.c
@@ -180,7 +180,7 @@ ui_turn_action_t ui_game_turn(struct game_s *g, int *load_game_i_ptr, int pi)
                 ui_data.ui_main_loop_action = UI_MAIN_LOOP_RACES;
                 break;
             case UI_MAIN_LOOP_EMPIREREPORT:
-                if ((opponi > 0) && (opponi < g->players)) {
+                if (IS_PLAYER(g, opponi)) {
                     ui_empirereport(g, pi, opponi);
                 } else {
                     LOG_DEBUG((3, "%s: invalid opponi %i on EMPIREREPORT\n", __func__, opponi));
@@ -188,7 +188,7 @@ ui_turn_action_t ui_game_turn(struct game_s *g, int *load_game_i_ptr, int pi)
                 ui_data.ui_main_loop_action = UI_MAIN_LOOP_RACES;
                 break;
             case UI_MAIN_LOOP_AUDIENCE:
-                if ((opponi > 0) && (opponi < g->players)) {
+                if (IS_PLAYER(g, opponi)) {
                     game_audience(g, pi, opponi);
                 } else {
                     LOG_DEBUG((3, "%s: invalid opponi %i on AUDIENCE\n", __func__, opponi));
