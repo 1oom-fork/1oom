50ddfad9c87721d738c1b19af195b1ee4f567ab2
MyName <fork1oom@yandex.com>
2025-01-22 17:37:44 +0300

    Static implementation of vgabuf in one piece of memory

---
+++ src/vgabuf.h
@@ -0,0 +1,25 @@
+#ifndef INC_1OOM_VGABUF_H
+#define INC_1OOM_VGABUF_H
+
+#include "types.h"
+
+/* double buffering + 2 aux buffers */
+#define NUM_VIDEOBUF    4
+
+#define VGABUF_W    320
+#define VGABUF_H    200
+#define VGABUF_SIZE     (VGABUF_W * VGABUF_H)
+
+/* FIXME */
+#define VGABUF_EXTRA_H    200
+
+#define VGABUF_SIZE_EXT     (VGABUF_W * (VGABUF_H + VGABUF_EXTRA_H))
+
+extern uint8_t vgabuf[NUM_VIDEOBUF * VGABUF_SIZE_EXT];
+
+static inline uint8_t *vgabuf_get(int i)
+{
+    return &vgabuf[i * VGABUF_SIZE_EXT];
+}
+
+#endif
