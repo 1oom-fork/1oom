006fdf900ee136c42c955c09d2e625065e1433b6
MyName <fork1oom@yandex.com>
2025-02-04 06:24:15 +0300

    Cleanup (video.render)

--- src/hw/sdl/hwsdl_video.h
+++ src/hw/sdl/hwsdl_video.h
@@ -3,7 +3,7 @@
 
 extern void hw_video_shutdown(void);
 extern void hw_video_update(void);
-extern void hw_video_refresh(int front);
+extern void hw_video_refresh(void);
 extern int hw_video_resize(int w, int h);
 extern void hw_video_input_grab(bool grab);
 
