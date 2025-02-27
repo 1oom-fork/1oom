31de0f6878484c00e00d406905d839c6dcd6b4f6
MyName <fork1oom@yandex.com>
2025-02-27 19:42:30 +0300

    hwsdl1::hw_opt_use_gl -> hwsdl::hw_opt_force_sw

--- src/hw/sdl/hwsdl_opt.h
+++ src/hw/sdl/hwsdl_opt.h
@@ -8,9 +8,9 @@ extern int hw_opt_screen_fsw;
 extern int hw_opt_screen_fsh;
 extern const char *hw_opt_sdlmixer_sf;
 extern int hw_opt_aspect;
+extern int hw_opt_force_sw;
 
 /* for SDL1 */
-extern int hw_opt_use_gl;
 extern int hw_opt_gl_filter;
 
 #endif
