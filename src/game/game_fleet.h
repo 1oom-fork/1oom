6cb6c06a3f0652c8caccf9a3f035eaeec5bfaadf
MyName <fork1oom@yandex.com>
2025-03-28 18:49:09 +0300

    Move fleet data type definitions: game.h -> game_fleet.h

--- src/game/game_fleet.h
+++ src/game/game_fleet.h
@@ -1,11 +1,42 @@
 #ifndef INC_1OOM_GAME_FLEET_H
 #define INC_1OOM_GAME_FLEET_H
 
+#include "boolvec.h"
 #include "game_types.h"
 #include "types.h"
 
 struct game_s;
 struct planet_s;
+
+typedef struct fleet_enroute_s {
+    player_id_t owner;
+    uint16_t x;
+    uint16_t y;
+    uint8_t dest;   /* planet index */
+    uint8_t speed;
+    BOOLVEC_DECLARE(visible, PLAYER_NUM);
+    shipcount_t ships[NUM_SHIPDESIGNS];
+} fleet_enroute_t;
+
+#define FLEET_ENROUTE_AI_MAX    208
+#define FLEET_ENROUTE_MAX   260
+
+typedef struct transport_s {
+    player_id_t owner;
+    uint16_t x;
+    uint16_t y;
+    uint8_t dest;   /* planet index */
+    uint8_t speed;
+    BOOLVEC_DECLARE(visible, PLAYER_NUM);
+    uint16_t pop;
+} transport_t;
+
+#define TRANSPORT_MAX   100
+
+typedef struct fleet_orbit_s {
+    BOOLVEC_DECLARE(visible, PLAYER_NUM);
+    shipcount_t ships[NUM_SHIPDESIGNS];
+} fleet_orbit_t;
 
 #define FLEET_SPEED_STARGATE    35
 
