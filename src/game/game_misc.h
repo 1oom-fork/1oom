a323129ebf863b627951376444cf454fad527c97
MyName <fork1oom@yandex.com>
2025-04-10 11:23:07 +0300

    Move game_get_tech_prod: game_misc -> game_planet

--- src/game/game_misc.h
+++ src/game/game_misc.h
@@ -21,7 +21,6 @@ extern void game_adjust_slider_group(int16_t *slidertbl, int slideri, int16_t va
 extern int game_get_min_dist(const struct game_s *g, int player_i, int planet_i);
 extern int game_get_pop_growth_max(const struct game_s *g, int planet_i, int max_pop3);
 extern int game_get_pop_growth_for_eco(const struct game_s *g, int planet_i, int eco);
-extern int game_get_tech_prod(int prod, int slider, race_t race, planet_special_t special);
 extern void game_print_prod_of_total(const struct game_s *g, player_id_t pi, int prod, char *buf);
 extern bool game_xy_is_in_nebula(const struct game_s *g, int x, int y);
 extern int game_calc_eta(const struct game_s *g, int speed, int x0, int y0, int x1, int y1);
