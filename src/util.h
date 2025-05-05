23f3dbbe79824910be4db37328ae2a194d0d8684
MyName <fork1oom@yandex.com>
2025-05-05 04:27:30 +0300

    Move util_table_remove_item: util -> game_util

--- src/util.h
+++ src/util.h
@@ -17,9 +17,6 @@ extern void util_trim_whitespace(char *str);
 extern void util_str_tolower(char *str);
 extern bool util_parse_number(const char *str, uint32_t *val_ptr);
 extern int32_t *util_parse_numbers(const char *str, char sep, int *numptr);
-extern void util_table_remove_item_keep_order(int itemi, void *tbl, int itemsz, int itemnum);
-extern void util_table_remove_item_keep_order_zero(int itemi, void *tbl, int itemsz, int itemnum);
-extern void util_table_remove_item_any_order(int itemi, void *tbl, int itemsz, int itemnum);
 
 #if !defined HAVE_STRCASECMP
 extern int strcasecmp(const char *s1, const char *s2);
