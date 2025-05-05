0c31dc928617582ca9a029ae30515ff2c04294fb
MyName <fork1oom@yandex.com>
2025-05-05 04:27:30 +0300

    Move util_table_remove_item: util -> game_util

--- src/util.c
+++ src/util.c
@@ -259,33 +259,6 @@ int32_t *util_parse_numbers(const char *str, char sep, int *numptr)
     return nums;
 }
 
-void util_table_remove_item_keep_order(int itemi, void *tbl, int itemsz, int itemnum)
-{
-    if ((itemi < 0) || (itemi >= (itemnum - 1))) {
-        return;
-    }
-    memmove(((uint8_t *)tbl) + itemi * itemsz, ((uint8_t *)tbl) + (itemi + 1) * itemsz, (itemnum - 1 - itemi) * itemsz);
-}
-
-void util_table_remove_item_keep_order_zero(int itemi, void *tbl, int itemsz, int itemnum)
-{
-    if ((itemi < 0) || (itemi >= itemnum)) {
-        return;
-    }
-    if (itemi < (itemnum - 1)) {
-        memmove(((uint8_t *)tbl) + itemi * itemsz, ((uint8_t *)tbl) + (itemi + 1) * itemsz, (itemnum - 1 - itemi) * itemsz);
-    }
-    memset(((uint8_t *)tbl) + (itemnum - 1) * itemsz, 0, itemsz);
-}
-
-void util_table_remove_item_any_order(int itemi, void *tbl, int itemsz, int itemnum)
-{
-    if ((itemi < 0) || (itemi >= (itemnum - 1))) {
-        return;
-    }
-    memcpy(((uint8_t *)tbl) + itemi * itemsz, ((uint8_t *)tbl) + (itemnum - 1) * itemsz, itemsz);
-}
-
 /* -------------------------------------------------------------------------- */
 
 /* The following `strcasecmp()' implementation is taken from:
