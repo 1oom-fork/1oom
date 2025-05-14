ba908eab87f9f0737d306a57495d2c4c3fcfce22
MyName <fork1oom@yandex.com>
2025-05-14 11:57:42 +0300

    Fix typo

--- src/game/game_audience.c
+++ src/game/game_audience.c
@@ -566,7 +566,7 @@ static void audience_menu_treaty(struct audience_s *au)
     }
     all_num = 0;
     for (player_id_t i = PLAYER_0; i < g->players; ++i) {
-        if ((i != ph) && (i != pa) && ((ea->treaty[i] == TREATY_ALLIANCE) || (game_num_aud_ask_break_nap && (ea->treaty[i] == TREATY_NONAGGRESSION)))) {
+        if ((i != ph) && (i != pa) && BOOLVEC_IS1(ea->contact, i) && ((ea->treaty[i] == TREATY_ALLIANCE) || (game_num_aud_ask_break_nap && (ea->treaty[i] == TREATY_NONAGGRESSION)))) {
             all_tbl[all_num++] = i;
         }
     }
