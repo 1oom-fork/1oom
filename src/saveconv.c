5c8246fe9dcee4ad2d30b9bee0af187043fdcc1f
MyName <fork1oom@yandex.com>
2025-05-15 14:17:06 +0300

    Move saveconv::main_1oom to saveconv_main.c

--- src/saveconv.c
+++ src/saveconv.c
@@ -7,7 +7,6 @@
 #include "main.h"
 #include "bits.h"
 #include "comp.h"
-#include "hw.h"
 #include "game/game.h"
 #include "game/game_aux.h"
 #include "game/game_save.h"
@@ -16,15 +15,7 @@
 #include "log.h"
 #include "os.h"
 #include "saveconv.h"
-#include "ui.h"
 #include "util.h"
-
-/* -------------------------------------------------------------------------- */
-
-const char *idstr_main = "saveconv";
-
-bool main_use_lbx = false;
-bool ui_use_audio = false;
 
 /* -------------------------------------------------------------------------- */
 
@@ -2447,7 +2438,7 @@ const struct cmdline_options_s main_cmdline_options[] = {
 
 /* -------------------------------------------------------------------------- */
 
-static int main_early_init(void)
+int saveconv_main_init(void)
 {
     struct game_s *g;
     struct game_aux_s *gaux;
@@ -2464,15 +2455,7 @@ static int main_early_init(void)
     return 0;
 }
 
-static int main_init(void)
-{
-    if (os_init()) {
-        return 1;
-    }
-    return 0;
-}
-
-static void main_shutdown(void)
+void saveconv_main_shutdown(void)
 {
     lib_free(gameptr->gaux->savenamebuf);
     lib_free(gameptr->gaux->savebuf);
@@ -2530,24 +2513,4 @@ int main_do(void)
         return 1;
     }
     return 0;
-}
-
-/* -------------------------------------------------------------------------- */
-
-int main_1oom(int argc, char **argv)
-{
-    if (main_early_init()) {
-        return 1;
-    }
-    if (options_parse_early(argc, argv)) {
-        return 1;
-    }
-    atexit(main_shutdown);
-    if (main_init()) {
-        return 2;
-    }
-    if (options_parse(argc, argv)) {
-        return 3;
-    }
-    return main_do();
 }
