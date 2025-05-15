5d51ab61489b4e37d0d2f7d22e211810e75c421b
MyName <fork1oom@yandex.com>
2025-05-15 20:22:08 +0300

    Add saveconv capabilities to -load feature

--- src/game/game.c
+++ src/game/game.c
@@ -17,6 +17,7 @@
 #include "log.h"
 #include "options.h"
 #include "rnd.h"
+#include "saveconv.h"
 #include "ui.h"
 #include "util.h"
 
@@ -420,9 +421,16 @@ int main_do(void)
             game_new_opts = game_opt_new;
             goto main_menu_new_game;
         } else if (game_opt_load_fname) {
-            if (game_save_do_load_fname(game_opt_load_fname, 0, &game)) {
+            saveconv_init();
+            if (saveconv_is_moo13(&game, game_opt_load_fname) && !saveconv_de_moo13(&game, game_opt_load_fname)) {
+                log_message("Loading save (MOO1.3): %s\n", game_opt_load_fname);
+            } else if (saveconv_is_text(&game, game_opt_load_fname) && !saveconv_de_text(&game, game_opt_load_fname)) {
+                log_message("Loading save (text): %s\n", game_opt_load_fname);
+            } else if (game_save_do_load_fname(game_opt_load_fname, 0, &game)) {
+                saveconv_shutdown();
                 log_fatal_and_die("Game: could not load save '%s'\n", game_opt_load_fname);
             }
+            saveconv_shutdown();
             game_opt_load_fname = 0;
             goto main_menu_start_game;
         } else if (game_opt_load_game) {
