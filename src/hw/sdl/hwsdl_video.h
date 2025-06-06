2d80ed02426b5fb5d14cf27746eee020715f4639
MyName <fork1oom@yandex.com>
2025-06-06 21:19:16 +0300

    src/hw/hw_video.c(h)

--- src/hw/sdl/hwsdl_video.h
+++ src/hw/sdl/hwsdl_video.h
@@ -1,9 +1,6 @@
 #ifndef INC_1OOM_HWSDL_VIDEO_H
 #define INC_1OOM_HWSDL_VIDEO_H
 
-extern void hw_video_shutdown(void);
-extern void hw_video_update(void);
-extern void hw_video_refresh(void);
 extern int hw_video_resize(int w, int h);
 extern int hw_video_toggle_fullscreen(void);
 extern void hw_video_input_grab(bool grab);
