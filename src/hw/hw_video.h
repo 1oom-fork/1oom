63ea35c80870be7455b157e4a315fd8573d37052
MyName <fork1oom@yandex.com>
2025-06-22 14:39:32 +0300

    Move video.render/update/setpal to hw_video::i_video

--- src/hw/hw_video.h
+++ src/hw/hw_video.h
@@ -1,6 +1,14 @@
 #ifndef INC_1OOM_HW_VIDEO_H
 #define INC_1OOM_HW_VIDEO_H
 
+#include "types.h"
+
+extern struct i_hw_video_s {
+    void (*render)(const uint8_t *buf);
+    void (*update)(void);
+    void (*setpal)(const uint8_t *pal, int first, int num);
+} i_hw_video;
+
 extern void hw_video_shutdown(void);
 extern void hw_video_refresh(void);
 extern void hw_video_update(void);
