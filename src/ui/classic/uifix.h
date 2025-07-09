f3009bf8bb632e7103a25b7e08e32997d3b26b91
MyName <fork1oom@yandex.com>
2025-07-09 19:18:07 +0300

    Add UI fixes

--- src/ui/classic/uifix.h
+++ src/ui/classic/uifix.h
@@ -3,10 +3,16 @@
 
 #include "types.h"
 
+/* Fix cursor offset when navigating with cursor keys. */
+extern bool ui_fix_cursor_nav_offset;
+
 /* Fix graphics bug with brighter stars drawn further away than dimmer ones. */
 extern bool ui_fix_starmap_background;
 
 /* Allow navigation through objects with assigned hotkeys using the cursor keys. */
 extern bool ui_qol_cursor_nav_all_obj;
+
+/* Move messages so that the selected star is always visible. */
+extern bool ui_qol_starmap_msg_pos;
 
 #endif
