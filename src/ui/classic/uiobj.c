eb7f5db9bdf6dd574d22c7178c68235e402f99ef
MyName <fork1oom@yandex.com>
2025-07-11 17:41:32 +0300

    Fix some inaccuracies in src/ui/classic/uiobj.c

--- src/ui/classic/uiobj.c
+++ src/ui/classic/uiobj.c
@@ -1180,19 +1180,18 @@ static void uiobj_click_obj(int16_t oi, int mx, int my)
     }
 }
 
-static void uiobj_finish_callback_delay_p(int delay)
+static inline void uiobj_finish_callback_delay_p(int delay)
 {
     if (uiobj_flag_have_cb) {
         ui_delay_prepare();
+        /* vgabuf_select_back() */
         uiobj_do_callback();
         ui_palette_set_n();
         uiobj_finish_frame();
         ui_delay_ticks_or_click(delay);
     } else {
-        ui_delay_prepare();
         ui_palette_set_n();
         uiobj_finish_frame();
-        ui_delay_ticks_or_click(delay); /* MOO1 does not do this, but we need it to update mouse_* etc */
     }
 }
 
@@ -1579,6 +1578,7 @@ int16_t uiobj_handle_input_cond(void)
 void uiobj_finish_frame(void)
 {
     int mx, my;
+    hw_event_handle();
     mx = mouse_x;
     my = mouse_y;
     uiobj_handle_objects();
