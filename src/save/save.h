1aa2a0d37fbce41fd355e85e0b2500966aac8739
MyName <fork1oom@yandex.com>
2025-08-10 07:28:21 +0300

    saveconv: Move moo13 format source code to libsave

--- src/save/save.h
+++ src/save/save.h
@@ -10,10 +10,17 @@
 #define GAME_SAVE_I_CONTINUE    (7 - 1)
 #define GAME_SAVE_I_UNDO        (8 - 1)
 
+#define SAVE_MOO13_LEN  59036
+#define SAVE_CMOO_LEN   154
+
 extern bool game_save_tbl_have_save[NUM_ALL_SAVES];
 extern char game_save_tbl_name[NUM_ALL_SAVES][SAVE_NAME_LEN];
 
 struct game_s;
+extern bool savetype_is_moo13(const char *fname);
+extern int savetype_moo13_save_do(const char *filename, const struct game_s *g);
+extern int savetype_moo13_load_do(const char *filename, struct game_s *g);
+
 extern void *libsave_1oom_open_check_header(const char *filename, int i, bool update_table, char *savename);
 extern int libsave_1oom_save_do(const char *filename, const char *savename, const struct game_s *g, int savei);
 extern int libsave_1oom_load_do(const char *filename, struct game_s *g, int savei, char *savename);
