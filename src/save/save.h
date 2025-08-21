8b839bb743a88c6d545f668b99dd1b8975cc26b9
MyName <fork1oom@yandex.com>
2025-08-21 21:34:49 +0300

    libsave_1oom: Naming

--- src/save/save.h
+++ src/save/save.h
@@ -4,9 +4,9 @@
 #include "types.h"
 
 struct game_s;
-extern void *game_save_open_check_header(const char *filename, char *savename);
-extern bool game_save_is_1oom(const char *filename);
-extern int game_save_do_save_do(const char *filename, const char *savename, const struct game_s *g);
-extern int game_save_do_load_do(const char *filename, struct game_s *g);
+extern void *libsave_1oom_open_check_header(const char *filename, char *savename);
+extern bool libsave_is_1oom(const char *filename);
+extern int libsave_1oom_save_do(const char *filename, const char *savename, const struct game_s *g);
+extern int libsave_1oom_load_do(const char *filename, struct game_s *g);
 
 #endif
