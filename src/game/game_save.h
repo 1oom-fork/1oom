602e290ada64df7b153833f4c47bfaa08fdbf418
MyName <fork1oom@yandex.com>
2025-08-21 21:21:55 +0300

    game_save: Separate format specific source code

--- src/game/game_save.h
+++ src/game/game_save.h
@@ -14,8 +14,6 @@
 extern bool game_save_tbl_have_save[NUM_ALL_SAVES];
 extern char game_save_tbl_name[NUM_ALL_SAVES][SAVE_NAME_LEN];
 
-extern void *game_save_open_check_header(const char *filename, char *savename);
-extern bool game_save_is_1oom(const char *filename);
 extern int game_save_check_saves(void);
 
 struct game_s;
