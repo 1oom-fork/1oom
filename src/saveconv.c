4804cad7e67d2bddcfd6dcf3cf20c6992a2198b5
MyName <fork1oom@yandex.com>
2025-08-21 23:50:04 +0300

    game_save: Refactor game_save_open_check_header

--- src/saveconv.c
+++ src/saveconv.c
@@ -167,9 +167,7 @@ static int savetype_de_smart(struct game_s *g, const char *fname)
     }
     fclose(fd);
     fd = NULL;
-    if ((fd = game_save_open_check_header(fname, -1, false, 0)) != 0) {
-        fclose(fd);
-        fd = NULL;
+    if (game_save_is_1oom(fname)) {
         savetypei = SAVETYPE_NATIVE;
         res = savetype[SAVETYPE_NATIVE].decode(g, fname);
     } else if (savetype_is_moo13(g, fname)) {
