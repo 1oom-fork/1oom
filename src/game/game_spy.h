7f8ee40399e5558f20c65e170e4e35ff470ef2bf
MyName <fork1oom@yandex.com>
2026-02-21 11:09:24 +0300

    spy_esp_s: Make temporary local variables explicit

--- src/game/game_spy.h
+++ src/game/game_spy.h
@@ -9,8 +9,6 @@ struct spy_turn_s {
 
 struct spy_esp_s {
     int tnum;
-    int tbl_num[TECH_FIELD_NUM];
-    uint8_t tbl_techi[TECH_FIELD_NUM][50];
     tech_field_t tbl_field[TECH_SPY_MAX];
     uint8_t tbl_tech2[TECH_SPY_MAX];
     int tbl_value[TECH_SPY_MAX];
@@ -19,7 +17,7 @@ struct spy_esp_s {
 struct game_s;
 
 extern int game_spy_select_useful_techs(struct game_s *g, struct spy_esp_s *s, player_id_t target, player_id_t spy, int minval, int a6);
-extern int game_spy_sift_useful_techs(struct game_s *g, struct spy_esp_s *s, player_id_t target, player_id_t spy, int a4);
+extern int game_spy_sift_useful_techs(struct game_s *g, player_id_t target, player_id_t spy, int a4);
 extern void game_spy_build(struct game_s *g);
 extern void game_spy_report(struct game_s *g);
 extern void game_spy_turn(struct game_s *g, struct spy_turn_s *st);
