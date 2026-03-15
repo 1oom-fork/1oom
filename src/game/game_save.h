ca88f92192f03ea413bc2869b6ce21796386736c
MyName <fork1oom@yandex.com>
2026-03-15 13:40:22 +0300

    libsave_1oom_open_check_header: Report version

--- src/game/game_save.h
+++ src/game/game_save.h
@@ -14,7 +14,7 @@
 extern bool game_save_tbl_have_save[NUM_ALL_SAVES];
 extern char game_save_tbl_name[NUM_ALL_SAVES][SAVE_NAME_LEN];
 
-extern void *libsave_1oom_open_check_header(const char *filename, char *savename);
+extern void *libsave_1oom_open_check_header(const char *filename, char *savename, uint32_t *version);
 extern void game_save_tbl_slot_init(int i);
 extern void game_save_tbl_slot_set(int i, const char *savename);
 extern int game_save_check_saves(void);
