fb2eadcbb29029eb59c08150ba39cff272717a63
MyName <fork1oom@yandex.com>
2026-03-18 13:50:03 +0300

    libsave: Fix tbl_ship_names

--- src/save/save_moo13_encode.c
+++ src/save/save_moo13_encode.c
@@ -15,6 +15,21 @@
 
 /* -------------------------------------------------------------------------- */
 
+static const char *game_str_tbl_ship_names_1oom0[RACE_NUM * SHIP_NAME_NUM] = {
+    "FIGHTER", "GUNBOAT", "COURIER", "FRIGATE", "DESTROYER", "CORVETTE", "CRUISER", "ESCORT", "WARSHIP", "DREADNOUGHT", "BATTLESHIP", "DREADSTAR",
+    "WOLVERINE", "GRIFFIN", "FERRET", "LYNX", "CHEETAH", "BOBCAT", "PANTHER", "LEOPARD", "WARCAT", "SABRETOOTH", "PUMA", "TIGER",
+    "PIRANHA", "STINGRAY", "MINNOW", "MANTA", "BARRACUDA", "MOREY", "SHARK", "MAKO", "WHALE", "KRAKEN", "MONITOR", "POLARIS",
+    "GOBLIN", "CYCLOPS", "DAEMON", "BANSHEE", "SPECTRE", "SPIRIT", "JUGGERNAUT", "VALKYRIE", "HYDRA", "TITAN", "COLOSSUS", "DRAGON",
+    "STARFIGHTER", "COMET", "SEEKER", "STAR WING", "STAR BLAZER", "STAR STREAK", "STAR BLADE", "DARK STAR", "CORAONA", "SUN FIRE", "STAR FURY", "NOVA",
+    "FOXBAT", "SPARROWHAWK", "PELICAN", "FALCON", "SKY HAWK", "SPACE GULL", "WARBIRD", "WARHAWK", "WAREAGLE", "CONDOR", "DREADWING", "SKY MASTER",
+    "SABRE", "CUTLASS", "DAGGER", "LANCER", "PEGASUS", "HORSEMAN", "RANGER", "KNIGHT", "AVENGER", "DRAGOON", "PALADIN", "EXCALIBER",
+    "PAW", "PANDA", "PATROL", "HUNTER", "GLADIATOR", "SENTINEL", "CLAW", "TOOTH", "WARBEAR", "GRIZZLY", "PUNISHER", "RHINO",
+    "MARAUDER", "HYPERION", "DRACHMA", "INTRUDER", "PENETRATOR", "TORMENTER", "AJAX", "TORNADO", "NEXUS", "NEMESIS", "DEVASTATOR", "ANNIHILATOR",
+    "SPIDER", "BUMBLEBEE", "NEEDLE", "HORNET", "SCORPION", "VENOM", "TARANTULA", "VIPER", "COBRA", "KING COBRA", "BLACK WIDOW", "DEATH WYN"
+};
+
+/* -------------------------------------------------------------------------- */
+
 bool libsave_moo13_check(const struct game_s *g)
 {
     if ((g->players < 2) || (g->players > 6)) return false;
@@ -406,7 +421,7 @@ static int libsave_moo13_encode(uint8_t *buf, const struct game_s *g)
         int srdb;
         srdb = 0xc410 + 0x468 * i;
         for (int j = 0; j < SHIP_NAME_NUM; ++j) {
-            strncpy((char *)&(buf[srdb + 0x198 + j * 12]), game_str_tbl_ship_names[e->race * SHIP_NAME_NUM + j], 11);
+            strncpy((char *)&(buf[srdb + 0x198 + j * 12]), game_str_tbl_ship_names_1oom0[e->race * SHIP_NAME_NUM + j], 11);
         }
     }
     return 0;
