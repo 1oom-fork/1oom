b4a61dbaa42044029f564278f06a5b0d6c0cbe6f
MyName <fork1oom@yandex.com>
2026-04-04 12:06:58 +0300

    hwsdl1: Fix header paths

--- src/hw/sdl/1/hwsdl1_audio.c
+++ src/hw/sdl/1/hwsdl1_audio.c
@@ -2,9 +2,9 @@
 
 #ifdef HAVE_SDLMIXER1
 #define HAVE_SDLMIXER
-#include "SDL.h"
-#include "SDL_mixer.h"
-#include "SDL_rwops.h"
+#include "SDL/SDL.h"
+#include "SDL/SDL_mixer.h"
+#include "SDL/SDL_rwops.h"
 #endif /* HAVE_SDLMIXER1 */
 
 #include "hwsdl_audio.c"
