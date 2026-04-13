304edfbe05634a0ccbab893b01606ea2f90f82a9
MyName <fork1oom@yandex.com>
2026-04-13 17:18:51 +0300

    Cleanup (bits.h: unaligned access, strict types)

--- src/bits.h
+++ src/bits.h
@@ -1,45 +1,79 @@
 #ifndef INC_1OOM_BITS_H
 #define INC_1OOM_BITS_H
 
-#include "config.h"
 #include "types.h"
 
-/* Allow unaligned access for various platforms */
-#if defined(__i386__) || defined(__x86_64__) || defined(__amd64__)
-#define ALLOW_UNALIGNED_ACCESS
-#endif
-#if defined(__m68020__) || defined(__m68030__) || defined(__m68040__) || defined(__m68060__)
-#define ALLOW_UNALIGNED_ACCESS
-#endif
-#if defined(__powerpc__) || defined(__ppc__)
-#define ALLOW_UNALIGNED_ACCESS
-#endif
+static inline void SET_LE_16(uint8_t *dst, uint16_t val)
+{
+    dst[0] = (uint8_t)(val & 0xffu);
+    dst[1] = (uint8_t)((val >> 8) & 0xffu);
+}
 
-#define BSWAP_16(_v_)   ((uint16_t)((((_v_) >> 8) & 0xff) | ((_v_) << 8)))
+static inline void SET_LE_S16(uint8_t *dst, int16_t val)
+{
+    SET_LE_16(dst, (uint16_t)val);
+}
 
-#if !defined WORDS_BIGENDIAN && defined ALLOW_UNALIGNED_ACCESS
-#define SET_LE_16(_p_,_v_) ((*((uint16_t *)(_p_))) = (_v_))
-#define GET_LE_16(_p_) (*((uint16_t const *)(_p_)))
-#define SET_LE_32(_p_,_v_) ((*((uint32_t *)(_p_))) = (_v_))
-#define GET_LE_32(_p_) (*((uint32_t const *)(_p_)))
-#else
-#define SET_LE_16(_p_,_v_) (((uint8_t *)(_p_))[0] = ((_v_) & 0xffu), ((uint8_t *)(_p_))[1] = (((_v_) >> 8) & 0xffu))
-#define GET_LE_16(_p_) (((uint16_t)(((uint8_t const *)(_p_))[0])) | (((uint16_t)(((uint8_t const *)(_p_))[1])) << 8))
-#define SET_LE_32(_p_,_v_) (((uint8_t *)(_p_))[0] = ((_v_) & 0xffu), ((uint8_t *)(_p_))[1] = (((_v_) >> 8) & 0xffu), ((uint8_t *)(_p_))[2] = (((_v_) >> 16) & 0xffu), ((uint8_t *)(_p_))[3] = (((_v_) >> 24) & 0xffu))
-#define GET_LE_32(_p_) (((uint32_t)GET_LE_16((_p_))) | (((uint32_t)GET_LE_16((_p_) + 2)) << 16))
-#endif
-#define GET_LE_24(_p_) (((uint32_t)GET_LE_16((_p_))) | (((uint32_t)*(((uint8_t const *)(_p_)) + 2)) << 16))
+static inline uint16_t GET_LE_16(const uint8_t *src)
+{
+    return ((uint16_t)(src[0])) | (((uint16_t)(src[1])) << 8);
+}
 
-#if defined WORDS_BIGENDIAN && defined ALLOW_UNALIGNED_ACCESS
-#define SET_BE_16(_p_,_v_) ((*((uint16_t *)(_p_))) = (_v_))
-#define GET_BE_16(_p_) (*((uint16_t const *)(_p_)))
-#define SET_BE_32(_p_,_v_) ((*((uint32_t *)(_p_))) = (_v_))
-#define GET_BE_32(_p_) (*((uint16_t const *)(_p_)))
-#else
-#define SET_BE_16(_p_,_v_) (((uint8_t *)(_p_))[1] = ((_v_) & 0xffu), ((uint8_t *)(_p_))[0] = (((_v_) >> 8) & 0xffu))
-#define GET_BE_16(_p_) (((uint16_t)(((uint8_t const *)(_p_))[1])) | (((uint16_t)(((uint8_t const *)(_p_))[0])) << 8))
-#define SET_BE_32(_p_,_v_) (((uint8_t *)(_p_))[3] = ((_v_) & 0xffu), ((uint8_t *)(_p_))[2] = (((_v_) >> 8) & 0xffu), ((uint8_t *)(_p_))[1] = (((_v_) >> 16) & 0xffu), ((uint8_t *)(_p_))[0] = (((_v_) >> 24) & 0xffu))
-#define GET_BE_32(_p_) (((uint32_t)GET_BE_16((_p_) + 2)) | (((uint32_t)GET_BE_16((_p_))) << 16))
-#endif
+static inline int16_t GET_LE_S16(const uint8_t *src)
+{
+    return (int16_t)GET_LE_16(src);
+}
+
+static inline void SET_LE_32(uint8_t *dst, uint32_t val)
+{
+    dst[0] = (uint8_t)(val & 0xffu);
+    dst[1] = (uint8_t)((val >> 8) & 0xffu);
+    dst[2] = (uint8_t)((val >> 16) & 0xffu);
+    dst[3] = (uint8_t)((val >> 24) & 0xffu);
+}
+
+static inline void SET_LE_S32(uint8_t *dst, int32_t val)
+{
+    SET_LE_32(dst, (uint32_t)val);
+}
+
+static inline uint32_t GET_LE_32(const uint8_t *src)
+{
+    return ((uint32_t)GET_LE_16(src)) | (((uint32_t)GET_LE_16(src + 2)) << 16);
+}
+
+static inline int32_t GET_LE_S32(const uint8_t *src)
+{
+    return (int32_t)GET_LE_32(src);
+}
+
+static inline uint32_t GET_LE_24(const uint8_t *src)
+{
+    return ((uint32_t)GET_LE_16(src)) | (((uint32_t)(src[2])) << 16);
+}
+
+static inline void SET_BE_16(uint8_t *dst, uint16_t val)
+{
+    dst[1] = (uint8_t)(val & 0xffu);
+    dst[0] = (uint8_t)((val >> 8) & 0xffu);
+}
+
+static inline uint16_t GET_BE_16(const uint8_t *src)
+{
+    return ((uint16_t)(src[1])) | (((uint16_t)(src[0])) << 8);
+}
+
+static inline void SET_BE_32(uint8_t *dst, uint32_t val)
+{
+    dst[3] = (uint8_t)(val & 0xffu);
+    dst[2] = (uint8_t)((val >> 8) & 0xffu);
+    dst[1] = (uint8_t)((val >> 16) & 0xffu);
+    dst[0] = (uint8_t)((val >> 24) & 0xffu);
+}
+
+static inline uint32_t GET_BE_32(const uint8_t *src)
+{
+    return ((uint32_t)GET_BE_16(src + 2)) | (((uint32_t)GET_BE_16(src)) << 16);
+}
 
 #endif
