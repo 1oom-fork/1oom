c3aa7398cdd24230dc26d2c36ba89281caf87f12
MyName <fork1oom@yandex.com>
2026-04-18 05:55:25 +0300

    hwsdl1: Cleanup, fix (#include ".c")
    Some fixes are based on 1oom 1.0

--- src/hw/sdl/hwsdl_video.h
+++ src/hw/sdl/hwsdl_video.h
@@ -1,6 +1,11 @@
 #ifndef INC_1OOM_HWSDL_VIDEO_H
 #define INC_1OOM_HWSDL_VIDEO_H
 
+extern bool hw_mouse_enabled;
+
 extern int hw_video_resize(int w, int h);
+
+extern void hw_mouse_grab(void);
+extern void hw_mouse_ungrab(void);
 
 #endif
