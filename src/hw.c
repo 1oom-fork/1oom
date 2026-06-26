97be2c4645829adc50254d9850311f99acb011b1
MyName <fork1oom@yandex.com>
2026-06-26 06:41:45 +0300

    hw: Move some declarations to hw_internal.h

---
+++ src/hw.c
@@ -0,0 +1,10 @@
+#include "config.h"
+
+#include "hw.h"
+#include "hw/hw_internal.h"
+
+#include "types.h"
+
+/* -------------------------------------------------------------------------- */
+
+struct i_hw_video_s i_hw_video = { 0 };
