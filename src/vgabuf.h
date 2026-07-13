2d5cb60c90e6000996143d8ad0b22ba86d3609c8
MyName <fork1oom@yandex.com>
2026-07-13 04:56:56 +0300

    ui: Move code related to drawing limits to vgabuf.c/h

--- src/vgabuf.h
+++ src/vgabuf.h
@@ -13,9 +13,6 @@
 
 #define vgabuf_get vgabuf_get_back
 
-#define vgabuf_draw_line_limit ui_draw_line_limit
-#define vgabuf_draw_line_limit_ctbl ui_draw_line_limit_ctbl
-
 extern void vgabuf_flip(void);
 
 /* Return back buffer. */
@@ -34,6 +31,8 @@ extern void vgabuf_fill_rect(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uin
 extern void vgabuf_put_pixel(int16_t x, int16_t y, uint8_t color);
 extern void vgabuf_draw_line(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint8_t color);
 extern void vgabuf_draw_line_ctbl(int16_t x0, int16_t y0, int16_t x1, int16_t y1, const uint8_t *colortbl, int colornum, int pos);
+extern void vgabuf_draw_line_limit(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint8_t color);
+extern void vgabuf_draw_line_limit_ctbl(int16_t x0, int16_t y0, int16_t x1, int16_t y1, const uint8_t *colortbl, int colornum, int pos);
 extern void vgabuf_draw_copy_line(int16_t x0, int16_t y0, int16_t x1, int16_t y1, bool flag_hmm);
 extern void vgabuf_draw_line_3h(int16_t x0, int16_t y0, int16_t x1, uint8_t color);
 extern void vgabuf_draw_box1(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint8_t color1, uint8_t color2);
@@ -49,7 +48,9 @@ static inline void vgabuf_fill(uint8_t color)
     vgabuf_fill_rect(VGABUF_RECT, color);
 }
 
-#define vgabuf_limits_set uiobj_set_limits
-#define vgabuf_limits_set_all uiobj_set_limits_all
+extern bool vgabuf_limits_outside(int16_t x0, int16_t y0, int16_t x1, int16_t y1);
+extern void vgabuf_limits_clamp_rect(int16_t *x0, int16_t *y0, int16_t *x1, int16_t *y1);
+extern void vgabuf_limits_set(int16_t x0, int16_t y0, int16_t x1, int16_t y1);
+extern void vgabuf_limits_set_all(void);
 
 #endif
