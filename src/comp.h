b127cc0ae6949d2407f5279f412c1f5fb6d2b112
MyName <fork1oom@yandex.com>
2026-07-31 21:42:41 +0300

    Cleanup (remove TBLLEN)

--- src/comp.h
+++ src/comp.h
@@ -19,6 +19,5 @@
 #define ADDSATT(_v_, _n_, _top_) do { int _t_; _t_ = (_v_) + (_n_); SETMIN(_t_, (_top_)); (_v_) = _t_; } while (0)
 #define SUBSATT(_v_, _n_, _bot_) do { int _t_; _t_ = (_v_) - (_n_); SETMAX(_t_, (_bot_)); (_v_) = _t_; } while (0)
 #define SUBSAT0(_v_, _n_) SUBSATT(_v_, _n_, 0)
-#define TBLLEN(_t_) (sizeof((_t_)) / sizeof((_t_)[0]))
 
 #endif
